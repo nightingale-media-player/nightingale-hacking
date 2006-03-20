0e8080ccc7aec0a3f9bc4d470b5c0d8d8de84984
ben <ben@5aad4ff5-4b58-4159-ba3f-0e6328df4cad>
2006-03-20 20:50:57 +0000

    Adding the first stab at our cross-platform build system. Right now it only builds the medialibrary component on windows, but this is the framework that we'll be using (subject to major modification, of course).
    git-svn-id: http://publicsvn.songbirdnest.com/client/trunk@105 5aad4ff5-4b58-4159-ba3f-0e6328df4cad

---
+++ allmakefiles.sh
@@ -0,0 +1,196 @@
+#! /bin/sh
+#
+# ***** BEGIN LICENSE BLOCK *****
+# Version: MPL 1.1/GPL 2.0/LGPL 2.1
+#
+# The contents of this file are subject to the Mozilla Public License Version
+# 1.1 (the "License"); you may not use this file except in compliance with
+# the License. You may obtain a copy of the License at
+# http://www.mozilla.org/MPL/
+#
+# Software distributed under the License is distributed on an "AS IS" basis,
+# WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+# for the specific language governing rights and limitations under the
+# License.
+#
+# The Original Code is mozilla.org code.
+#
+# The Initial Developer of the Original Code is
+# Netscape Communications Corporation.
+# Portions created by the Initial Developer are Copyright (C) 1999
+# the Initial Developer. All Rights Reserved.
+#
+# Contributor(s):
+#
+# Alternatively, the contents of this file may be used under the terms of
+# either the GNU General Public License Version 2 or later (the "GPL"), or
+# the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+# in which case the provisions of the GPL or the LGPL are applicable instead
+# of those above. If you wish to allow use of your version of this file only
+# under the terms of either the GPL or the LGPL, and not to allow others to
+# use your version of this file under the terms of the MPL, indicate your
+# decision by deleting the provisions above and replace them with the notice
+# and other provisions required by the GPL or the LGPL. If you do not delete
+# the provisions above, a recipient may use your version of this file under
+# the terms of any one of the MPL, the GPL or the LGPL.
+#
+# ***** END LICENSE BLOCK *****
+
+# allmakefiles.sh - List of all makefiles. 
+#   Appends the list of makefiles to the variable, MAKEFILES.
+#   There is no need to rerun autoconf after adding makefiles.
+#   You only need to run configure.
+#
+#   Please keep the modules in this file in sync with those in
+#   mozilla/build/unix/modules.mk
+#
+
+MAKEFILES=""
+
+# add_makefiles - Shell function to add makefiles to MAKEFILES
+add_makefiles() {
+    MAKEFILES="$MAKEFILES $*"
+}
+
+if [ "$srcdir" = "" ]; then
+    srcdir=.
+fi
+
+#
+# App makefiles
+#
+
+MAKEFILES_app="
+Makefile
+app/Makefile
+app/content/Makefile
+app/icons/Makefile
+app/preferences/Makefile
+"
+
+MAKEFILES_build="
+build/autodefs.mk
+"
+
+MAKEFILES_components="
+components/Makefile
+"
+
+MAKEFILES_feathers="
+feathers/Makefile
+feathers/rubberducky/Makefile
+feathers/cardinal/Makefile
+"
+
+MAKEFILES_installer="
+installer/Makefile
+"
+
+MAKEFILES_locales="
+locales/Makefile
+locales/branding/Makefile
+locales/en-US/Makefile
+"
+
+#
+# Component makefiles
+#
+
+MAKEFILES_dataremote="
+components/dataremote/Makefile
+components/dataremote/public/Makefile
+components/dataremote/src/Makefile
+"
+
+MAKEFILES_devices="
+components/devices/Makefile
+components/devices/base/Makefile
+components/devices/base/public/Makefile
+components/devices/base/src/Makefile
+"
+
+MAKEFILES_devicemanager="
+components/devices/manager/Makefile
+components/devices/manager/public/Makefile
+components/devices/manager/src/Makefile
+"
+
+MAKEFILES_cddevice="
+components/devices/cd/Makefile
+components/devices/cd/public/Makefile
+components/devices/cd/src/Makefile
+"
+
+MAKEFILES_downloaddevice="
+components/devices/download/Makefile
+components/devices/download/public/Makefile
+components/devices/download/src/Makefile
+"
+
+MAKEFILES_wmdevice="
+components/devices/wm/Makefile
+components/devices/wm/public/Makefile
+components/devices/wm/src/Makefile
+"
+
+MAKEFILES_integration="
+components/integration/Makefile
+components/integration/public/Makefile
+components/integration/src/Makefile
+components/integration/src/linux/Makefile
+components/integration/src/macosx/Makefile
+components/integration/src/win32/Makefile
+"
+
+MAKEFILES_mediacore="
+components/mediacore/Makefile
+components/mediacore/metadata/Makefile
+components/mediacore/metadata/public/Makefile
+components/mediacore/metadata/src/Makefile
+components/mediacore/transcoding/Makefile
+components/mediacore/transcoding/public/Makefile
+components/mediacore/transcoding/src/Makefile
+"
+
+MAKEFILES_medialibrary="
+components/medialibrary/Makefile
+components/medialibrary/public/Makefile
+components/medialibrary/src/Makefile
+"
+
+MAKEFILES_playlistplayback="
+components/playlistplayback/Makefile
+components/playlistplayback/src/Makefile
+components/playlistplayback/public/Makefile
+"
+
+MAKEFILES_playlistreaders="
+components/playlistreaders/Makefile
+components/playlistreaders/m3u/Makefile
+components/playlistreaders/m3u/public/Makefile
+components/playlistreaders/m3u/src/Makefile
+components/playlistreaders/pls/Makefile
+components/playlistreaders/pls/public/Makefile
+components/playlistreaders/pls/src/Makefile
+"
+
+MAKEFILES_playlistsource="
+components/playlistsource/Makefile
+components/playlistsource/public/Makefile
+components/playlistsource/src/Makefile
+"
+
+MAKEFILES_servicesource="
+components/servicesource/Makefile
+components/servicesource/public/Makefile
+components/servicesource/src/Makefile
+"
+
+#
+# Put it all together
+#
+
+add_makefiles "
+$MAKEFILES_build
+$MAKEFILES_medialibrary
+"
---
+++ build/rules.mk
@@ -0,0 +1,277 @@
+#
+# BEGIN SONGBIRD GPL
+# 
+# This file is part of the Songbird web player.
+#
+# Copyright© 2006 Pioneers of the Inevitable LLC
+# http://www.songbirdnest.com
+# 
+# This file may be licensed under the terms of of the
+# GNU General Public License Version 2 (the “GPL”).
+# 
+# Software distributed under the License is distributed 
+# on an “AS IS” basis, WITHOUT WARRANTY OF ANY KIND, either 
+# express or implied. See the GPL for the specific language 
+# governing rights and limitations.
+#
+# You should have received a copy of the GPL along with this 
+# program. If not, go to http://www.gnu.org/licenses/gpl.html
+# or write to the Free Software Foundation, Inc., 
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+# 
+# END SONGBIRD GPL
+#
+
+##############################################################################
+# Rules.mk
+#
+# This file takes care of lots of messy rules. Each one is explained below.
+###############################################################################
+
+#------------------------------------------------------------------------------
+# Only include this file once
+ifndef RULES_MK_INCLUDED
+RULES_MK_INCLUDED=1
+#------------------------------------------------------------------------------
+
+targets=
+clean_targets=
+
+ifdef SUBDIRS
+targets += make_subdirs
+clean_targets += make_subdirs
+endif
+
+ifdef XPIDL_SRCS
+ifndef XPIDL_HEADER_SRCS
+XPIDL_HEADER_SRCS = $(XPIDL_SRCS)
+endif
+ifndef XPIDL_TYPELIB_SRCS
+XPIDL_TYPELIB_SRCS = $(XPIDL_SRCS)
+endif
+endif
+
+ifdef XPIDL_HEADER_SRCS
+targets += xpidl_compile_headers
+clean_targets += xpidl_clean_headers
+endif
+
+ifdef XPIDL_TYPELIB_SRCS
+targets += xpidl_compile_typelibs
+clean_targets += xpidl_clean_typelibs
+endif
+
+ifdef XPIDL_MODULE
+targets += xpidl_link
+clean_targets += xpidl_clean_link
+endif
+
+ifdef CPP_SOURCES
+targets += cpp_compile
+clean_targets += cpp_clean
+endif
+
+ifdef DYNAMIC_LIB
+targets += dll_link
+clean_targets += dll_clean
+endif
+
+all:   $(targets) \
+       garbage \
+       $(NULL)
+
+clean: $(clean_targets) \
+       $(NULL)
+
+#------------------------------------------------------------------------------
+# Rules for C++ compilation
+#------------------------------------------------------------------------------
+
+# CPP_SOURCES - a list of .cpp files to be compiled
+# CPP_INCLUDES - a list of include dirs with the '-I' prepended
+# CPP_FLAGS - an override of the default flags to pass to the compiler
+# CPP_EXTRA_FLAGS - a list of additional flags to pass to the compiler
+# CPP_DEFS - a override of the default defines to pass to the compiler with -D added
+# CPP_EXTRA_DEFS - a list of additional defines with -D to pass to the compiler
+
+ifdef CPP_SOURCES
+
+ifdef CPP_FLAGS
+compile_flags = $(CPP_FLAGS)
+else
+compile_flags = $(CXXFLAGS)
+ifdef CPP_EXTRA_FLAGS
+compile_flags += $(CPP_EXTRA_FLAGS)
+endif
+endif
+
+ifdef CPP_DEFS
+compile_defs = $(CPP_DEFS)
+else
+compile_defs = $(DEFS)
+ifdef CPP_EXTRA_DEFS
+compile_defs += $(CPP_EXTRA_DEFS)
+endif
+endif
+
+compiler_objects = $(CPP_SOURCES:.cpp=$(OBJ_SUFFIX))
+
+$(compiler_objects) :%$(OBJ_SUFFIX): %.cpp
+	$(CXX) $(compile_flags) $(compile_defs) $(CPP_INCLUDES) $<
+
+cpp_compile: $(compiler_objects)
+
+cpp_clean:
+	@rm -f $(compiler_objects) vc70.pdb
+
+.PHONY : cpp_compile cpp_clean
+
+endif #CPP_SOURCES
+
+#-----------------------
+
+# DYNAMIC_LIB - the name of a dll to link
+# DYNAMIC_LIB_OBJS - the object files to link into the dll
+# DYNAMIC_LIB_IMPORTS - an override to the default list of libs to link
+# DYNAMIC_LIB_EXTRA_IMPORTS - an additional list of libs to link
+# DYNAMIC_LIB_FLAGS - an override to the default linker flags
+# DYNAMIC_LIB_EXTRA_FLAGS - a list of additional flags to pass to the linker
+
+ifdef DYNAMIC_LIB
+
+ifdef DYNAMIC_LIB_FLAGS
+linker_flags = $(DYNAMIC_LIB_FLAGS)
+else
+linker_flags = $(LDFLAGS) $(LDFLAGS_DLL)
+ifdef DYNAMIC_LIB_EXTRA_FLAGS
+linker_flags += $(DYNAMIC_LIB_EXTRA_FLAGS)
+endif
+endif
+
+ifdef DYNAMIC_LIB_IMPORTS
+linker_imports = $(DYNAMIC_LIB_IMPORTS)
+else
+linker_imports = $(DEFAULT_LIBS)
+ifdef DYNAMIC_LIB_EXTRA_IMPORTS
+linker_imports += $(DYNAMIC_LIB_EXTRA_IMPORTS)
+endif
+endif
+
+dll_link: $(DYNAMIC_LIB_OBJS)
+	$(LD) $(LDFLAGS_OUT)"$(DYNAMIC_LIB)" $(linker_flags) $(linker_imports) $(DYNAMIC_LIB_OBJS)
+
+dll_clean:
+	@rm -rf $(DYNAMIC_LIB) \
+	        $(DYNAMIC_LIB:$(DLL_SUFFIX)=.pdb) \
+	        $(DYNAMIC_LIB:$(DLL_SUFFIX)=.lib) \
+	        $(DYNAMIC_LIB:$(DLL_SUFFIX)=.exp) \
+	        $(NULL)
+
+.PHONY : dll_link dll_clean
+
+endif #DYNAMIC_LIB
+
+#------------------------------------------------------------------------------
+# Rules for XPIDL compilation
+#------------------------------------------------------------------------------
+
+# XPIDL_SRCS - a list of idl files to turn into header and typelib files
+# XPIDL_HEADER_SRCS - a list of idl files to turn into C++ header files
+# XPIDL_TYPELIB_SRCS - a list of idl files to turn into xpt typelib files
+# XPIDL_MODULE - the name of an xpt file that will created from linking several
+#                other xpt typelibs
+# XPIDL_MODULE_TYPELIBS - a list of xpt files to link into the module
+
+ifdef XPIDL_HEADER_SRCS
+
+xpidl_headers  = $(XPIDL_HEADER_SRCS:.idl=.h)
+
+xpidl_compile_headers: $(XPIDL_HEADER_SRCS) $(xpidl_headers)
+$(xpidl_headers): %.h: %.idl
+	$(XPIDL) -m header -I $(MOZSDK_IDL_DIR) -I $(srcdir) $<
+
+xpidl_clean_headers:
+	@rm -f $(xpidl_headers)
+
+.PHONY : xpidl_compile_headers xpidl_clean_headers
+
+endif #XPIDL_HEADER_SRCS
+
+#-----------------------
+
+ifdef XPIDL_TYPELIB_SRCS
+
+xpidl_typelibs = $(XPIDL_TYPELIB_SRCS:.idl=.xpt)
+
+xpidl_compile_typelibs: $(XPIDL_TYPELIB_SRCS) $(xpidl_typelibs)
+$(xpidl_typelibs): %.xpt: %.idl
+	$(XPIDL) -m typelib -I $(MOZSDK_IDL_DIR) -I $(srcdir) $<
+
+xpidl_clean_typelibs:
+	@rm -f $(xpidl_typelibs)
+
+.PHONY : xpidl_compile_typelibs xpidl_clean_typelibs
+
+endif #XPIDL_TYPELIB_SRCS
+
+#-----------------------
+
+ifdef XPIDL_MODULE
+
+xpidl_module_typelibs = $(XPIDL_MODULE_TYPELIBS)
+
+xpidl_link: $(xpidl_module_typelibs)
+	$(XPTLINK) $(XPIDL_MODULE) $(xpidl_module_typelibs)
+
+xpidl_clean_link:
+	@rm -f $(XPIDL_MODULE)
+
+.PHONY : xpidl_link xpidl_clean_link
+endif #XPIDL_MODULE
+
+#------------------------------------------------------------------------------
+# Rules for running make recursively
+#------------------------------------------------------------------------------
+
+# SUBDIRS - set to a list of subdirectories that contain makefiles
+# SUBDIRDEPS - a list of dependencies of the form dir1:dir2 (dir1 depends on
+#              dir2 having already been processed)
+#
+# rules: subdirs - runs make in the list of subdirectories
+
+ifdef SUBDIRS
+
+make_subdirs: $(SUBDIRS)
+
+ifdef SUBDIRDEPS
+$(SUBDIRDEPS)
+endif
+
+$(SUBDIRS):
+	$(MAKE) -C $@ $(MAKECMDGOALS)
+
+.PHONY : make_subdirs $(SUBDIRS)
+
+endif #SUBDIRS
+
+#------------------------------------------------------------------------------
+# Rules for cleaning up
+#------------------------------------------------------------------------------
+
+# GARBAGE - a list of files to delete upon completion
+
+ifdef GARBAGE
+remove_cmd = rm -f $(GARBAGE)
+out:
+	$(warning garbage string: $(GARBAGE))
+.PHONY : out
+endif #GARBAGE
+
+garbage:
+	@$(remove_cmd)
+
+.PHONY : garbage
+
+#------------------------------------------------------------------------------
+endif #RULES_MK_INCLUDED
+#------------------------------------------------------------------------------
