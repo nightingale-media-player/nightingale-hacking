32cd4f506d30745b4412eaf6f51542557f4e280f
ben <ben@5aad4ff5-4b58-4159-ba3f-0e6328df4cad>
2006-05-03 02:04:54 +0000

    yes, a real macosx dmg file is built at the end of every built now, FUCK YEAH
    git-svn-id: http://publicsvn.songbirdnest.com/client/trunk@485 5aad4ff5-4b58-4159-ba3f-0e6328df4cad

--- installer/macosx/make-installer.sh
+++ installer/macosx/make-installer.sh
@@ -2,22 +2,19 @@
 
 CURRENT_DIR=`pwd`
 
-cd ../..
-rm -rf _built_installer
-mkdir _built_installer
+rm -rf ../../../_built_installer
+mkdir ../../../_built_installer
 
-cd ${CURRENT_DIR}/../../compiled/
 rm -rf _packaging
 mkdir _packaging
 mkdir _packaging/Songbird.app
 cp -RL dist/* _packaging/Songbird.app
-cp -RL ${CURRENT_DIR}/*.plist _packaging/Songbird.app/Contents/
-cp -RL ${CURRENT_DIR}/Resources/* _packaging/Songbird.app/Contents/Resources/
-cp ${CURRENT_DIR}/MacSongbirdInstall.icns _packaging/Songbird.app/Contents/Resources/
-cp ${CURRENT_DIR}/GPL.txt _packaging/Songbird.app/
-cp ${CURRENT_DIR}/LICENSE.txt _packaging/Songbird.app/
-CP ${CURRENT_DIR}/TRADEMARK.txt _packaging/Songbird.app/
+cp -RL ../../../installer/macosx/*.plist ../../_packaging/Songbird.app/Contents/
+cp -RL ../../../installer/macosx/Resources/* ../../_packaging/Songbird.app/Contents/Resources/
+cp ../../../installer/macosx//MacSongbirdInstall.icns ../../_packaging/Songbird.app/Contents/Resources/
+cp ../../../installer/macosx/GPL.txt ../../_packaging/Songbird.app/
+cp ../../../installer/macosx/LICENSE.txt ../../_packaging/Songbird.app/
+cp ../../../installer/macosx/TRADEMARK.txt ../../_packaging/Songbird.app/
 
-cd ${CURRENT_DIR}
-./make-diskimage ${CURRENT_DIR}/../../_built_installer/Songbird.dmg ${CURRENT_DIR}/../../compiled/_packaging Songbird -null- ${CURRENT_DIR}/songbird.dsstore
+../../../installer/macosx/make-diskimage ../../../_built_installer/Songbird.dmg ../../../compiled/_packaging Songbird -null- ../../../installer/macosx/songbird.dsstore
 
