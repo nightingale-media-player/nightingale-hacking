1fb2880d731a1e7f6909814fd516c7c381a8838a
ben <ben@5aad4ff5-4b58-4159-ba3f-0e6328df4cad>
2006-05-03 00:32:44 +0000

    -w00t
    git-svn-id: http://publicsvn.songbirdnest.com/client/trunk@481 5aad4ff5-4b58-4159-ba3f-0e6328df4cad

--- installer/linux/make-installer.sh
+++ installer/linux/make-installer.sh
@@ -1,3 +1,26 @@
 #!/bin/bash
 
+CURRENT_DIR=`pwd`
+CURRENT_DATE=`date +%Y%m%d`
+SONGBIRD_DIR=Songbird_${CURRENT_DATE}
+
+rm -rf ../../built_installer
+mkdir ../../built_installer
+
+cd ${CURRENT_DIR}/../../compiled/
+rm -rf ${SONGBIRD_DIR}
+mkdir ${SONGBIRD_DIR}
+
+cp -RL dist/* ${SONGBIRD_DIR}
+cp ${CURRENT_DIR}/GPL.txt ${SONGBIRD_DIR}
+cp ${CURRENT_DIR}/LICENSE.txt ${SONGBIRD_DIR}
+cp ${CURRENT_DIR}/TRADEMARK.txt ${SONGBIRD_DIR}
+
+rm -f ${SONGBIRD_DIR}.tar
+rm -f ${SONGBIRD_DIR}.tar.gz
+
+tar -c -f ${SONGBIRD_DIR}.tar ${SONGBIRD_DIR}
+gzip ${SONGBIRD_DIR}.tar
+
+cp ${SONGBIRD_DIR}.tar.gz ${CURRENT_DIR}/../../_built_installer
 
