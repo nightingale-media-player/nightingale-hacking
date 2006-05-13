043a30f06a0835f7ec2af6a00bf31c04590d1119
aus <aus@5aad4ff5-4b58-4159-ba3f-0e6328df4cad>
2006-05-13 00:13:29 +0000

    Fixing the make-mozilla-sdk.sh script
    git-svn-id: http://publicsvn.songbirdnest.com/client/trunk@557 5aad4ff5-4b58-4159-ba3f-0e6328df4cad

--- tools/common/scripts/make-mozilla-sdk.sh
+++ tools/common/scripts/make-mozilla-sdk.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 
+bin_files="regxpcom*
+           xpidl*
+           xpt_dump*
+           xpt_link*
+"
+
+lib_files="*embed_base.*
+           *nspr4.*
+           *nspr4_s.*
+           *plc4.*
+           *plc4_s.*
+           *plds4.*
+           *plds4_s.*
+           *unicharutil_s.*
+           *xpcom.*
+           *xpcomglue.*
+           *xpcomglue_s.*
+           *xul.*
+           *XUL*
+"
+
 notice() {
   echo $* 1>&2
 }
@@ -9,43 +30,37 @@ if [ $# != 2 ]; then
   exit 1
 fi
 
-bin_files="regxpcom* \
-           xpidl* \
-           xpt_dump* \
-           xpt_link* \
-"
+reldistdir="$1"
+temp1=`dirname "$reldistdir"`
+temp2=`basename "$reldistdir"`
+distdir="`cd \"$temp1\" 2>/dev/null && pwd || echo \"$temp1\"`/$temp2"
 
-lib_files="*embed_base.* \
-           *nspr4.* \
-           *nspr4_s.* \
-           *plc4.* \
-           *plc4_s.* \
-           *plds4.* \
-           *plds4_s.* \
-           *unicharutil_s.* \
-           *xpcom.* \
-           *xpcomglue.* \
-           *xpcomglue_s.* \
-           *xul.* \
-           *XUL* \
-"
+relsdkdir="$2"
+temp1=`dirname "$relsdkdir"`
+temp2=`basename "$relsdkdir"`
+sdkdir="`cd \"$temp1\" 2>/dev/null && pwd || echo \"$temp1\"`/$temp2"
+
+mkdir -p "$sdkdir"
 
 notice "copying binary files..."
 cd "$sdkdir" && mkdir -p bin
-cd "$distdir"/bin && cp -Lfp "$bin_files" "$sdkdir"/bin
+cd "$distdir/bin" && cp -Lfp $bin_files "$sdkdir/bin"
 
 notice "copying library files..."
 cd "$sdkdir" && mkdir -p lib
-cd "$distdir"/lib && cp -Lfp "$lib_files" "$sdkdir"/lib
+# some os don't have all these files, so silence errors
+cd "$distdir/lib" && cp -Lfp $lib_files "$sdkdir/lib" 2>/dev/null
 
 notice "copying include files..."
 cd "$sdkdir" && mkdir -p include
-cd "$distdir"/include && cp -RLfp * "$sdkdir"/indlude
+cd "$distdir/include" && cp -RLfp * "$sdkdir/include"
 
-notice "copying binary files..."
+notice "copying idl files..."
 cd "$sdkdir" && mkdir -p idl
-cd "$distdir"/idl && cp -RLfp * "$sdkdir"/idl
+cd "$distdir/idl" && cp -Lfp * "$sdkdir/idl"
 
 notice "copying frozen sdk..."
 cd "$sdkdir" && mkdir -p frozen
-cd "$distdir"/sdk && cp -RLfp * "$sdkdir"/frozen
+cd "$distdir/sdk" && cp -RLfp * "$sdkdir/frozen"
+
+notice "done."
