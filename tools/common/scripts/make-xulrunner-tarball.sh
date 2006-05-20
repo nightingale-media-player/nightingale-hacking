5b9d4ee8287c5b4862c37fe4b8dd132be35e4f0a
redfive <redfive@5aad4ff5-4b58-4159-ba3f-0e6328df4cad>
2006-05-20 00:04:49 +0000

    Make the xulrunner tarball. doesn't tar into 'xulrunner'
    git-svn-id: http://publicsvn.songbirdnest.com/client/trunk@653 5aad4ff5-4b58-4159-ba3f-0e6328df4cad

---
+++ tools/common/scripts/make-xulrunner-tarball.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+notice() {
+  echo $* 1>&2
+}
+
+if [ $# != 3 ]; then
+  notice "usage: make-xulrunner-tarball.sh mozilla-build-dist-bin-dir songbird-tarball-dest-dir target-tarball-name"
+  exit 1
+fi
+
+srcdir="$1"
+temp1=`dirname "$srcdir"`
+temp2=`basename "$srcdir"`
+bindir="`cd \"$temp1\" 2>/dev/null && pwd || echo \"$temp1\"`/$temp2"
+
+destdir="$2"
+temp1=`dirname "$destdir"`
+temp2=`basename "$destdir"`
+tarballdir="`cd \"$temp1\" 2>/dev/null && pwd || echo \"$temp1\"`/$temp2"
+
+tarballname="$3"
+
+tarball="$tarballdir/$tarballname"
+
+echo cd "$bindir" 
+cd "$bindir" 
+
+notice "creating tarball in dest..."
+tar czvhf $tarball *
+
+notice "done."
