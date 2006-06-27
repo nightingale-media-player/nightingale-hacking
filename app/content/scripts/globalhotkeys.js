2f310511cf86914eda9a230355d9725151cdaddf
lone <lone@5aad4ff5-4b58-4159-ba3f-0e6328df4cad>
2006-06-27 00:50:32 +0000

    added hotkeys preference pane
    git-svn-id: http://publicsvn.songbirdnest.com/client/trunk@1015 5aad4ff5-4b58-4159-ba3f-0e6328df4cad

--- app/content/scripts/globalhotkeys.js
+++ app/content/scripts/globalhotkeys.js
@@ -23,143 +23,27 @@
 // END SONGBIRD GPL
 //
  */
+ 
+ // This needs to become an api so plugins can register new actions and callbacks for them
 
-// Global Hotkeys
+var hotkey_service;
 
-const DOM_VK_CANCEL         = 0x03;
-const DOM_VK_HELP           = 0x06;
-const DOM_VK_BACK_SPACE     = 0x08;
-const DOM_VK_TAB            = 0x09;
-const DOM_VK_CLEAR          = 0x0C;
-const DOM_VK_RETURN         = 0x0D;
-const DOM_VK_ENTER          = 0x0E;
-const DOM_VK_SHIFT          = 0x10;
-const DOM_VK_CONTROL        = 0x11;
-const DOM_VK_ALT            = 0x12;
-const DOM_VK_PAUSE          = 0x13;
-const DOM_VK_CAPS_LOCK      = 0x14;
-const DOM_VK_ESCAPE         = 0x1B;
-const DOM_VK_SPACE          = 0x20;
-const DOM_VK_PAGE_UP        = 0x21;
-const DOM_VK_PAGE_DOWN      = 0x22;
-const DOM_VK_END            = 0x23;
-const DOM_VK_HOME           = 0x24;
-const DOM_VK_LEFT           = 0x25;
-const DOM_VK_UP             = 0x26;
-const DOM_VK_RIGHT          = 0x27;
-const DOM_VK_DOWN           = 0x28;
-const DOM_VK_PRINTSCREEN    = 0x2C;
-const DOM_VK_INSERT         = 0x2D;
-const DOM_VK_DELETE         = 0x2E;
+var user_agent = navigator.userAgent;
+var PLATFORM_WIN32 = user_agent.indexOf("Windows") != -1;
+var PLATFORM_MACOSX = user_agent.indexOf("Mac OS X") != -1;
+var PLATFORM_LINUX = user_agent.indexOf("Linux") != -1;
 
-// DOM_VK_0 - DOM_VK_9 match their ascii values
-const DOM_VK_0              = 0x30;
-const DOM_VK_1              = 0x31;
-const DOM_VK_2              = 0x32;
-const DOM_VK_3              = 0x33;
-const DOM_VK_4              = 0x34;
-const DOM_VK_5              = 0x35;
-const DOM_VK_6              = 0x36;
-const DOM_VK_7              = 0x37;
-const DOM_VK_8              = 0x38;
-const DOM_VK_9              = 0x39;
-
-const DOM_VK_SEMICOLON      = 0x3B;
-const DOM_VK_EQUALS         = 0x3D;
-
-// DOM_VK_A - DOM_VK_Z match their ascii values
-const DOM_VK_A              = 0x41;
-const DOM_VK_B              = 0x42;
-const DOM_VK_C              = 0x43;
-const DOM_VK_D              = 0x44;
-const DOM_VK_E              = 0x45;
-const DOM_VK_F              = 0x46;
-const DOM_VK_G              = 0x47;
-const DOM_VK_H              = 0x48;
-const DOM_VK_I              = 0x49;
-const DOM_VK_J              = 0x4A;
-const DOM_VK_K              = 0x4B;
-const DOM_VK_L              = 0x4C;
-const DOM_VK_M              = 0x4D;
-const DOM_VK_N              = 0x4E;
-const DOM_VK_O              = 0x4F;
-const DOM_VK_P              = 0x50;
-const DOM_VK_Q              = 0x51;
-const DOM_VK_R              = 0x52;
-const DOM_VK_S              = 0x53;
-const DOM_VK_T              = 0x54;
-const DOM_VK_U              = 0x55;
-const DOM_VK_V              = 0x56;
-const DOM_VK_W              = 0x57;
-const DOM_VK_X              = 0x58;
-const DOM_VK_Y              = 0x59;
-const DOM_VK_Z              = 0x5A;
-
-const DOM_VK_CONTEXT_MENU   = 0x5D;
-
-const DOM_VK_NUMPAD0        = 0x60;
-const DOM_VK_NUMPAD1        = 0x61;
-const DOM_VK_NUMPAD2        = 0x62;
-const DOM_VK_NUMPAD3        = 0x63;
-const DOM_VK_NUMPAD4        = 0x64;
-const DOM_VK_NUMPAD5        = 0x65;
-const DOM_VK_NUMPAD6        = 0x66;
-const DOM_VK_NUMPAD7        = 0x67;
-const DOM_VK_NUMPAD8        = 0x68;
-const DOM_VK_NUMPAD9        = 0x69;
-const DOM_VK_MULTIPLY       = 0x6A;
-const DOM_VK_ADD            = 0x6B;
-const DOM_VK_SEPARATOR      = 0x6C;
-const DOM_VK_SUBTRACT       = 0x6D;
-const DOM_VK_DECIMAL        = 0x6E;
-const DOM_VK_DIVIDE         = 0x6F;
-const DOM_VK_F1             = 0x70;
-const DOM_VK_F2             = 0x71;
-const DOM_VK_F3             = 0x72;
-const DOM_VK_F4             = 0x73;
-const DOM_VK_F5             = 0x74;
-const DOM_VK_F6             = 0x75;
-const DOM_VK_F7             = 0x76;
-const DOM_VK_F8             = 0x77;
-const DOM_VK_F9             = 0x78;
-const DOM_VK_F10            = 0x79;
-const DOM_VK_F11            = 0x7A;
-const DOM_VK_F12            = 0x7B;
-const DOM_VK_F13            = 0x7C;
-const DOM_VK_F14            = 0x7D;
-const DOM_VK_F15            = 0x7E;
-const DOM_VK_F16            = 0x7F;
-const DOM_VK_F17            = 0x80;
-const DOM_VK_F18            = 0x81;
-const DOM_VK_F19            = 0x82;
-const DOM_VK_F20            = 0x83;
-const DOM_VK_F21            = 0x84;
-const DOM_VK_F22            = 0x85;
-const DOM_VK_F23            = 0x86;
-const DOM_VK_F24            = 0x87;
-
-const DOM_VK_NUM_LOCK       = 0x90;
-const DOM_VK_SCROLL_LOCK    = 0x91;
-
-const DOM_VK_COMMA          = 0xBC;
-const DOM_VK_PERIOD         = 0xBE;
-const DOM_VK_SLASH          = 0xBF;
-const DOM_VK_BACK_QUOTE     = 0xC0;
-const DOM_VK_OPEN_BRACKET   = 0xDB;
-const DOM_VK_BACK_SLASH     = 0xDC;
-const DOM_VK_CLOSE_BRACKET  = 0xDD;
-const DOM_VK_QUOTE          = 0xDE;
-
-const DOM_VK_META           = 0xE0;
-
-var   hotkey_service;
+var meta_key_str = "meta";
+if (PLATFORM_WIN32) meta_key_str = "win";
+if (PLATFORM_MACOSX) meta_key_str = "command";
+if (PLATFORM_LINUX) meta_key_str = "meta";
 
 function initGlobalHotkeys() {
   // Access global hotkeys component (if it exists)
   var globalHotkeys = Components.classes["@songbirdnest.com/Songbird/GlobalHotkeys;1"];
   if (globalHotkeys) {
     hotkey_service = globalHotkeys.getService(Components.interfaces.sbIGlobalHotkeys);
-    if (hotkey_service) loadHotkeysFromPrefs()
+    loadHotkeysFromPrefs()
   }
 }
 
@@ -173,15 +57,15 @@ var hotkeyHandler =
 {
     OnHotkey: function( hotkeyid )
     {
-      switch ( hotkeyid )
+      switch ( hotkeyid.toLowerCase() )
       {
         // Call the appropriate action 
-        case "Playback: Volume up": hotkey_volumeUp(); break;
-        case "Playback: Volume down": hotkey_volumeDown(); break;
-        case "Playback: Next in playlist": hotkey_nextTrack(); break;
-        case "Playback: Previous in playlist": hotkey_previousTrack(); break;
-        case "Playback: Play/Pause": hotkey_playPause(); break;
-        case "Playback: Pause": hotkey_pause(); break;
+        case "playback: volume up": hotkey_volumeUp(); break;
+        case "playback: volume down": hotkey_volumeDown(); break;
+        case "playback: next in playlist": hotkey_nextTrack(); break;
+        case "playback: previous in playlist": hotkey_previousTrack(); break;
+        case "playback: play/pause": hotkey_playPause(); break;
+        case "playback: pause": hotkey_pause(); break;
         default: alert("Unknown hotkey action '" + hotkeyid + "'"); break;
       }
     },
@@ -201,8 +85,10 @@ var hotkeyHandler =
 
 // Load hotkeys
 function loadHotkeysFromPrefs() {
-  var count = SBDataGetValue("globalhotkey.count");
-  if (count == 0) count = setDefaultGlobalHotkeys();
+  if (!hotkey_service) return;
+  setDefaultGlobalHotkeys();
+  if (!SBDataGetIntValue("globalhotkeys.enabled")) return;
+  var count = SBDataGetIntValue("globalhotkeys.count");
   for (var i=0;i<count;i++) {
     // Read hotkey binding from user preferences
     var root = "globalhotkey." + i + ".";
@@ -217,153 +103,67 @@ function loadHotkeysFromPrefs() {
     var meta = false;
     var keyCode = 0;
     for (var j=0;j<keys.length;j++) {
+      keys[j] = keys[j].toLowerCase();
       if (keys[j] == "alt") alt = true;
       else if (keys[j] == "shift") shift = true;
-      else if (keys[j] == "control") ctrl = true;
+      else if (keys[j] == "ctrl") ctrl = true;
       else if (keys[j] == "meta") meta = true;
       else keyCode = stringToKeyCode(keys[j]);
     }
     // If we had a key code (and possibly modifiers), register the corresponding action for it
     if (keyCode != 0) hotkey_service.AddHotkey(keyCode, alt, ctrl, shift, meta, action, hotkeyHandler);
   }
+  beginWatchingHotkeys();
 }
 
 // Sets the default hotkey settings
 function setDefaultGlobalHotkeys() {
-  SBDataSetValue("globalhotkey.count", 5);
-  SBDataSetValue("globalhotkey.0.key",    "meta-up");
-  SBDataSetValue("globalhotkey.0.action", "Playback: Volume up");
-  SBDataSetValue("globalhotkey.1.key",    "meta-down");
-  SBDataSetValue("globalhotkey.1.action", "Playback: Volume down");
-  SBDataSetValue("globalhotkey.2.key",    "meta-right");
-  SBDataSetValue("globalhotkey.2.action", "Playback: Next in playlist");
-  SBDataSetValue("globalhotkey.3.key",    "meta-left");
-  SBDataSetValue("globalhotkey.3.action", "Playback: Previous in playlist");
-  SBDataSetValue("globalhotkey.4.key",    "meta-numpad0");
-  SBDataSetValue("globalhotkey.4.action", "Playback: Play/Pause");
+  if (!SBDataGetIntValue("globalhotkeys.changed")) {
+    SBDataSetValue("globalhotkeys.changed", 1);
+    SBDataSetValue("globalhotkeys.enabled", 1);
+
+    SBDataSetValue("globalhotkeys.count", 5);
+    
+    SBDataSetValue("globalhotkey.0.key",             "meta-$38");
+    SBDataSetValue("globalhotkey.0.key.readable",    meta_key_str + "-up");
+    SBDataSetValue("globalhotkey.0.action",          "Playback: Volume up");
+    
+    SBDataSetValue("globalhotkey.1.key",             "meta-$40");
+    SBDataSetValue("globalhotkey.1.key.readable",    meta_key_str + "-down");
+    SBDataSetValue("globalhotkey.1.action",          "Playback: Volume down");
+    
+    SBDataSetValue("globalhotkey.2.key",             "meta-$39");
+    SBDataSetValue("globalhotkey.2.key.readable",    meta_key_str + "-right");
+    SBDataSetValue("globalhotkey.2.action",          "Playback: Next in playlist");
+    
+    SBDataSetValue("globalhotkey.3.key",             "meta-$37");
+    SBDataSetValue("globalhotkey.3.key.readable",    meta_key_str + "-left");
+    SBDataSetValue("globalhotkey.3.action",          "Playback: Previous in playlist");
+    
+    SBDataSetValue("globalhotkey.4.key",             "meta-$96");
+    SBDataSetValue("globalhotkey.4.key.readable",    meta_key_str + "-numpad0");
+    SBDataSetValue("globalhotkey.4.action",          "Playback: Play/Pause");
+  }
 }
 
 // Returns the key code for a given key string
 function stringToKeyCode( str ) {
-  switch (str.toLowerCase()) {
-    case "cancel": return DOM_VK_CANCEL;
-    case "help": return DOM_VK_HELP;
-    case "backspace": return DOM_VK_BACK_SPACE;
-    case "tab": return DOM_VK_TAB;
-    case "clear": return DOM_VK_CLEAR;
-    case "return": return DOM_VK_RETURN;
-    case "enter": return DOM_VK_ENTER;
-    case "shift": return DOM_VK_SHIFT;
-    case "control": return DOM_VK_CONTROL;
-    case "alt": return DOM_VK_ALT;
-    case "pause": return DOM_VK_PAUSE;
-    case "capslock": return DOM_VK_CAPS_LOCK;
-    case "escape": return DOM_VK_ESCAPE;
-    case "space": return DOM_VK_SPACE;
-    case "pageup": return DOM_VK_PAGE_UP;
-    case "pagedown": return DOM_VK_PAGE_DOWN;
-    case "end": return DOM_VK_END;
-    case "home": return DOM_VK_HOME;
-    case "left": return DOM_VK_LEFT;
-    case "up": return DOM_VK_UP;
-    case "right": return DOM_VK_RIGHT;
-    case "down": return DOM_VK_DOWN;
-    case "printscreen": return DOM_VK_PRINTSCREEN;
-    case "insert": return DOM_VK_INSERT;
-    case "delete": return DOM_VK_DELETE;
-    case "0": return DOM_VK_0;
-    case "1": return DOM_VK_1;
-    case "2": return DOM_VK_2;
-    case "3": return DOM_VK_3;
-    case "4": return DOM_VK_4;
-    case "5": return DOM_VK_5;
-    case "6": return DOM_VK_6;
-    case "7": return DOM_VK_7;
-    case "8": return DOM_VK_8;
-    case "9": return DOM_VK_9;
-    case "semicolon": return DOM_VK_SEMICOLON;
-    case "equals": return DOM_VK_EQUALS;
-    case "a": return DOM_VK_A;
-    case "b": return DOM_VK_B;
-    case "c": return DOM_VK_C;
-    case "d": return DOM_VK_D;
-    case "e": return DOM_VK_E;
-    case "f": return DOM_VK_F;
-    case "g": return DOM_VK_G;
-    case "h": return DOM_VK_H;
-    case "i": return DOM_VK_I;
-    case "j": return DOM_VK_J;
-    case "k": return DOM_VK_K;
-    case "l": return DOM_VK_L;
-    case "m": return DOM_VK_M;
-    case "n": return DOM_VK_N;
-    case "o": return DOM_VK_O;
-    case "p": return DOM_VK_P;
-    case "q": return DOM_VK_Q;
-    case "r": return DOM_VK_R;
-    case "s": return DOM_VK_S;
-    case "t": return DOM_VK_T;
-    case "u": return DOM_VK_U;
-    case "v": return DOM_VK_V;
-    case "w": return DOM_VK_W;
-    case "x": return DOM_VK_X;
-    case "y": return DOM_VK_Y;
-    case "z": return DOM_VK_Z;
-    case "contextmenu": return DOM_VK_CONTEXT_MENU;
-    case "numpad0": return DOM_VK_NUMPAD0;
-    case "numpad1": return DOM_VK_NUMPAD1;
-    case "numpad2": return DOM_VK_NUMPAD2;
-    case "numpad3": return DOM_VK_NUMPAD3;
-    case "numpad4": return DOM_VK_NUMPAD4;
-    case "numpad5": return DOM_VK_NUMPAD5;
-    case "numpad6": return DOM_VK_NUMPAD6;
-    case "numpad7": return DOM_VK_NUMPAD7;
-    case "numpad8": return DOM_VK_NUMPAD8;
-    case "numpad9": return DOM_VK_NUMPAD9;
-    case "multiply": return DOM_VK_MULTIPLY;
-    case "add": return DOM_VK_ADD;
-    case "separator": return DOM_VK_SEPARATOR;
-    case "subtract": return DOM_VK_SUBTRACT;
-    case "decimal": return DOM_VK_DECIMAL;
-    case "divide": return DOM_VK_DIVIDE;
-    case "f1": return DOM_VK_F1;
-    case "f2": return DOM_VK_F2;
-    case "f3": return DOM_VK_F3;
-    case "f4": return DOM_VK_F4;
-    case "f5": return DOM_VK_F5;
-    case "f6": return DOM_VK_F6;
-    case "f7": return DOM_VK_F7;
-    case "f8": return DOM_VK_F8;
-    case "f9": return DOM_VK_F9;
-    case "f10": return DOM_VK_F10;
-    case "f11": return DOM_VK_F11;
-    case "f12": return DOM_VK_F12;
-    case "f13": return DOM_VK_F13;
-    case "f14": return DOM_VK_F14;
-    case "f15": return DOM_VK_F15;
-    case "f16": return DOM_VK_F16;
-    case "f17": return DOM_VK_F17;
-    case "f18": return DOM_VK_F18;
-    case "f19": return DOM_VK_F19;
-    case "f20": return DOM_VK_F20;
-    case "f21": return DOM_VK_F21;
-    case "f22": return DOM_VK_F22;
-    case "f23": return DOM_VK_F23;
-    case "f24": return DOM_VK_F24;
-    case "numlock": return DOM_VK_NUM_LOCK;
-    case "scrolllock": return DOM_VK_SCROLL_LOCK;
-    case "comma": return DOM_VK_COMMA;
-    case "period": return DOM_VK_PERIOD;
-    case "slash": return DOM_VK_SLASH;
-    case "backquote": return DOM_VK_BACK_QUOTE;
-    case "openbracket": return DOM_VK_OPEN_BRACKET;
-    case "backslash": return DOM_VK_BACK_SLASH;
-    case "closebracket": return DOM_VK_CLOSE_BRACKET;
-    case "quote": return DOM_VK_QUOTE;
-    case "meta": return DOM_VK_META;
-  }
+  if (str.slice(0, 1) == '$') return str.slice(1);
   return 0;
 }
+
+var songbird_hotkeys_event;
+
+function beginWatchingHotkeys() {
+  songbird_hotkeys_event = new sbIDataRemote("globalhotkeys.changed");
+  songbird_hotkeys_event.bindCallbackFunction( globalHotkeysChanged, true )
+}
+
+function globalHotkeysChanged(v) {
+  resetGlobalHotkeys();
+  loadHotkeysFromPrefs();
+}
+
 
 /*
 function log(str) {
---
+++ app/content/scripts/preferences/hotkeys.js
@@ -0,0 +1,171 @@
+// todo: internationalize 
+
+var gHotkeysPane = {
+
+  _list: null,
+  _remove: null,
+  _set: null,
+  _action: null,
+  _hotkey: null,
+
+  init: function ()
+  {
+    var jsLoader = Components.classes["@mozilla.org/moz/jssubscript-loader;1"].getService(Components.interfaces.mozIJSSubScriptLoader);
+    jsLoader.loadSubScript( "chrome://songbird/content/scripts/sbIDataRemote.js", this );
+
+    this._list = document.getElementById("hotkey.list");
+    this._set = document.getElementById("hotkey.set");
+    this._remove = document.getElementById("hotkey.remove");
+    this._action = document.getElementById("hotkey.actions");
+    this._hotkey = document.getElementById("hotkey.hotkey");
+
+    this.loadHotkeys();
+  },
+  
+  loadHotkeys: function()
+  {
+    var count = this.SBDataGetValue("globalhotkeys.count");
+    for (var i=0;i<count;i++) {
+      // Read hotkey binding from user preferences
+      var root = "globalhotkey." + i + ".";
+      var keycombo = this.SBDataGetValue(root + "key");
+      var keydisplay = this.SBDataGetValue(root + "key.readable");
+      var action = this.SBDataGetValue(root + "action");
+      // make list items accordingly
+      this._addItem(keycombo, keydisplay, action, -1);
+    }
+    this.updateButtons();
+  },
+  
+  _addItem: function(keycombo, keydisplay, action, replace)
+  {
+    // if an index to replace was specified, figure out the corresponding item to use as a marker for insertBefore
+    var before = (replace == -1) ? null : this._list.getItemAtIndex(replace);
+    // make list item
+    var listitem = document.createElement("listitem");
+    listitem.keycombo = keycombo;
+    var listcell_action = document.createElement("listcell");
+    listcell_action.setAttribute("label", action);
+    var listcell_keydisplay = document.createElement("listcell");
+    listcell_keydisplay.setAttribute("label", keydisplay.toUpperCase());
+    listitem.appendChild(listcell_action);
+    listitem.appendChild(listcell_keydisplay);
+    if (before) {
+      // insert the item at the correct spot
+      this._list.insertBefore(listitem, before);
+      // and remove the item we were supposed to replace
+      this._list.removeChild(before);
+    } else {
+      // append at the end of the list (replace was -1)
+      this._list.appendChild(listitem);
+    }
+  },
+  
+  saveHotkeys: function()
+  {
+    // Save all hotkeys to prefs, extract them from the list itself
+    var n = this._list.getRowCount();
+    this.SBDataSetValue("globalhotkeys.count", n);
+    for (var i=0;i<n;i++)
+    {
+      var root = "globalhotkey." + i + ".";
+      var item = this._list.getItemAtIndex(i);
+      var keycombo = item.keycombo;
+      var actioncell = item.firstChild;
+      var keydisplaycell = actioncell.nextSibling;
+      var actionstr = actioncell.getAttribute("label");
+      var keydisplay = keydisplaycell.getAttribute("label");
+      this.SBDataSetValue(root + "key", keycombo);
+      this.SBDataSetValue(root + "key.readable", keydisplay);
+      this.SBDataSetValue(root + "action", actionstr);
+    }
+    this.SBDataFireEvent("globalhotkeys.changed");
+  },
+  
+  onSelectHotkey: function()
+  {
+    this.updateButtons();
+    var selected = this._list.selectedIndex;
+    if (selected >= 0) {
+      var item = this._list.getItemAtIndex(selected);
+      var actioncell = item.firstChild;
+      var keydisplaycell = actioncell.nextSibling;
+      // load action in menulist
+      var nodes = this._action.firstChild.childNodes;
+      var actionstr = actioncell.getAttribute("label");
+      for (var i=0;i<nodes.length;i++) {
+        if (nodes[i].getAttribute("label") == actionstr) {
+          this._action.selectedItem = nodes[i]
+          break;
+        }
+      }
+      // load hotkey in hotkey control
+      this._hotkey.setHotkey(item.keycombo, keydisplaycell.getAttribute("label"));
+    }
+  },
+  
+  updateButtons: function()
+  {
+    // disable set & remove when no item is selected
+    var disabled = (this._list.selectedIndex== -1);
+    this._remove.setAttribute("disabled", disabled);
+    this._set.setAttribute("disabled", disabled);
+  },
+  
+  addHotkey: function()
+  {
+    // add the hotkey to the list
+    var keycombo = this._hotkey.getHotkey(false);
+    if (this._checkComboExists(keycombo, -1)) return;
+    var keydisplay = this._hotkey.getHotkey(true);
+    var action = this._action.selectedItem.getAttribute("label");
+    if (action == "" || keycombo == "" || keydisplay == "") return;
+    this._addItem(keycombo, keydisplay, action, -1);
+    // select the last (newly added) item
+    this._list.selectedIndex = this._list.getRowCount()-1;
+    this.saveHotkeys();
+  },
+  
+  setHotkey: function()
+  {
+    // change the hotkey item that's currently selected
+    var selected = this._list.selectedIndex;
+    var keycombo = this._hotkey.getHotkey(false);
+    if (this._checkComboExists(keycombo, selected)) return;
+    var keydisplay = this._hotkey.getHotkey(true);
+    var action = this._action.selectedItem.getAttribute("label");
+    if (action == "" || keycombo == "" || keydisplay == "") return;
+    this._addItem(keycombo, keydisplay, action, selected);
+    // reselect the item after it's been changed
+    this._list.selectedIndex = selected;
+    this.saveHotkeys();
+  },
+  
+  _checkComboExists: function(keycombo, ignoreentry)
+  {
+    // checks wether the key combination already exists
+    for (var i=0;i<this._list.getRowCount();i++) 
+    {
+      if (i == ignoreentry) continue;
+      var item = this._list.getItemAtIndex(i);
+      if (item.keycombo == keycombo) {
+        alert("This hotkey is already taken");
+        return true;
+      }
+    }
+    return false;
+  },
+  
+  removeHotkey: function()
+  {
+    // remove the selected item from the list
+    var index = this._list.selectedIndex;
+    var item = this._list.getItemAtIndex(index);
+    this._list.removeChild(item);
+    // select the next item, or the previous one if that was the last
+    if (index >= this._list.getRowCount()) index = this._list.getRowCount()-1;
+    this._list.selectedIndex = index;
+    this.saveHotkeys();
+  }
+  
+};
