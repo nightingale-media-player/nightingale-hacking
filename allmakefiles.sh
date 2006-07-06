374e9965632b7455000837d5bd69451dbdfc5c6e
aus <aus@5aad4ff5-4b58-4159-ba3f-0e6328df4cad>
2006-07-06 22:40:46 +0000

    added javascript window cloak integration component for linux and os x.
    git-svn-id: http://publicsvn.songbirdnest.com/client/trunk@1139 5aad4ff5-4b58-4159-ba3f-0e6328df4cad

--- allmakefiles.sh
+++ allmakefiles.sh
@@ -165,6 +165,10 @@ MAKEFILES_integration="
 components/integration/Makefile
 components/integration/public/Makefile
 components/integration/src/Makefile
+components/integration/src/linux/Makefile
+components/integration/src/macosx/Makefile
+components/integration/src/win32/Makefile
+components/integration/src/script/Makefile
 "
 
 MAKEFILES_mediacore="
