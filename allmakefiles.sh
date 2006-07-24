0adbdbcde7bc96e9d7cac7a11b56ce557ba655c5
aus <aus@5aad4ff5-4b58-4159-ba3f-0e6328df4cad>
2006-07-24 20:53:57 +0000

    Added ogg metadata handler makefiles to allmakefiles.sh
    git-svn-id: http://publicsvn.songbirdnest.com/client/trunk@1414 5aad4ff5-4b58-4159-ba3f-0e6328df4cad

--- allmakefiles.sh
+++ allmakefiles.sh
@@ -177,6 +177,9 @@ components/mediacore/metadata/handler/Makefile
 components/mediacore/metadata/handler/id3/Makefile
 components/mediacore/metadata/handler/id3/public/Makefile
 components/mediacore/metadata/handler/id3/src/Makefile
+components/mediacore/metadata/handler/ogg/Makefile
+components/mediacore/metadata/handler/ogg/public/Makefile
+components/mediacore/metadata/handler/ogg/src/Makefile
 components/mediacore/metadata/manager/Makefile
 components/mediacore/metadata/manager/public/Makefile
 components/mediacore/metadata/manager/src/Makefile
