2ac8d23a08203db604072d8b77db234841ecda60
aus <aus@5aad4ff5-4b58-4159-ba3f-0e6328df4cad>
2006-07-25 00:58:56 +0000

    added the ability to specify an extra custom parameter to apply-patches.sh so you can reverse all the patches by doing 'patch patchdir mozilladir -R'
    git-svn-id: http://publicsvn.songbirdnest.com/client/trunk@1429 5aad4ff5-4b58-4159-ba3f-0e6328df4cad

--- tools/scripts/apply-patches.sh
+++ tools/scripts/apply-patches.sh
@@ -1,10 +1,11 @@
 if [ $# = 0 ]; then
-  echo usage: apply-patches patchdir targetdir
+  echo usage: apply-patches patchdir targetdir [-R]
   exit 1
 fi
 
 patchdir="$1"
 targetdir="$2"
+reverse="$3"
 
 list_files() {
   find . -maxdepth 1 -type f \
@@ -27,7 +28,7 @@ abspath="`cd \"$D\" 2>/dev/null && pwd || echo \"$D\"`/$B"
 for ((i=0; $i<$num_patches; i=$i+1)); do
   p=${patches[$i]}
   notice  "# applying $p #"
-  patch --backup-if-mismatch -p0 \
+  patch $reverse --backup-if-mismatch -p0 \
     -d "$targetdir" -i "$abspath/$p"
   echo ""
 done
