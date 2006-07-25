99685166c7bc7f2a8946e39e240df23a1407f8da
aus <aus@5aad4ff5-4b58-4159-ba3f-0e6328df4cad>
2006-07-25 19:58:27 +0000

    helper script to make the xulrunner.zip on win32 within cygwin.
    git-svn-id: http://publicsvn.songbirdnest.com/client/trunk@1474 5aad4ff5-4b58-4159-ba3f-0e6328df4cad

---
+++ tools/scripts/make-xulrunner-zip.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+notice() {
+  echo $* 1>&2
+}
+
+if [ $# != 3 ]; then
+  notice "usage: make-xulrunner-zip.sh [mozilla-bin-dir] [songbird-zip-dest-dir] [target-zip-name]"
+  exit 1
+fi
+
+srcdir="$1"
+temp1=`dirname "$srcdir"`
+temp2=`basename "$srcdir"`
+bindir="`cd \"$temp1\" 2>/dev/null && pwd || echo \"$temp1\"`/$temp2"
+
+destdir="$2"
+temp1=`dirname "$destdir"`
+temp2=`basename "$destdir"`
+zipdir="`cd \"$temp1\" 2>/dev/null && pwd || echo \"$temp1\"`/$temp2"
+
+zipname="$3"
+
+zipball="$zipdir/$zipname"
+
+echo cd "$bindir" 
+cd "$bindir" 
+
+rm -rf ../xulrunner
+mkdir ../xulrunner
+cp -a * ../xulrunner
+cd ..
+
+notice "creating zip in dest..."
+zip -r -9 -v $zipball xulrunner 
+
+notice "done."
