507cfb035d4a7333c752889a1b7e9655430976fa
ben <ben@5aad4ff5-4b58-4159-ba3f-0e6328df4cad>
2006-09-22 07:52:53 +0000

    Bug 1136 - "[Mac] Shoutcast pls fail to play". r=matt.
    git-svn-id: http://publicsvn.songbirdnest.com/client/trunk@2503 5aad4ff5-4b58-4159-ba3f-0e6328df4cad

--- components/playlistplayback/src/coreQT.js
+++ components/playlistplayback/src/coreQT.js
@@ -50,8 +50,12 @@ function CoreQT()
   this._waitRetryCount = 0;
   this._lastVolume = 0;
   this._muted = false;
-  this._starttime = 0;
+  this._starttime = null;
   this._playFromBeginning = true;
+  
+  // This is used for the bizarre case of loading streams within QT. See the
+  // later use in waitForPlayer().
+  this._doubleCheckInterval = null;
   
   this._pokingUrl = false;
   this._request = null;
@@ -70,27 +74,36 @@ CoreQT.prototype.constructor = CoreQT();
 
 CoreQT.prototype.cleanupOnError = function ()
 {
-  try {
-    //Probably an error :(
-    this._buffering = false;
-    this._playing = false;
-    this._paused = false;
-    
+  // Something bad happened so reset *everything* :(
+
+  this._buffering = false;
+  this._playing = false;
+  this._paused = false;
+  this._starttime = null;
+
+  if (this._waitForPlayInterval) {
     clearInterval(this._waitForPlayInterval);
     this._waitForPlayInterval = 0;
-    this._waitRetryCount = 0;
-    
-    //XXXAus: Don't ask me why I have to do this twice, I DO NOT KNOW! =P
-    this._sourcewindow.location.reload(false);
-    SetQTObject();
-    this._sourcewindow.location.reload(false);
-    SetQTObject();
-    
-    this.LOG("cleanupOnError!", "CoreQT");
-
-  } catch(e) {
-    dump(e);
   }
+  this._waitRetryCount = 0;
+
+  if (this._doubleCheckInterval) {
+    clearInterval(this._doubleCheckInterval);
+    this._doubleCheckInterval = null;
+  }
+
+  // XXXAus: Don't ask me why I have to do this twice, I DO NOT KNOW! =P
+  for (var index = 0; index < 2; index++) {
+    this._sourcewindow.location.reload(false);
+    try { 
+      SetQTObject();
+    }
+    catch (e) {
+      this.LOG(e, "CoreQT");
+    }
+  }
+
+  this.LOG("cleanupOnError!", "CoreQT");
 }
 
 CoreQT.prototype.waitForPlayer = function ()
@@ -101,6 +114,7 @@ CoreQT.prototype.waitForPlayer = function ()
   switch(playerStatus)
   {
     case "Waiting":
+      this._buffering = true;
       this._waitRetryCount++;
       if(this._waitRetryCount > 200) {
         this.cleanupOnError();
@@ -108,27 +122,49 @@ CoreQT.prototype.waitForPlayer = function ()
     break;
     
     case "Loading":
+      this._buffering = true;
     break;
     
     case "Playable":
       this.play();
       this._buffering = false;
+      // We've definitely got something, so clear the doubleCheck
+      if (this._doubleCheckInterval) {
+        clearInterval(this._doubleCheckInterval);
+        this._doubleCheckInterval = null;
+      }
     break;    
     
     case "Complete":
       this.LOG("Complete.", "CoreQT");
       this.play();
-      this._buffering = false;
       clearInterval(this._waitForPlayInterval);
       this._waitForPlayInterval = 0;
       this._waitRetryCount = 0;
+      this._buffering = false;
+      
+      // XXXben I know this seems dumb, but QT may try loading something else
+      // since the url could point to a stream playlist. We want to check again
+      // to catch that case. 500ms seems to be an okay wait value, but who
+      // knows what will happen with crazy network conditions...
+      if (!this._doubleCheckInterval)
+        this._doubleCheckInterval = setInterval("gQTCore.waitForPlayer();", 500);
+      else {
+        clearInterval(this._doubleCheckInterval);
+        this._doubleCheckInterval = false;
+      }
     break;
     
     default:
-      if(playerStatus == null || playerStatus.indexOf("Error") != -1)
-      {
+      // QT doesn't put all it's error conditions in an 'Error' category.
+      // Instead it likes to say things like "Complete: Connection Failed"! And
+      // unfortunately a list of possible codes isn't available so we just have
+      // to keep adding error conditions as we come across them.
+      if((playerStatus == null) ||
+         (playerStatus.indexOf("Error") != -1) ||
+         (playerStatus.indexOf("Failed") != -1) ||
+         (playerStatus.indexOf("Disconnected") != -1))
         this.cleanupOnError();
-      }
   }
   
   return;
@@ -136,22 +172,29 @@ CoreQT.prototype.waitForPlayer = function ()
 
 CoreQT.prototype.pokingUrlListener = function ( )
 {
-  if(gQTCore._request.readyState == 2)
-  {    
-    if(gQTCore._request.status == 200 &&
-       gQTCore._request.channel.contentLength > 4096 )
-    {
+  var request = gQTCore._request;
+  
+  if(request.readyState == 2) {
+    var status = request.status;
+    var channel = request.channel;
+    var contentLength = channel.contentLength;
+
+    // If contentLength is -1 that means that the size in unknown. This could
+    // be the case for streams. Go ahead and take a chance that it's a valid
+    // file.
+    if ((status == 200) && (contentLength != 0)) {
       gQTCore._availability = gQTCore.REMOTE_FILE_AVAILABLE;
       gQTCore.playURL(gQTCore._url);
     }
-    else
-	{
+    else {
       gQTCore._availability = gQTCore.FILE_NOT_AVAILABLE;
       gQTCore._buffering = false;
       gQTCore._playing = false;
-	}
+    }
+    
     gQTCore._pokingUrl = false;
-    gQTCore._request.abort();
+    request.abort();
+    gQTCore._request = null;
   }
 }
 
@@ -176,6 +219,7 @@ CoreQT.prototype.verifyFileAvailability = function ( aURL )
     this._request.onreadystatechange = gQTCore.pokingUrlListener;
     this._request.open("GET", aURL, true);
     this._request.send(null);
+    this._buffering = true;
     
     return this.NOT_LOCAL_FILE;
   }
@@ -224,8 +268,7 @@ CoreQT.prototype.verifyFileAvailability = function ( aURL )
 // Set the url and tell it to just play it.  Eventually this talks to the media library object to make a temp playlist.
 CoreQT.prototype.playURL = function ( aURL )
 {
-  if (this._buffering &&
-	  this._pokingUrl == false)
+  if (this._buffering && !this._pokingUrl)
     this.cleanupOnError();
 
   this._verifyObject();
@@ -233,44 +276,43 @@ CoreQT.prototype.playURL = function ( aURL )
   if (!aURL)
     throw Components.results.NS_ERROR_INVALID_ARG;
 
-  var isAvailable = this.FILE_NOT_AVAILABLE;
-  
-  if(this._availability == this.REMOTE_FILE_AVAILABLE)
-    isAvailable = this._availability;
-  else
-    isAvailable = this.verifyFileAvailability(aURL);
+  var isAvailable = this._availability == this.REMOTE_FILE_AVAILABLE ?
+                    this.REMOTE_FILE_AVAILABLE :
+                    this.verifyFileAvailability(aURL);
 
   this._url = this.sanitizeURL(aURL);
   this._paused = false;
   this._playing = true;
   this._buffering = true;  
-  
+    
   if(isAvailable == this.FILE_NOT_AVAILABLE)
     throw new Error("Local file not available");
 
-  if(isAvailable == this.REMOTE_FILE_AVAILABLE ||
-     isAvailable == this.NOT_LOCAL_FILE)
-  {
-    this._isremote = true;
-
-    if(isAvailable == this.NOT_LOCAL_FILE)
-      return true;
+  switch (isAvailable) {
+    case this.NOT_LOCAL_FILE:
+      this._isremote = true;
+      // Wait for the XmlHttpRequest to get back to us
+      return;
+    case this.REMOTE_FILE_AVAILABLE:
+      this._isremote = true;
+      break;
+    default:
+      this._isremote = false;
   }
-  else
-  {
-     this._isremote = false;
-  }
 
-  this._availability = 0;
+  this._availability = FILE_NOT_AVAILABLE;
+  this._starttime = new Date();
   this._object.SetURL(this._url);
   this.setVolume(this._lastVolume);
   
-  if(this._waitForPlayInterval)
-  {
+  if (this._waitForPlayInterval) {
     clearInterval(this._waitForPlayInterval);
     this._waitForPlayInterval = 0;
   }
-  
+  if (this._doubleCheckInterval) {
+    clearInterval(this._doubleCheckInterval);
+    this._doubleCheckInterval = null;
+  }
   this._waitForPlayInterval = setInterval("gQTCore.waitForPlayer();", 333);
 
   return true;
@@ -294,6 +336,8 @@ CoreQT.prototype.play = function ()
 
   this._paused = false;
   this._playing = true;
+  if (!this._starttime)
+    this._starttime = new Date();
 
   return true;
 };
@@ -308,8 +352,9 @@ CoreQT.prototype.pause = function ()
     this.LOG(e, "CoreQT");
   }
   
-  this._paused = true;
-  return this._paused;
+  this._starttime = null;
+  
+  return this._paused = true;
 };
   
 CoreQT.prototype.stop = function ()
@@ -333,6 +378,7 @@ CoreQT.prototype.stop = function ()
 
   this._paused = false;
   this._playing = false;
+  this._starttime = null;
 
   return true;
 };
@@ -344,15 +390,13 @@ CoreQT.prototype.getPlaying = function ()
   if(this._buffering)
     return true;
 
-  // XXXben - getPosition and getLength both return 0 when we're dealing with
-  //          a stream in QT, so don't stop if this is a stream.
+  // XXXben - getLength returns 0 when we're dealing with a stream in QT, so
+  //          don't stop if this is a stream.
   var length = this.getLength();
   var position = this.getPosition();
-
-  if ((position > 0) && (length == position))
-  {
+  if ((length > 0) && (length == position))
     this.stop();
-  }
+
   return this._playing;
 };
   
@@ -371,7 +415,6 @@ CoreQT.prototype.getLength = function ()
     return 1;
   
   try {
- 
     var timeScale = this._object.GetTimeScale();
     var duration = this._object.GetDuration();
   
@@ -380,7 +423,8 @@ CoreQT.prototype.getLength = function ()
     if(playLength > 3579139410)
       playLength = 0;
 
-  } catch(e) {
+  }
+  catch (e) {
     this.LOG(e, "CoreQT");
   }
  
@@ -396,16 +440,25 @@ CoreQT.prototype.getPosition = function ()
     return 0;
 
   try {
-    var currentPos = this._object.GetTime();
-    var timeScale = this._object.GetTimeScale();
-    curPos = (currentPos / timeScale * 1000);
-    
-    if(!currentPos && this._isremote) {
-      var now = new Date();
-      return now.getTime() - this._starttime.getTime();
+    var qtTime = this._object.GetTime();
+    var qtTimeScale = this._object.GetTimeScale();
+    curPos = (qtTime / qtTimeScale * 1000);
+  }
+  catch (e) {
+    this.LOG(e, "CoreQT");    
+  }
+  
+  if (!curPos && this._isremote) {
+    // We have to fake a value here to keep PlaylistPlayback happy for streamed
+    // files (shoutcast, etc.). See bug 1136.
+    if (this._starttime) {
+      var startTime = this._starttime.getTime();
+      var date = new Date();
+      var currentTime = date.getTime();
+      curPos = currentTime - startTime;
     }
-  } catch(e) {
-    this.LOG(e, "CoreQT");
+    else
+      curPos = 0;
   }
   
   return curPos;
@@ -534,8 +587,14 @@ catch (err) {
 
 function SetQTObject()
 {
-  var theDocumentQTInstance = document.getElementById( coreQTDocumentElementIdentifier );
+  var theDocumentQTInstance =
+    document.getElementById(coreQTDocumentElementIdentifier);
+  if (!theDocumentQTInstance)
+    throw new Error("Couldn't get the QT element from the document!");
+
   var theQTInstance = theDocumentQTInstance.contentWindow;
+  if (!theQTInstance)
+    throw new Error("Couldn't get QT's contentWindow");
       
   gQTCore.setId("QT1");
   gQTCore.setObject(theQTInstance);
