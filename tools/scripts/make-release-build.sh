b5f0da98a5ae7c9c4718e9e35eb97b52f3d41553
aus <aus@5aad4ff5-4b58-4159-ba3f-0e6328df4cad>
2006-09-27 03:06:11 +0000

    release build script.
    git-svn-id: http://publicsvn.songbirdnest.com/client/trunk@2556 5aad4ff5-4b58-4159-ba3f-0e6328df4cad

---
+++ tools/scripts/make-release-build.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+notice() {
+  echo $* 1>&2
+}
+
+DEPTH=../..
+CURRENT_DIR=`pwd`
+
+export SONGBIRD_OFFICIAL=1
+export UPDATE_CHANNEL="release"
+export SB_ENABLE_JARS=1
+export SB_ENABLE_INSTALLER=1
+
+cd ${DEPTH}
+make -f songbird.mk clobber
+make -f songbird.mk
+make -C compiled/update complete
+
+unset SONGBIRD_OFFICIAL
+unset UPDATE_CHANNEL
+unset SB_ENABLE_JARS
+unset SB_ENABLE_INSTALLER
+
+notice
+notice "Done. MAR and snippet are in compiled/update."
+notice "Please copy these files to a safe location!"
+notice "They are required to generate the update for the next build."
+notice
