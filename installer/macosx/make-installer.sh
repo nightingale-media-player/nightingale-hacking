2eaf562335d2049271d809937aed4fb1a0833678
matt <matt@5aad4ff5-4b58-4159-ba3f-0e6328df4cad>
2006-10-18 01:32:35 +0000

    added ability to specify a disk icon to make-installer.sh
    git-svn-id: http://publicsvn.songbirdnest.com/client/trunk@2717 5aad4ff5-4b58-4159-ba3f-0e6328df4cad

--- installer/macosx/make-installer.sh
+++ installer/macosx/make-installer.sh
@@ -12,4 +12,4 @@ cp ${DEPTH}/installer/macosx/LICENSE.txt ${DEPTH}/compiled/dist/Songbird.app/
 cp ${DEPTH}/installer/macosx/TRADEMARK.txt ${DEPTH}/compiled/dist/Songbird.app/
 ln -s /Applications/ ${DEPTH}/compiled/dist/Applications
 
-${DEPTH}/installer/macosx/make-diskimage ${DEPTH}/_built_installer/Songbird_${CURRENT_DATE}_${ARCH}.dmg ${DEPTH}/compiled/dist Songbird -null- ${DEPTH}/installer/macosx/songbird.dsstore ${DEPTH}/installer/macosx/background.tiff
+${DEPTH}/installer/macosx/make-diskimage ${DEPTH}/_built_installer/Songbird_${CURRENT_DATE}_${ARCH}.dmg ${DEPTH}/compiled/dist Songbird -null- ${DEPTH}/installer/macosx/songbird.dsstore ${DEPTH}/installer/macosx/background.tiff ${DEPTH}/installer/macosx/MacSongbirdDiskImage.icns 
