4b826bd8b5995a73c7c4f1974b059747b4eea8ea
aus <aus@5aad4ff5-4b58-4159-ba3f-0e6328df4cad>
2007-01-18 03:15:17 +0000

    turns out mozilla sets winver to 0x500 now instead of 0x400, adjusted our script so it sets looks for 0x500 and replaces it with 0x501 so integration and usb device compiles properly.
    git-svn-id: http://publicsvn.songbirdnest.com/client/trunk@3069 5aad4ff5-4b58-4159-ba3f-0e6328df4cad

--- tools/scripts/make-mozilla-sdk.sh
+++ tools/scripts/make-mozilla-sdk.sh
@@ -109,7 +109,7 @@ cd "$srcdir/tools/update-packaging" && cp -Lfp $update_script_files "$sdkdir/scr
 notice "performing post-processing..."
 
 # bump WINVER and _WIN32_WINNT to 0x501 on windows
-cd "$sdkdir/include" && perl -pi -e 's/WINVER 0x400/WINVER 0x501/g;s/_WIN32_WINNT 0x400/_WIN32_WINNT 0x501/g' ./mozilla-config.h
+cd "$sdkdir/include" && perl -pi -e 's/WINVER 0x500/WINVER 0x501/g;s/_WIN32_WINNT 0x500/_WIN32_WINNT 0x501/g' ./mozilla-config.h
 cd "$sdkdir/include" && rm -f ./mozilla-config.h.bak
 
 notice "done."
