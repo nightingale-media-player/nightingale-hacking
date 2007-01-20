8ec736196d95652d120a70b954e94f61bc83786b
matt <matt@5aad4ff5-4b58-4159-ba3f-0e6328df4cad>
2007-01-20 01:46:51 +0000

    copying vlc build scripts from branch to trunk
    git-svn-id: http://publicsvn.songbirdnest.com/client/trunk@3110 5aad4ff5-4b58-4159-ba3f-0e6328df4cad

---
+++ tools/scripts/make-vlc-mac.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+notice() {
+  echo $* 1>&2
+}
+
+if [ $# != 1 ]; then
+  notice "Configures and builds VLC from /builds/vlc/ and copies it into dependencies/"
+  notice "Note:  Expects to be run from tools/scripts/"
+  notice "Usage: make-vlc-mac.sh [debug|release]"
+  exit 1
+fi
+
+pwd=`pwd`
+cd ../../
+base=`pwd`
+pluginroot="$base/dependencies/macosx/plugins/vlc"
+scripts="$base/tools/scripts"
+
+echo "Building /builds/vlc and copying to $pluginroot/$1/"
+
+cd /builds/vlc/
+
+make clean
+$scripts/configure-vlc-mac-$1.sh
+make
+$scripts/make-vlc-mac-tarball.sh $pluginroot/$1/
+
+cd $pwd
