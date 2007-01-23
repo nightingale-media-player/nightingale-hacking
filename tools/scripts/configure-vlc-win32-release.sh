7d350b9379726515205a7be03109aa7d2ea976a5
matt <matt@5aad4ff5-4b58-4159-ba3f-0e6328df4cad>
2007-01-23 00:19:47 +0000

    copying vlc configure scripts to trunk
    git-svn-id: http://publicsvn.songbirdnest.com/client/trunk@3128 5aad4ff5-4b58-4159-ba3f-0e6328df4cad

---
+++ tools/scripts/configure-vlc-win32-release.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+#./bootstrap && \
+  CPPFLAGS="-I/usr/win32/include -I/usr/win32/include/ebml" \
+  LDFLAGS=-L/usr/win32/lib \
+  PKG_CONFIG_PATH=/usr/win32/lib/pkgconfig \
+  CC="gcc -mno-cygwin" CXX="g++ -mno-cygwin" \
+  ./configure \
+      --enable-sdl --with-sdl-config-path=/usr/win32/bin --disable-gtk \
+      --disable-activex \
+      --enable-nls \
+      --enable-ffmpeg --with-ffmpeg-mp3lame --with-ffmpeg-faac \
+      --with-ffmpeg-zlib --enable-faad --enable-flac --enable-theora \
+      --with-wx-config-path=/usr/win32/bin \
+      --with-freetype-config-path=/usr/win32/bin \
+      --with-fribidi-config-path=/usr/win32/bin \
+      --enable-live555 --with-live555-tree=/usr/win32/live.com \
+      --enable-caca --with-caca-config-path=/usr/win32/bin \
+      --with-xml2-config-path=/usr/win32/bin \
+      --with-dvdnav-config-path=/usr/win32/bin \
+      --disable-cddax --disable-vcdx --enable-goom \
+      --enable-twolame --disable-dvdread \
+      --disable-gnomevfs \
+      --disable-dts \
+      --enable-mozilla \
+      --with-mozilla-sdk-path=/usr/win32/gecko-sdk \
+      --enable-release \
+      --disable-debug \
