b40e3fa6ffec11ce2c631c3c7d000e45846ee50b
lone <lone@5aad4ff5-4b58-4159-ba3f-0e6328df4cad>
2007-02-07 23:47:14 +0000

    missing cmdline.js
    git-svn-id: http://publicsvn.songbirdnest.com/client/trunk@3323 5aad4ff5-4b58-4159-ba3f-0e6328df4cad

---
+++ app/content/scripts/cmdline.js
@@ -0,0 +1,93 @@
+try 
+{
+
+  // Module specific global for auto-init/deinit support
+  var commandline_module = {};
+  commandline_module.init_once = 0;
+  commandline_module.deinit_once = 0;
+
+  commandline_module.onLoad = function()
+  {
+    if (commandline_module.init_once++) { dump("WARNING: commandline_module double init!!\n"); return; }
+    commandLineItemHandler.init();
+  }
+
+  commandline_module.onUnload = function()
+  {
+    if (commandline_module.deinit_once++) { dump("WARNING: commandline_module double deinit!!\n"); return; }
+    commandLineItemHandler.shutdown();
+    window.removeEventListener("load", commandline_module.onLoad, false);
+    window.removeEventListener("unload", commandline_module.onUnload, false);
+  }
+
+  var commandLineItemHandler = {
+    cmdline_items: null,
+    
+    init: function() {
+      var cmdline = Components.classes["@songbirdnest.com/commandlinehandler/general-startup;1?type=songbird"];
+      if (cmdline) {
+        var cmdline_service = cmdline.getService(Components.interfaces.nsICommandLineHandler);
+        if (cmdline_service) {
+          cmdline_items = cmdline_service.QueryInterface(Components.interfaces.sbICommandLineItems);
+          if (cmdline_items)
+            cmdline_items.addItemHandler(this);
+        }
+      }
+    },
+    
+    shutdown: function() {
+      if (cmdline_items) cmdline_items.removeItemHandler(this);
+    },
+    
+    handleItem: function(aUriSpec, aCount, aTotal) {
+      if (aUriSpec.toLowerCase().indexOf("http:") == 0 ||
+          aUriSpec.toLowerCase().indexOf("https:") == 0) {
+        // lone> this should open tabs if necessary, rather than override the last url that might have been loaded on the previous handleItem call
+        var theServiceTree = document.getElementById( 'frame_servicetree' );
+        if (!theServiceTree.doneInit) {
+          theDefaultUrlOverride = aUriSpec; 
+        } else {
+          theServiceTree.launchURL( aUriSpec );
+        }
+      } else {
+        if (!_drop_filelist) 
+          _drop_filelist = Array();
+        
+        if (isXPI(aUriSpec)) 
+          installXPI(aUriSpec);
+        else 
+          _drop_filelist.push(aUriSpec);
+          
+        if (aCount == aTotal-1) {
+          // if we are already importing, our items will be imported automatically, otherwise, start the drop handler
+          if (!importingDrop) {
+            firstDrop = true;
+            importingDrop = true;
+            setTimeout( SBDropped, 10 ); // Next frame
+            importingDrop = false;
+          }
+        }
+      }
+      return true;
+    },
+    
+    QueryInterface : function(aIID) {
+      if (!aIID.equals(Components.interfaces.sbICommandLineItemHandler) &&
+          !aIID.equals(Components.interfaces.nsISupports)) 
+      {
+        throw Components.results.NS_ERROR_NO_INTERFACE;
+      }
+      return this;
+    }
+  };
+
+  // Auto-init/deinit registration
+  window.addEventListener("load", commandline_module.onLoad, false);
+  window.addEventListener("unload", commandline_module.onUnload, false);
+}
+catch(e)
+{
+  dump("cmdline.js - " + e + "\n");
+}
+
+
