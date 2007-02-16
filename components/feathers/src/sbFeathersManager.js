2ff202988e64aa2737087c80141cc6e7b37e8bc5
matt <matt@5aad4ff5-4b58-4159-ba3f-0e6328df4cad>
2007-02-16 21:44:34 +0000

    Bug 2428 - First pass implementation and test cases for FeathersManager.
    git-svn-id: http://publicsvn.songbirdnest.com/client/trunk@3465 5aad4ff5-4b58-4159-ba3f-0e6328df4cad

---
+++ components/feathers/src/sbFeathersManager.js
@@ -0,0 +1,446 @@
+/**
+//
+// BEGIN SONGBIRD GPL
+// 
+// This file is part of the Songbird web player.
+//
+// Copyright(c) 2005-2007 POTI, Inc.
+// http://songbirdnest.com
+// 
+// This file may be licensed under the terms of of the
+// GNU General Public License Version 2 (the "GPL").
+// 
+// Software distributed under the License is distributed 
+// on an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either 
+// express or implied. See the GPL for the specific language 
+// governing rights and limitations.
+//
+// You should have received a copy of the GPL along with this 
+// program. If not, go to http://www.gnu.org/licenses/gpl.html
+// or write to the Free Software Foundation, Inc., 
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+// 
+// END SONGBIRD GPL
+//
+ */
+ 
+/**
+ * \file sbFeathersManager.js
+ * \brief Coordinates the loading of feathers (combination of skin and XUL window layout)
+ */ 
+ 
+ 
+//
+// TODO:
+//  * Add doxygen comments
+//  * Make skin switching actually do something
+//  * Add function arg assertions
+// 
+ 
+const CONTRACTID = "@songbirdnest.com/songbird/feathersmanager;1";
+const CLASSNAME = "Songbird Feathers Manager Service Interface";
+const CID = Components.ID("{99f24350-a67f-11db-befa-0800200c9a66}");
+const IID = Components.interfaces.sbIFeathersManager;
+
+var gOS = null;
+
+
+/**
+ * /class ArrayEnumerator
+ * /brief Converts a js array into an nsISimpleEnumerator
+ */
+function ArrayEnumerator(array) 
+{
+  this.data = array;
+}
+ArrayEnumerator.prototype = {
+
+  index: 0,
+  
+  getNext: function() {
+    return this.data[this.index++];
+  },
+  
+  hasMoreElements: function() {            
+    if (this.index < this.data.length)
+      return true;
+    else
+      return false;
+  },
+  
+  QueryInterface: function(iid)
+  {
+    if (!iid.equals(Components.interfaces.nsISimpleEnumerator))
+      throw Components.results.NS_ERROR_NO_INTERFACE;
+    return this;
+  }
+}
+
+
+
+
+/**
+ * /class FeathersManager
+ * /brief Coordinates the loading of feathers
+ *
+ * Acts as a registry for skins and layout (known as feathers)
+ * and manages compatibility and selection.
+ */
+function FeathersManager() {
+  try {
+    gOS      = Components.classes["@mozilla.org/observer-service;1"]
+                        .getService(Components.interfaces.nsIObserverService);
+    if (gOS.addObserver) {
+      // We should wait until the profile has been loaded to start
+      gOS.addObserver(this, "profile-after-change", false);
+      // We need to unhook things on shutdown
+      gOS.addObserver(this, "xpcom-shutdown", false);
+    }
+  } catch (e) { }
+  
+  
+  this._skins = {};
+  this._layouts = {};
+  this._mappings = {};
+  this._listeners = [];
+};
+FeathersManager.prototype.constructor = FeathersManager;
+FeathersManager.prototype = {
+  
+  _currentSkin: null,
+  _currentLayout: null,
+  
+  // Hash of skin descriptions keyed by provider (e.g. classic/1.0)
+  _skins: null,
+  
+  // Hash of layout descriptions keyed by URL
+  _layouts: null,
+  
+  
+  // Hash of layout URL to hash of compatible skin providers, pointing to 
+  // showChrome value.  
+  //
+  // eg
+  // {  
+  //     mainwin.xul : {
+  //       blueskin: true,
+  //       redskin: false,
+  //     }
+  // }
+  //
+  // Compatibility is determined by whether or not a skinProvider
+  // key is *defined* in the hash, not the actual value it points to.
+  // In the above example false means "don't show chrome"
+  _mappings: null,
+  
+  
+  // Array of sbIFeathersChangeListeners
+  _listeners: null,
+
+  _layoutCount: 0,
+  _skinCount: 0,
+  
+
+  _init: function FeathersManager_init() {
+    // Hmm, don't care?
+  },
+  
+  _deinit: function FeathersManager_deinit() {
+      this._skins = {}
+      this._layouts = {};
+      this._mappings = {};
+      this._listeners = [];
+  },
+  
+  
+  
+  get currentSkin() {
+    return this._currentSkin;
+  },
+  
+  get currentLayout() {
+    return this._currentLayout;
+  },
+  
+  get skinCount() {
+    return this._skinCount;
+  },
+  
+  get layoutCount() {
+    return this._layoutCount;
+  },
+
+  getSkinDescriptions: function FeathersManager_getSkinDescriptions() {
+    // Copy all the descriptions into an array, and then return an enumerator
+    return new ArrayEnumerator( [this._skins[key] for (key in this._skins)] );
+  },
+
+  getLayoutDescriptions: function FeathersManager_getLayoutDescriptions() {
+    // Copy all the descriptions into an array, and then return an enumerator
+    return new ArrayEnumerator( [this._layouts[key] for (key in this._layouts)] );
+  },
+  
+  
+  
+  registerSkin: function FeathersManager_registerSkin(skinDesc) {
+    // TODO: Verify desc
+    
+    if (this._skins[skinDesc.provider] == null) {
+      this._skinCount++;
+    }
+    this._skins[skinDesc.provider] = skinDesc;
+    
+    // Notify observers
+    this._onUpdate();
+  },
+
+  unregisterSkin: function FeathersManager_unregisterSkin(skinDesc) {
+    if (this._skins[skinDesc.provider]) {
+      delete this._skins[skinDesc.provider];
+      this._skinCount--;
+    }
+
+    // Notify observers
+    this._onUpdate();
+  },
+
+  getSkinDescription: function FeathersManager_getSkinDescription(provider) {
+    return this._skins[provider];
+  },
+  
+  
+  
+  registerLayout: function FeathersManager_registerLayout(layoutDesc) {
+    // TODO: Verify desc
+   
+    if (this._layouts[layoutDesc.url] == null) {
+      this._layoutCount++;
+    }
+    this._layouts[layoutDesc.url] = layoutDesc;
+    
+    // Notify observers
+    this._onUpdate();
+  },
+
+  unregisterLayout: function FeathersManager_unregisterLayout(layoutDesc) {
+    if (this._layouts[layoutDesc.url]) {
+      delete this._layouts[layoutDesc.url];
+      this._layoutCount--;
+    }
+    
+    // Notify observers
+    this._onUpdate();    
+  },
+    
+  getLayoutDescription: function FeathersManager_getLayoutDescription(url) {
+    return this._layouts[url];
+  }, 
+  
+
+
+  addMapping: function FeathersManager_addMapping(layoutURL, skinProvider, showChrome) {
+    // TODO verify
+    
+    if (this._mappings[layoutURL] == null) {
+      this._mappings[layoutURL] = {};
+    }
+    this._mappings[layoutURL][skinProvider] = showChrome;
+    
+    // Notify observers
+    this._onUpdate();    
+  },
+
+  removeMapping: function FeathersManager_removeMapping(layoutURL, skinProvider) {
+    if (this._mappings[layoutURL]) {
+      delete this._mappings[layoutURL][skinProvider];
+    }
+
+    // Notify observers
+    this._onUpdate();    
+  },
+  
+
+
+  shouldShowChrome: function FeathersManager_shouldShowChrome(layoutURL, skinProvider) {
+    if (this._mappings[layoutURL]) {
+      return this._mappings[layoutURL][skinProvider] == true;
+    }
+   
+    return false; 
+  },
+
+
+
+  getSkinsForLayout: function FeathersManager_getSkinsForLayout(layoutURL) {
+    var skins = [];
+    
+    // Find skin descriptions that are compatible with the given layout.
+    if (this._mappings[layoutURL]) {
+      for (skinProvider in this._mappings[layoutURL]) {
+        var desc = this.getSkinDescription(skinProvider);
+        if (desc) {
+          skins.push(desc);
+        }
+      }
+    }   
+    return new ArrayEnumerator( skins );
+  },
+
+
+
+  select: function FeathersManager_select(layoutURL, skinProvider) {
+    layoutDescription = this.getLayoutDescription(layoutURL);
+    skinDescription = this.getSkinDescription(skinProvider);
+    
+    // Make sure we know about the requested skin and layout
+    if (layoutDescription == null || skinDescription == null) {
+      return false; // Should this be an exception?
+    }
+    
+    // Check compatibility.
+    // True/false refer to the showChrome value, so check for undefined
+    // to determine compatibility.
+    if (this._mappings[layoutURL][skinProvider] === undefined) {
+      return false;
+    } 
+    
+    // Notify that a select is about to occur
+    this._onSelect(layoutDescription, skinDescription);
+    
+    // TODO!
+    // Can we get away with killing the main window, or do we have to completely restart?
+    // Modify dataremotes and keep a copy of the previous values.
+    
+    return true;
+  },
+
+
+
+  addChangeListener: function FeathersManager_addChangeListener(listener) {
+    this._listeners.push(listener);
+  },
+  
+  
+  removeChangeListener: function FeathersManager_removeChangeListener(listener) {
+    var index = this._listeners.indexOf(listener);
+    if (index > -1) {
+      this._listeners.splice(index,1);
+    }
+  },
+
+
+  _onUpdate: function FeathersManager_onUpdate() {
+    this._listeners.forEach( function (listener) {
+      listener.onFeathersUpdate();
+    });
+  },
+
+
+  _onSelect: function FeathersManager_onSelect(layoutDesc, skinDesc) {
+    // Verify args
+    layoutDesc = layoutDesc.QueryInterface(Components.interfaces.sbILayoutDescription);
+    skinDesc = skinDesc.QueryInterface(Components.interfaces.sbISkinDescription);
+    
+    // Broadcast notification
+    this._listeners.forEach( function (listener) {
+      listener.onFeathersSelectRequest(layoutDesc, skinDesc);
+    });
+  },
+
+      
+
+  // watch for XRE startup and shutdown messages 
+  observe: function(subject, topic, data) {
+    switch (topic) {
+    case "profile-after-change":
+      gOS.removeObserver(this, "profile-after-change");
+      
+      // Preferences are initialized, ready to start the service
+      this._init();
+      break;
+    case "xpcom-shutdown":
+      gOS.removeObserver(this, "xpcom-shutdown");
+      this._deinit();
+      
+      gOS  = null;
+      break;
+    }
+  },
+
+  /**
+   * See nsISupports.idl
+   */
+  QueryInterface: function(iid) {
+    if (!iid.equals(IID) &&
+        !iid.equals(Components.interfaces.nsIObserver) && 
+        !iid.equals(Components.interfaces.nsISupportsWeakReference) &&
+        !iid.equals(Components.interfaces.nsISupports))
+      throw Components.results.NS_ERROR_NO_INTERFACE;
+    return this;
+  }
+}; // FeathersManager.prototype
+
+
+
+
+
+/**
+ * ----------------------------------------------------------------------------
+ * Registration for XPCOM
+ * ----------------------------------------------------------------------------
+ */
+var gModule = {
+  registerSelf: function(componentManager, fileSpec, location, type) {
+    componentManager = componentManager.QueryInterface(Components.interfaces.nsIComponentRegistrar);
+    for (var key in this._objects) {
+      var obj = this._objects[key];
+      componentManager.registerFactoryLocation(obj.CID, obj.className, obj.contractID,
+                                               fileSpec, location, type);
+    }
+    var categoryManager = Components.classes["@mozilla.org/categorymanager;1"]
+                                    .getService(Components.interfaces.nsICategoryManager);
+    categoryManager.addCategoryEntry("app-startup", this._objects.feathersmanager.className,
+                                    "service," + this._objects.feathersmanager.contractID, 
+                                    true, true, null);
+  },
+
+  getClassObject: function(componentManager, cid, iid) {
+    if (!iid.equals(Components.interfaces.nsIFactory))
+      throw Components.results.NS_ERROR_NOT_IMPLEMENTED;
+
+    for (var key in this._objects) {
+      if (cid.equals(this._objects[key].CID))
+        return this._objects[key].factory;
+    }
+    
+    throw Components.results.NS_ERROR_NO_INTERFACE;
+  },
+
+  _makeFactory: #1= function(ctor) {
+    function ci(outer, iid) {
+      if (outer != null)
+        throw Components.results.NS_ERROR_NO_AGGREGATION;
+      return (new ctor()).QueryInterface(iid);
+    } 
+    return { createInstance: ci };
+  },
+  
+  _objects: {
+    // The FeathersManager Component
+    feathersmanager:     { CID        : CID,
+                                 contractID : CONTRACTID,
+                                 className  : CLASSNAME,
+                                 factory    : #1#(FeathersManager)
+                               },
+  },
+
+  canUnload: function(componentManager) { 
+    return true; 
+  }
+}; // gModule
+
+function NSGetModule(comMgr, fileSpec) {
+  return gModule;
+} // NSGetModule
+
+
---
+++ components/feathers/test/test_feathersManager.js
@@ -0,0 +1,290 @@
+/*
+//
+// BEGIN SONGBIRD GPL
+//
+// This file is part of the Songbird web player.
+//
+// Copyright(c) 2005-2007 POTI, Inc.
+// http://songbirdnest.com
+//
+// This file may be licensed under the terms of of the
+// GNU General Public License Version 2 (the "GPL").
+//
+// Software distributed under the License is distributed
+// on an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either
+// express or implied. See the GPL for the specific language
+// governing rights and limitations.
+//
+// You should have received a copy of the GPL along with this
+// program. If not, go to http://www.gnu.org/licenses/gpl.html
+// or write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+//
+// END SONGBIRD GPL
+//
+*/
+
+/**
+ * \brief FeathersManager test file
+ */
+
+
+var feathersManager =  Components.classes['@songbirdnest.com/songbird/feathersmanager;1']
+                                 .getService(Components.interfaces.sbIFeathersManager);
+
+// List of skin descriptions used in the test cases
+var skins = [];
+
+// List of layout descriptions used in the test cases
+var layouts = [];
+
+
+
+/**
+ * Generic description object that works for skins and layouts
+ */
+function FeathersDescription() {
+  this.wrappedJSObject = this;
+};
+FeathersDescription.prototype = {
+  QueryInterface: function(iid) {
+    if (!iid.equals(Components.interfaces.sbISkinDescription) &&
+        !iid.equals(Components.interfaces.sbILayoutDescription)) 
+      throw Components.results.NS_ERROR_NO_INTERFACE;
+    return this;
+  }
+};
+
+
+/**
+ * sbIFeathersChangeListener for testing callback
+ * support in FeathersManager
+ */
+var feathersChangeListener = {
+  
+  // Count update notifications so that they can be confirmed
+  // with an assert.
+  updateCounter: 0,
+  onFeathersUpdate: function() {
+    this.updateCounter++;
+  },
+  
+  // Set skin and layout to be expected on next select,
+  // and reset when received.
+  expectLayout: null,
+  expectSkin: null,
+  onFeathersSelectRequest: function(layoutDesc, skinDesc) {
+    assertEqual(layoutDesc.wrappedJSObject, this.expectLayout);
+    assertEqual(skinDesc.wrappedJSObject, this.expectSkin);
+    this.expectLayout = null;
+    this.expectSkin = null;
+  },
+  
+  QueryInterface: function(iid) {
+    if (!iid.equals(Components.interfaces.sbIFeathersChangeListener)) 
+      throw Components.results.NS_ERROR_NO_INTERFACE;
+    return this;
+  }
+}
+
+
+/** 
+ * Turn an nsISimpleEnumerator into a generator
+ */ 
+function wrapEnumerator(enumerator, iface)
+{
+  while (enumerator.hasMoreElements()) {
+    yield enumerator.getNext().QueryInterface(iface);
+  }
+}
+
+
+/**
+ * Make sure that the given enumerator contains all objects in 
+ * the given list and nothing else.
+ */
+function assertEnumeratorEqualsArray(enumerator, list) {
+   list = list.concat([]); // Clone list before modifying 
+   for (var item in enumerator) {
+    assertEqual(list.indexOf(item.wrappedJSObject) > -1, true);
+    list.splice(list.indexOf(item.wrappedJSObject), 1);
+  }
+  assertEqual(list.length, 0);
+}
+
+
+
+
+/** 
+ * Populate the feathers manager
+ */
+function setup()
+{
+  // Register for change callbacks
+  feathersManager.addChangeListener(feathersChangeListener);
+
+  // Make some skins
+  var skin = new FeathersDescription();
+  skin.name = "Blue Skin";
+  skin.provider = "blue/1.0";
+  skins.push(skin);
+  feathersManager.registerSkin(skin);
+
+  skin = new FeathersDescription();
+  skin.name = "Red Skin";
+  skin.provider = "red/1.0";
+  skins.push(skin);
+  feathersManager.registerSkin(skin);
+
+  skin = new FeathersDescription();
+  skin.name = "Orange Skin";
+  skin.provider = "orange/1.0";
+  skins.push(skin);
+  feathersManager.registerSkin(skin);
+  
+  // Make some layouts
+  var layout = new FeathersDescription();
+  layout.name = "Big Layout";
+  layout.url = "chrome://biglayout/content/mainwin.xul";
+  layouts.push(layout);
+  feathersManager.registerLayout(layout);
+  
+  layout = new FeathersDescription();
+  layout.name = "Mini Layout";
+  layout.url = "chrome://minilayout/content/mini.xul";
+  layouts.push(layout);
+  feathersManager.registerLayout(layout);
+  
+  // Create some mappings
+  // Blue -> big, Red -> big, Orange -> mini with chrome
+  feathersManager.addMapping(layouts[1].url, skins[0].provider, false);
+  feathersManager.addMapping(layouts[1].url, skins[1].provider, false);
+  feathersManager.addMapping(layouts[0].url, skins[2].provider, true);
+}
+
+
+/**
+ * Remove everything from the feathers manager
+ */
+function teardown() {
+  skins.forEach(function(skin) { 
+    feathersManager.unregisterSkin(skin);
+  });
+
+  layouts.forEach(function(layout) { 
+    feathersManager.unregisterLayout(layout);
+  });
+
+  // Remove mappings
+  // Blue -> big, Red -> big, Orange -> mini 
+  feathersManager.removeMapping(layouts[1].url, skins[0].provider);
+  feathersManager.removeMapping(layouts[1].url, skins[1].provider);
+  
+  // Remove change listener before final modification to confirm
+  // that it actually gets unhooked.
+  feathersManager.removeChangeListener(feathersChangeListener);
+  
+  feathersManager.removeMapping(layouts[0].url, skins[2].provider);
+}
+
+
+/**
+ * Test all functionality in sbFeathersManager
+ */
+function runTest () {
+  setup();
+  
+  // ------------------------
+  // Test change notification.  We should have seen 8 update notifications
+  assertEqual(feathersChangeListener.updateCounter, 8);
+  
+  // ------------------------
+  // Test Getters / Make sure setup succeeded
+  
+  // Verify all skins added properly
+  assertEqual(feathersManager.skinCount, skins.length);
+  var enumerator = wrapEnumerator(feathersManager.getSkinDescriptions(),
+                     Components.interfaces.sbISkinDescription);
+  assertEnumeratorEqualsArray(enumerator, skins);
+  
+  // Verify all layouts added properly
+  assertEqual(feathersManager.layoutCount, layouts.length);
+  var enumerator = wrapEnumerator(feathersManager.getLayoutDescriptions(), 
+                     Components.interfaces.sbILayoutDescription);
+  assertEnumeratorEqualsArray(enumerator, layouts);
+
+  // Test description getters
+  var desc = feathersManager.getSkinDescription(skins[1].provider).wrappedJSObject;
+  assertEqual(desc, skins[1]);
+  desc = feathersManager.getLayoutDescription(layouts[1].url).wrappedJSObject;
+  assertEqual(desc, layouts[1]);
+
+  // ------------------------
+  // Verify mappings
+  enumerator = wrapEnumerator(feathersManager.getSkinsForLayout(layouts[0].url), 
+                 Components.interfaces.sbISkinDescription);
+  assertEnumeratorEqualsArray(enumerator, [skins[2]]);
+  enumerator = wrapEnumerator(feathersManager.getSkinsForLayout(layouts[1].url), 
+                 Components.interfaces.sbISkinDescription);
+  assertEnumeratorEqualsArray(enumerator, [skins[0], skins[1]]);
+  
+  // ------------------------
+  // Verify showChrome
+  assertEqual( feathersManager.shouldShowChrome(layouts[0].url, skins[2].provider), true );
+  assertEqual( feathersManager.shouldShowChrome(layouts[0].url, skins[1].provider), false );
+  assertEqual( feathersManager.shouldShowChrome(layouts[1].url, skins[1].provider), false );
+
+
+  // ------------------------
+  // Test selection mechanism
+  // TODO Actually select!
+  
+  // First with an invalid pair
+  assertEqual(feathersManager.select(layouts[0].url, skins[1].provider), false);
+
+  // Then with a valid pair.  Expect an onSelect callback.
+  feathersChangeListener.expectSkin = skins[0];
+  feathersChangeListener.expectLayout = layouts[1];
+  assertEqual(feathersManager.select(layouts[1].url, skins[0].provider), true);
+  // Make sure onSelect callback occurred
+  assertEqual(feathersChangeListener.expectSkin, null);
+  assertEqual(feathersChangeListener.expectLayout, null);
+
+
+
+  // ------------------------
+  // Get rid of everything we added
+
+  // Reset update counter so we can test teardown
+  feathersChangeListener.updateCounter = 0;
+
+  teardown();
+  
+  // ------------------------
+  // Test change notification.  We should have seen 7 update notifications,
+  // as we unhooked before the last removeMapping call.
+  assertEqual(feathersChangeListener.updateCounter, 7);
+  feathersChangeListener.updateCounter = 0;
+  
+  // ------------------------
+  // Make sure teardown succeeded
+  assertEqual(feathersManager.skinCount, 0);
+  var enumerator = feathersManager.getSkinDescriptions();
+  assertEqual(enumerator.hasMoreElements(), false); 
+
+  assertEqual(feathersManager.layoutCount, 0);
+  var enumerator = feathersManager.getLayoutDescriptions();
+  assertEqual(enumerator.hasMoreElements(), false); 
+
+  // ------------------------  
+  // Verify mappings are gone
+  var enumerator = feathersManager.getSkinsForLayout(layouts[0].url);
+  assertEqual(enumerator.hasMoreElements(), false); 
+  var enumerator = feathersManager.getSkinsForLayout(layouts[1].url);
+  assertEqual(enumerator.hasMoreElements(), false); 
+  
+  
+  return Components.results.NS_OK;
+}
+
+
