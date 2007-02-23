01ac570a7d8367bbdbd4ec29c6ee7e69f193d923
redfive <redfive@5aad4ff5-4b58-4159-ba3f-0e6328df4cad>
2007-02-23 01:52:24 +0000

    BuildFarm related stuff - md5sum on linux (almost on mac, commented out) - cleaned up linux script - dump build_id for all platforms so build farm can suck it up
    git-svn-id: http://publicsvn.songbirdnest.com/client/trunk@3555 5aad4ff5-4b58-4159-ba3f-0e6328df4cad

--- installer/linux/make-installer.sh
+++ installer/linux/make-installer.sh
@@ -1,27 +1,34 @@
 #!/bin/bash
 
 DEPTH=../../..
-CURRENT_DIR=`pwd`
 CURRENT_DATE=`date +%Y%m%d`
 SONGBIRD_DIR=Songbird_${CURRENT_DATE}
 SONGBIRD_ARCH=$1
-
-rm -rf ${DEPTH}/compiled/_built_installer
-mkdir ${DEPTH}/compiled/_built_installer
-
-rm -rf ${DEPTH}/compiled/${SONGBIRD_DIR}
-mkdir ${DEPTH}/compiled/${SONGBIRD_DIR}
-
-cp -RL ${DEPTH}/compiled/dist/* ${DEPTH}/compiled/${SONGBIRD_DIR}
-cp ${DEPTH}/installer/linux/LICENSE.txt ${DEPTH}/compiled/${SONGBIRD_DIR}
-cp ${DEPTH}/installer/linux/TRADEMARK.txt ${DEPTH}/compiled/${SONGBIRD_DIR}
-
-rm -f ${DEPTH}/compiled/${SONGBIRD_DIR}_${SONBIRD_ARCH}.tar
-rm -f ${DEPTH}/compiled/${SONGBIRD_DIR}_${SONBIRD_ARCH}.tar.gz
+SONGBIRD_FILENAME=${SONGBIRD_DIR}_${SONGBIRD_ARCH}
 
 cd ${DEPTH}/compiled
-tar -c -f ${SONGBIRD_DIR}_${SONGBIRD_ARCH}.tar ${SONGBIRD_DIR}
-gzip ${SONGBIRD_DIR}_${SONGBIRD_ARCH}.tar
 
-cp ${SONGBIRD_DIR}_${SONGBIRD_ARCH}.tar.gz _built_installer
+# clean up any remaining installers & dirs and make fresh dirs
+rm -rf ./_built_installer
+mkdir ./_built_installer
+
+rm -rf ./${SONGBIRD_DIR}
+mkdir ./${SONGBIRD_DIR}
+
+rm -f ./${SONGBIRD_FILENAME}.tar
+rm -f ./${SONGBIRD_FILENAME}.tar.gz
+
+# pull everything we want into the installer working dir
+cp -RL ./dist/* ./${SONGBIRD_DIR}
+cp ${DEPTH}/installer/linux/LICENSE.txt ./${SONGBIRD_DIR}
+cp ${DEPTH}/installer/linux/TRADEMARK.txt ./${SONGBIRD_DIR}
+
+# tar and gzip the goods
+tar -c -f ./${SONGBIRD_FILENAME}.tar ./${SONGBIRD_DIR}
+gzip ./${SONGBIRD_FILENAME}.tar
+
+# copy tarball into final location and create md5sum
+cd ./_built_installer
+cp ../${SONGBIRD_FILENAME}.tar.gz .
+md5sum ./${SONGBIRD_FILENAME}.tar.gz > ./${SONGBIRD_FILENAME}.tar.gz.md5
 
