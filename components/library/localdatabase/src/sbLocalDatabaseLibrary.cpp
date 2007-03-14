76fbedfb05d06b46a73830bc0db1d9b39d4d4674
aus <aus@5aad4ff5-4b58-4159-ba3f-0e6328df4cad>
2007-03-14 03:55:04 +0000

    oops, snprintf does not exist in the msvcrt, only _snprintf. Switched to using the  portable version PR_snprintf instead.
    git-svn-id: http://publicsvn.songbirdnest.com/client/trunk@3751 5aad4ff5-4b58-4159-ba3f-0e6328df4cad

--- components/library/localdatabase/src/sbLocalDatabaseLibrary.cpp
+++ components/library/localdatabase/src/sbLocalDatabaseLibrary.cpp
@@ -47,6 +47,7 @@
 #include <nsIUUIDGenerator.h>
 
 #include <stdio.h>
+#include <prprf.h>
 
 NS_IMPL_ISUPPORTS2(sbLocalDatabaseLibrary,
                    sbILibrary,
@@ -386,7 +387,7 @@ sbLocalDatabaseLibrary::CreateMediaItem(nsIURI *aUri,
    * Set create and update.  Make a string of the current time in milliseconds
    */
   char buf[20];
-  int len = snprintf(buf, sizeof(buf), "%ld",
+  int len = PR_snprintf(buf, sizeof(buf), "%ld",
                      PRUint64((PR_Now() / PR_USEC_PER_MSEC)));
   buf[sizeof(buf) - 1] = '\0';
   nsAutoString create = NS_ConvertASCIItoUTF16(buf, len);
