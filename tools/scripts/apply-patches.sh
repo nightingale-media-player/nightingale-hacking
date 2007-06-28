1bd373ae6a64249c9b79986965d60ff21990e4a7
redfive <redfive@5aad4ff5-4b58-4159-ba3f-0e6328df4cad>
2007-06-28 20:15:18 +0000

    removing force on patch appy to break buildbots when patch fails
    git-svn-id: http://publicsvn.songbirdnest.com/client/trunk@5289 5aad4ff5-4b58-4159-ba3f-0e6328df4cad

--- tools/scripts/apply-patches.sh
+++ tools/scripts/apply-patches.sh
@@ -30,7 +30,7 @@ abspath="`cd \"$D\" 2>/dev/null && pwd || echo \"$D\"`/$B"
 for ((i=0; $i<$num_patches; i=$i+1)); do
   p=${patches[$i]}
   notice  "# applying $p #"
-  patch -f $reverse --backup-if-mismatch -p0 \
+  patch $reverse --backup-if-mismatch -p0 \
     -d "$targetdir" -i "$abspath/$p"
   echo ""
 done
