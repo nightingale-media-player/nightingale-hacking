937aa9b313ce86a8ea52b11f6b96e3083a941a3b
mook <mook@5aad4ff5-4b58-4159-ba3f-0e6328df4cad>
2007-07-19 01:12:53 +0000

    bug 2630 - make the extension install progress be our prefs dialog and not the default toolkit EM dialog (r=matt)
    git-svn-id: http://publicsvn.songbirdnest.com/client/trunk@5576 5aad4ff5-4b58-4159-ba3f-0e6328df4cad

---
+++ app/content/scripts/preferences/main_overlay.js
@@ -0,0 +1,56 @@
+/*
+ //
+// BEGIN SONGBIRD GPL
+// 
+// This file is part of the Songbird web player.
+//
+// Copyright(c) 2007 POTI, Inc.
+// http://songbirdnest.com
+// 
+// This file may be licensed under the terms of of the
+// GNU General Public License Version 2 (the "GPL").
+// 
+// Software distributed under the License is distributed 
+// on an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either 
+// express or implied. See the GPL for the specific language 
+// governing rights and limitations.
+//
+// You should have received a copy of the GPL along with this 
+// program. If not, go to http://www.gnu.org/licenses/gpl.html
+// or write to the Free Software Foundation, Inc., 
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+// 
+// END SONGBIRD GPL
+//
+*/
+
+addEventListener("load", function() {
+  removeEventListener("load", arguments.callee, false);
+  
+  // switch to the Addons view if we're getting one installed
+  if ("arguments" in window &&
+      window.arguments[0] instanceof Components.interfaces.nsIDialogParamBlock &&
+      window.arguments[1] instanceof Components.interfaces.nsIObserver) {
+
+    var paneAddons = document.getElementById("paneAddons");
+
+    function doStartup() {
+      var frameAddons = document.getElementById("addonsFrame");
+      frameAddons.contentWindow.arguments = window.arguments;
+      frameAddons.contentWindow.Startup();
+
+      paneAddons.removeEventListener("paneload", doStartup, false);
+    }
+    
+    if (!paneAddons.loaded) {
+      // this is needed in case the pane hasn't loaded yet and the pref window
+      // loads it dynamically as we show it (in reality, this will happen all
+      // the time, since this is the initial load)
+      paneAddons.addEventListener("paneload", doStartup, false);
+      document.documentElement.showPane(paneAddons);
+    } else {
+      // also pass to the internal frame if it is already loaded
+      doStartup();
+    }
+  }
+}, false);
