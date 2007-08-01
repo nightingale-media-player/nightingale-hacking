8ee339c94ac6b145f9abd558230b95f1a66720ba
aus <aus@5aad4ff5-4b58-4159-ba3f-0e6328df4cad>
2007-08-01 22:38:32 +0000

    bug 4099 -goodbye frames! hello css fixed position!
    git-svn-id: http://publicsvn.songbirdnest.com/client/trunk@5721 5aad4ff5-4b58-4159-ba3f-0e6328df4cad

--- documentation/make-webpageapi-docs.sh
+++ documentation/make-webpageapi-docs.sh
@@ -81,6 +81,6 @@ rm -f $librarybasefilescleanup
 # Run NaturalDocs
 $topsrcdir/tools/common/naturaldocs/NaturalDocs \
 -i $docstempdir \
--o FramedHTML $docsoutputdir \
+-o HTML $docsoutputdir \
 -p $ndprojectdir \
 -s Default Docs -r
