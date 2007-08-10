2fafb1ca69840daee3661bde309b3ec54c3795ce
lone <lone@5aad4ff5-4b58-4159-ba3f-0e6328df4cad>
2007-08-10 21:13:50 +0000

    reverted two changes ("playlist_test" strings) so mig can commit his patch
    git-svn-id: http://publicsvn.songbirdnest.com/client/trunk@5824 5aad4ff5-4b58-4159-ba3f-0e6328df4cad

--- app/content/scripts/searchHandler.js
+++ app/content/scripts/searchHandler.js
@@ -532,7 +532,7 @@ const gSearchHandler = {
    */
   _isPlaylistShowing: function SearchHandler__isPlaylistShowing() {
     var url = gBrowser.mCurrentBrowser.currentURI.spec;
-    return url.indexOf("sb-library-page-playlist") >= 0;
+    return url.indexOf("playlist_test") >= 0;
   },  
    
    
