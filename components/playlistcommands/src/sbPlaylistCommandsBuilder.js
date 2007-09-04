41036b0df2dee68580efdbb78dfb6507fccaf6fa
lone <lone@5aad4ff5-4b58-4159-ba3f-0e6328df4cad>
2007-09-04 20:11:53 +0000

    again
    git-svn-id: http://publicsvn.songbirdnest.com/client/trunk@6211 5aad4ff5-4b58-4159-ba3f-0e6328df4cad

--- components/playlistcommands/src/sbPlaylistCommandsBuilder.js
+++ components/playlistcommands/src/sbPlaylistCommandsBuilder.js
@@ -1220,7 +1220,7 @@ PlaylistCommandsBuilder.prototype = {
       obj.m_menus.push(menuitem);
     }
     obj.m_root_commands = obj.m_menus[0].m_Menu;
-    obj.m_Context = this.dupObject(obj.m_Context);
+    obj.m_Context = this.dupObject(this.m_Context);
         
     // return the copy
     return obj;
