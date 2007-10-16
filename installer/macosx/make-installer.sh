d726f0b3068ce485932e055ebcb849bff6d2de21
aus <aus@5aad4ff5-4b58-4159-ba3f-0e6328df4cad>
2007-10-16 22:35:52 +0000

    bug 4961 ensure readme.txt is in right spot and aligned properly with new background image.
    git-svn-id: http://publicsvn.songbirdnest.com/client/trunk@6768 5aad4ff5-4b58-4159-ba3f-0e6328df4cad

--- installer/macosx/make-installer.sh
+++ installer/macosx/make-installer.sh
@@ -8,8 +8,16 @@ ARCH="$1"
 rm -rf ${DEPTH}/compiled/_built_installer
 mkdir ${DEPTH}/compiled/_built_installer
 
-cp ${DEPTH}/compiled/dist/Songbird.app/README.txt ${DEPTH}/compiled/dist/README.txt
+cp ${DEPTH}/compiled/dist/Songbird.app/Contents/Resources/README.txt ${DEPTH}/compiled/dist/README.txt
 ln -s /Applications/ ${DEPTH}/compiled/dist/Applications
 
-${DEPTH}/installer/macosx/make-diskimage ${DEPTH}/compiled/_built_installer/Songbird_${CURRENT_DATE}_${ARCH}.dmg ${DEPTH}/compiled/dist Songbird -null- ${DEPTH}/installer/macosx/songbird.dsstore ${DEPTH}/installer/macosx/background.tiff ${DEPTH}/installer/macosx/MacSongbirdDiskImage.icns 
+${DEPTH}/installer/macosx/make-diskimage \
+  ${DEPTH}/compiled/_built_installer/Songbird_${CURRENT_DATE}_${ARCH}.dmg \
+  ${DEPTH}/compiled/dist \
+  Songbird \
+  -null- \
+  ${DEPTH}/installer/macosx/songbird.dsstore \
+  ${DEPTH}/installer/macosx/background.tiff \
+  ${DEPTH}/installer/macosx/MacSongbirdDiskImage.icns 
+
 md5 ${DEPTH}/compiled/_built_installer/Songbird_${CURRENT_DATE}_${ARCH}.dmg > ${DEPTH}/compiled/_built_installer/Songbird_${CURRENT_DATE}_${ARCH}.dmg.md5
