712300dca5a5f3a9a8ed2a15bf89a23700ef5621
ben <ben@5aad4ff5-4b58-4159-ba3f-0e6328df4cad>
2007-12-06 22:36:49 +0000

    Bug 6040 - '[prefs] add-ons pane cleanup'. r=ian
    git-svn-id: http://publicsvn.songbirdnest.com/client/trunk@7440 5aad4ff5-4b58-4159-ba3f-0e6328df4cad

--- app/content/scripts/preferences/addOns.js
+++ app/content/scripts/preferences/addOns.js
@@ -1,43 +1,67 @@
 /*
  //
 // BEGIN SONGBIRD GPL
-// 
+//
 // This file is part of the Songbird web player.
 //
 // Copyright(c) 2005-2007 POTI, Inc.
 // http://songbirdnest.com
-// 
+//
 // This file may be licensed under the terms of of the
 // GNU General Public License Version 2 (the "GPL").
-// 
-// Software distributed under the License is distributed 
-// on an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either 
-// express or implied. See the GPL for the specific language 
+//
+// Software distributed under the License is distributed
+// on an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either
+// express or implied. See the GPL for the specific language
 // governing rights and limitations.
 //
-// You should have received a copy of the GPL along with this 
+// You should have received a copy of the GPL along with this
 // program. If not, go to http://www.gnu.org/licenses/gpl.html
-// or write to the Free Software Foundation, Inc., 
+// or write to the Free Software Foundation, Inc.,
 // 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
-// 
+//
 // END SONGBIRD GPL
 //
 */
 
 var AddonsPrefPane = {
 
-  load: function load() {
-    // We need to wait until the IFrame loads completely
-    var addonsIFrame = document.getElementById("addonsFrame");
-    addonsIFrame.addEventListener("load", this.onEMLoad, true);
+  _addonsDoc: null,
+
+  _hideResizer: function() {
+    const resizerBox = this._addonsDoc.getElementById("resizerBox");
+    resizerBox.setAttribute("hidden", "true");
   },
-  
+
+  _hidePlugins: function() {
+    const viewGroup = this._addonsDoc.getElementById("viewGroup");
+    if (viewGroup.getAttribute("last-selected") != "extensions") {
+      // Tell the EM's Startup() function to select the extensions view.
+      viewGroup.setAttribute("last-selected", "extensions");
+    }
+
+    // Hide the plugins radio option.
+    const pluginRadio = this._addonsDoc.getElementById("plugins-view");
+    pluginRadio.setAttribute("hidden", "true");
+  },
+
+  load: function load() {
+    const self = AddonsPrefPane;
+
+    // We need to wait until the IFrame loads completely
+    const addonsIFrame = document.getElementById("addonsFrame");
+    addonsIFrame.addEventListener("load", self.onEMLoad, true);
+  },
+
   onEMLoad: function onEMLoad() {
-    // Kill the resizer
-    var addonsIFrame = document.getElementById("addonsFrame");
-    var addonsDoc = addonsIFrame.contentDocument;
-    var resizerBox = addonsDoc.getElementById("resizerBox");
-    if (resizerBox)
-      resizerBox.style.display = "none";
+    const self = AddonsPrefPane;
+
+    const addonsIFrame = document.getElementById("addonsFrame");
+    addonsIFrame.removeEventListener("load", self.onEMLoad, true);
+
+    self._addonsDoc = addonsIFrame.contentDocument;
+
+    self._hideResizer();
+    self._hidePlugins();
   }
 };
