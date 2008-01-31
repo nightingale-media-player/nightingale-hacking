6f64836b889a220ee680bbce21964f3c36420b0e
mook <mook@5aad4ff5-4b58-4159-ba3f-0e6328df4cad>
2008-01-31 16:06:04 +0000

    bug 6868 - content type format (r=stevo)
    git-svn-id: http://publicsvn.songbirdnest.com/client/trunk@8014 5aad4ff5-4b58-4159-ba3f-0e6328df4cad

---
+++ components/devices/base/src/sbContentTypeFormat.cpp
@@ -0,0 +1,160 @@
+/* vim: set sw=2 :miv */
+/*
+//
+// BEGIN SONGBIRD GPL
+//
+// This file is part of the Songbird web player.
+//
+// Copyright(c) 2005-2008 POTI, Inc.
+// http://songbirdnest.com
+//
+// This file may be licensed under the terms of of the
+// GNU General Public License Version 2 (the "GPL").
+//
+// Software distributed under the License is distributed
+// on an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either
+// express or implied. See the GPL for the specific language
+// governing rights and limitations.
+//
+// You should have received a copy of the GPL along with this
+// program. If not, go to http://www.gnu.org/licenses/gpl.html
+// or write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+//
+// END SONGBIRD GPL
+//
+*/
+
+#include "sbContentTypeFormat.h"
+
+#include <nsAutoLock.h>
+#include <nsAutoPtr.h>
+#include <nsComponentManagerUtils.h>
+#include <nsISimpleEnumerator.h>
+#include <nsISupportsPrimitives.h>
+
+class FourCCEnumerator : public nsISimpleEnumerator {
+public:
+  NS_DECL_NSISIMPLEENUMERATOR;
+  NS_DECL_ISUPPORTS;
+protected:
+  friend class sbContentTypeFormat;
+  FourCCEnumerator(sbContentTypeFormat* aFormat,
+                   nsTArray<FourCC>* aArray)
+   : mFormat(aFormat),
+     mArray(aArray),
+     mIndex(0)
+  {
+  }
+
+private:
+  ~FourCCEnumerator() {}
+
+protected:
+  nsRefPtr<sbContentTypeFormat> mFormat;
+  nsTArray<FourCC> *mArray;
+  PRUint32 mIndex; // index of next item to look at
+};
+
+NS_IMPL_THREADSAFE_ISUPPORTS1(FourCCEnumerator, nsISimpleEnumerator)
+
+/* boolean hasMoreElements (); */
+NS_IMETHODIMP FourCCEnumerator::HasMoreElements(PRBool *_retval)
+{
+  NS_ENSURE_ARG_POINTER(_retval);
+  
+  *_retval = mIndex < mArray->Length();
+  return NS_OK;
+}
+
+/* nsISupports getNext (); */
+NS_IMETHODIMP FourCCEnumerator::GetNext(nsISupports **_retval)
+{
+  NS_ENSURE_ARG_POINTER(_retval);
+  NS_ENSURE_TRUE(mIndex < mArray->Length(), NS_ERROR_UNEXPECTED);
+  
+  nsresult rv;
+  
+  nsCOMPtr<nsISupportsPRUint32> fourCC =
+    do_CreateInstance(NS_SUPPORTS_PRUINT32_CONTRACTID, &rv);
+  NS_ENSURE_SUCCESS(rv, rv);
+  rv = fourCC->SetData(mArray->SafeElementAt(mIndex, 0));
+  NS_ENSURE_SUCCESS(rv, rv);
+  ++mIndex;
+  
+  return NS_OK;
+}
+
+/******************************************************************************/
+
+NS_IMPL_THREADSAFE_ISUPPORTS1(sbContentTypeFormat, sbIContentTypeFormat)
+
+sbContentTypeFormat::sbContentTypeFormat()
+ : mHasInitialized(PR_FALSE)
+{
+  mInitLock = nsAutoLock::NewLock(__FILE__);
+}
+
+sbContentTypeFormat::~sbContentTypeFormat()
+{
+  /* destructor code */
+}
+
+/* void Init (in FourCC aContainerFormat,
+              [array, size_is (aEncodingFormatsCount)] in FourCC aEncodingFormats,
+              in unsigned long aEncodingFormatsCount,
+              [array, size_is (aDecodingFormatsCount)] in FourCC aDecodingFormats,
+              in unsigned long aDecodingFormatsCount); */
+NS_IMETHODIMP sbContentTypeFormat::Init(FourCC aContainerFormat,
+                                        FourCC *aEncodingFormats,
+                                        PRUint32 aEncodingFormatsCount,
+                                        FourCC *aDecodingFormats,
+                                        PRUint32 aDecodingFormatsCount)
+{
+  NS_ENSURE_TRUE(mInitLock, NS_ERROR_NOT_INITIALIZED);
+  
+  { /* scope the initialization lock */
+    nsAutoLock initLock(mInitLock);
+    if (mHasInitialized)
+      return NS_ERROR_ALREADY_INITIALIZED;
+    mHasInitialized = PR_TRUE;
+  } /* end scope for initialization lock */
+  
+  mContainerFormat = aContainerFormat;
+  // since this is init and the only mutator, it is safe to assume that the
+  // arrays are all empty at this point.  Just append is fine.
+  NS_ASSERTION(mEncodingFormats.IsEmpty(), "Encoding formats before init!");
+  NS_ASSERTION(mDecodingFormats.IsEmpty(), "Decoding formats before init!");
+  mEncodingFormats.AppendElements(aEncodingFormats, aEncodingFormatsCount);
+  mDecodingFormats.AppendElements(aDecodingFormats, aDecodingFormatsCount);
+  
+  return NS_OK;
+}
+
+/* readonly attribute FourCC containerFormat; */
+NS_IMETHODIMP sbContentTypeFormat::GetContainerFormat(FourCC *aContainerFormat)
+{
+  NS_ENSURE_ARG_POINTER(aContainerFormat);
+  *aContainerFormat = mContainerFormat;
+  return NS_OK;
+}
+
+/* readonly attribute nsISimpleEnumerator encodingFormats; */
+NS_IMETHODIMP sbContentTypeFormat::GetEncodingFormats(nsISimpleEnumerator * *aEncodingFormats)
+{
+  NS_ENSURE_ARG_POINTER(aEncodingFormats);
+  nsRefPtr<FourCCEnumerator> enumerator =
+    new FourCCEnumerator(this, &mEncodingFormats);
+  NS_ENSURE_TRUE(enumerator, NS_ERROR_OUT_OF_MEMORY);
+  return CallQueryInterface(enumerator.get(), aEncodingFormats);
+}
+
+/* readonly attribute nsISimpleEnumerator decodingFormats; */
+NS_IMETHODIMP sbContentTypeFormat::GetDecodingFormats(nsISimpleEnumerator * *aDecodingFormats)
+{
+  NS_ENSURE_ARG_POINTER(aDecodingFormats);
+  nsRefPtr<FourCCEnumerator> enumerator =
+    new FourCCEnumerator(this, &mDecodingFormats);
+  NS_ENSURE_TRUE(enumerator, NS_ERROR_OUT_OF_MEMORY);
+  return CallQueryInterface(enumerator.get(), aDecodingFormats);
+}
---
+++ components/devices/base/test/test_content_type_formats.js
@@ -0,0 +1,74 @@
+/* vim: set sw=2 :miv */
+/*
+//
+// BEGIN SONGBIRD GPL
+//
+// This file is part of the Songbird web player.
+//
+// Copyright(c) 2005-2008 POTI, Inc.
+// http://songbirdnest.com
+//
+// This file may be licensed under the terms of of the
+// GNU General Public License Version 2 (the "GPL").
+//
+// Software distributed under the License is distributed
+// on an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either
+// express or implied. See the GPL for the specific language
+// governing rights and limitations.
+//
+// You should have received a copy of the GPL along with this
+// program. If not, go to http://www.gnu.org/licenses/gpl.html
+// or write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+//
+// END SONGBIRD GPL
+//
+*/
+
+/**
+ * \brief Content Type Formats test
+ */
+
+Components.utils.import("resource://app/components/ArrayConverter.jsm");
+
+/* return a new random FourCC. well, smells like one anyway. */
+function generateFourCC() {
+  return (Math.random() * (-1 >>> 0)) << 0;
+}
+
+function CompareResults(aJSArray, aEnumerator, aMessage) {
+  var result = ArrayConverter.JSArray(aEnumerator);
+  assertTrue(aJSArray.length, result.length, aMessage);
+  
+  const nsISupportsPRUint32 = Components.interfaces.nsISupportsPRUint32;
+  
+  for (var i = 0; i < result.length; ++i) {
+    var data = result[i].QueryInterface(nsISupportsPRUint32).data;
+    assertEqual(aJSArray[i], data, aMessage + "[" + i + "]");
+  }
+}
+
+function runTest () {
+  var formats = Components.classes["@songbirdnest.com/Songbird/Device/ContentTypeFormat;1"]
+                          .CreateInstance(Components.interfaces.sbIContentTypeFormat);
+  assertTrue(formats, "Failed to create content type format component");
+  
+  var FCCContainer = generateFourCC();
+  var FCCEncode = [], FCCDecode = [];
+  for (var i = 0; i < 10; ++i) {
+    FCCEncode.push(generateFourCC());
+    FCCDecode.push(generateFourCC());
+  }
+  
+  formats.init(FCCContainer, FCCEncode, FCCEncode.length, FCCDecode, FCCDecode.length);
+  try {
+    formats.init(FCCContainer, FCCEncode, FCCEncode.length, FCCDecode, FCCDecode.length);
+    fail("re-initialization of content type format did not throw");
+  } catch (ex) {
+    /* expected to throw */
+  }
+  
+  assertEqual(formats.containerFormat, FCCContainer);
+  CompareResults(FCCEncode, formats.encodingFormats, "encodingFormats");
+  CompareResults(FCCDecode, formats.decodingFormats, "decodingFormats");
+}
