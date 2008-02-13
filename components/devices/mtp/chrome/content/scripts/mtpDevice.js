e9eb154dbd870d345c6e176dbfd1dba7d8532065
aus <aus@5aad4ff5-4b58-4159-ba3f-0e6328df4cad>
2008-02-13 19:03:02 +0000

    bug 6831 -add events to sbIDeviceEvent (errors required by error spec in UED doc). -add error event handling r=mook
    git-svn-id: http://publicsvn.songbirdnest.com/client/trunk@8217 5aad4ff5-4b58-4159-ba3f-0e6328df4cad

--- components/devices/mtp/chrome/content/scripts/mtpDevice.js
+++ components/devices/mtp/chrome/content/scripts/mtpDevice.js
@@ -34,15 +34,134 @@ if (typeof(Ci) == "undefined")
   var Ci = Components.interfaces;
 if (typeof(Cr) == "undefined")
   var Cr = Components.results;
+if (typeof(Cu) == "undefined")
+  var Cu = Components.utils;
   
 var mtpCore = {
-  deviceManager: null,
+  _deviceEventListener: null,
+  _deviceManager:       null,
+  _observerSvc:         null,
+  _promptSvc:           null,
+  _stringBundle:        null,
+  _stringBundleSvc:     null,
   
-  initialize: function mtpCore_initialize() {
-  
+  // ************************************
+  // nsIObserver implementation
+  // ************************************
+  observe: function mtpCore_observe(aSubject, 
+                                    aTopic, 
+                                    aData) {
+    switch (aTopic) {
+      case this._cfg.appQuitTopic :
+        this._shutdown();
+      break;
+    }
   },
   
-  shutdown: function mtpCore_shutdown() {
+  // ************************************
+  // Internal methods
+  // ************************************
+  _initialize: function mtpCore_initialize() {
+    this._observerSvc = Cc["@mozilla.org/observer-service;1"]
+                          .getService(Ci.nsIObserverService);
+    this._observerSvc.addObserver(this, "quit-application", false);
+
+    this._deviceManager = Cc["@songbirdnest.com/Songbird/DeviceManager;2"]
+                            .getService(Ci.sbIDeviceManager2);
+    
+    var deviceEventListener = {
+      mtpCore: this,
+      onDeviceEvent: function deviceEventListener_onDeviceEvent(aDeviceEvent) {
+        this.mtpCore._processDeviceEvent(aDeviceEvent);
+      }
+    };
+
+    this._deviceEventListener = deviceEventListener;
+    this._deviceManager.addEventListener(deviceEventListener);
+    
+    this._promptSvc = Cc["@mozilla.org/embedcomp/prompt-service;1"]
+                        .getService(Ci.nsIPromptService);
+                        
+    this._stringBundleSvc = Cc["@mozilla.org/intl/stringbundle;1"]
+                              .getService(Ci.nsIStringBundleService);
+    this._stringBundle = 
+      this._stringBundleSvc
+          .createBundle( "chrome://songbird/locale/songbird.properties" );
+    
+  },
   
-  }
+  _shutdown: function mtpCore_shutdown() {
+    this._observerSvc.removeObserver(this, "quit-application");
+    
+    this._deviceManagerSvc.removeEventListener(this._deviceEventListener);
+    this._deviceEventListener = null;
+
+    this._observerSvc = null;
+    this._deviceManager = null;
+    this._promptSvc = null;
+    
+    this._stringBundle = null;
+    this._stringBundleSvc = null;
+  },
+  
+  _processDeviceEvent: function mtpCore_processDeviceEvent(aDeviceEvent) {
+    
+    // For now, we only process events coming from sbIDevice.
+    if(!(aDeviceEvent.origin instanceof Ci.sbIDevice))
+      return;
+    
+    var applicationName = this._stringBundle.getStringFromName("brandShortName");
+
+    var errorTitle = "";
+    var errorMsg = "";
+    
+    var device = aDeviceEvent.origin.QueryInterface(Ci.sbIDevice);
+    
+    var windowMediator = Cc["@mozilla.org/appshell/window-mediator;1"]
+                           .getService(Ci.nsIWindowMediator);
+    var mainWindow = windowMediator.getMostRecentWindow("Songbird:Main");
+    
+    switch(aDeviceEvent.type) {
+      case Ci.sbIDeviceEvent.EVENT_DEVICE_ACCESS_DENIED: {
+        errorTitle = 
+          this._stringBundle
+              .getStringFromName("device.error.access_denied.title");
+        errorMsg = 
+          this._stringBundle
+              .formatStringFromName("device.error.access_denied.message",
+                                    [applicationName, device.name],
+                                    2);
+        this._promptSvc.alert(mainWindow, errorTitle, errorMsg);
+      }
+      break;
+      
+      case Ci.sbIDeviceEvent.EVENT_DEVICE_NOT_ENOUGH_FREESPACE: {
+        errorTitle = 
+          this._stringBundle
+              .getStringFromName("device.error.not_enough_freespace.title");
+        errorMsg = 
+          this._stringBundle
+              .formatStringFromName("device.error.not_enough_freespace.message",
+                                    [applicationName, device.name],
+                                    2);
+        this._promptSvc.alert(mainWindow, errorTitle, errorMsg);
+      }
+      break;
+      
+      case Ci.sbIDeviceEvent.EVENT_DEVICE_NOT_AVAILABLE: {
+        errorTitle = 
+          this._stringBundle
+              .getStringFromName("device.error.not_available.title");
+        errorMsg = 
+          this._stringBundle
+              .formatStringFromName("device.error.not_avaialble.message",
+                                    [applicationName, device.name],
+                                    2);
+        this._promptSvc.alert(mainWindow, errorTitle, errorMsg);
+      }
+      break;
+    }
+  },
 };
+
+mtpCore._initialize();
