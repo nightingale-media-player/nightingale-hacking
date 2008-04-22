cc073cfeb251e1501aae3820bbf43026f9e19e40
matt <matt@5aad4ff5-4b58-4159-ba3f-0e6328df4cad>
2008-04-22 18:45:21 +0000

    Bug 8410 - commented out unsupported formats
    git-svn-id: http://publicsvn.songbirdnest.com/client/trunk@9033 5aad4ff5-4b58-4159-ba3f-0e6328df4cad

--- components/mediacore/metadata/manager/test/test_metadatajob_writing.js
+++ components/mediacore/metadata/manager/test/test_metadatajob_writing.js
@@ -32,8 +32,10 @@ var gTestFileLocation = "testharness/metadatamanager/files/";
 
 // TODO Confirm this extension list. Don't forget to update files/Makefile.in as well.
 var gSupportedFileExtensions = [
-    "mp3", "mov", "m4p", "m4v",
-    "ogv", "ogm", "ogx", "tta", "wv"
+    "mp3", "mov", "m4p", "ogm", "ogx", "tta", "wv"
+    
+// TODO Taglib does not support video files
+// "m4v", "ogv"
     
 // TODO ogg, flac, and mpc writing works, but seems to double-encode utf8. Filed as bug 8813.
 // "ogg", "flac", "mpc"
