41e03214d1411bfc7f6f4f514138ca38e089cadd
matt <matt@5aad4ff5-4b58-4159-ba3f-0e6328df4cad>
2008-06-04 17:58:48 +0000

    Bug 9437 - add wavpack to the list of media formats supported by gstreamer
    git-svn-id: http://publicsvn.songbirdnest.com/client/trunk@9577 5aad4ff5-4b58-4159-ba3f-0e6328df4cad

--- components/playlistplayback/src/coreGStreamerSimple.js
+++ components/playlistplayback/src/coreGStreamerSimple.js
@@ -46,7 +46,7 @@ function CoreGStreamerSimple()
 
   this._mediaUrlExtensions = ["mp3", "ogg", "flac", "mpc", "wav", "m4a", "m4v",
                               "wmv", "asf", "avi",  "mov", "mpg", "mp4", "ogm",
-                              "mp2", "mka", "mkv",  "oga", "ogv", "ogx"];
+                              "mp2", "mka", "mkv",  "oga", "ogv", "ogx", "wv"];
   this._mediaUrlSchemes = ["mms", "rstp"];
 
   this._videoUrlExtensions = ["wmv", "asf",  "avi", "mov", "mpg", "m4v", "mp4",
