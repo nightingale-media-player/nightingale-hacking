857cde637fdd770ca745e0ab87d86017d98dadcb
aus <aus@5aad4ff5-4b58-4159-ba3f-0e6328df4cad>
2008-07-25 21:36:59 +0000

    bug 7355 -oops :( Forgot the remote api would need to have this component explicitly available. r=me
    git-svn-id: http://publicsvn.songbirdnest.com/client/trunk@10127 5aad4ff5-4b58-4159-ba3f-0e6328df4cad

--- components/library/localdatabase/src/sbLocalDatabaseMigrationHelper.js
+++ components/library/localdatabase/src/sbLocalDatabaseMigrationHelper.js
@@ -60,105 +60,173 @@ sbLocalDatabaseMigrationHelper.prototype = {
   classDescription: "Songbird Local Database Library Migration",
   classID:          Components.ID("{744f6217-4cb9-4929-8d1d-72492c1b8c83}"),
   contractID:       SBLDBCOMP + "MigrationHelper;1",
+  implementationLanguage: Ci.nsIProgrammingLanguage.JAVASCRIPT,
+
+  // needs to be DOM_OBJECT to allow remoteAPI to access it.
+  flags: Ci.nsIClassInfo.DOM_OBJECT,
+
   constructor:      sbLocalDatabaseMigrationHelper,
   
   _initialized: false,
   
+  _securityMixin: null,
+  _initializedSCC: false,
+  
+  _publicWProps: [ "" ],
+  _publicRProps: [ "internal:latestSchemaVersion",
+                   "classinfo:classDescription",
+                   "classinfo:contractID",
+                   "classinfo:classID",
+                   "classinfo:implementationLanguage",
+                   "classinfo:flags" ],
+  _publicMethods: [ "internal:canMigrate",
+                    "internal:migrate"],
+  _publicInterfaces: [ Ci.nsISupports,
+                       Ci.nsIClassInfo,
+                       Ci.nsISecurityCheckedComponent,
+                       Ci.sbILocalDatabaseMigrationHelper,
+                       Ci.sbIJobProgress ],
+  
   _latestSchemaVersion: 5,
   _lowestFromSchemaVersion: Number.MAX_VALUE,
   
-  _migrationHandlers:   null
-}
+  _migrationHandlers:   null,
 
-//
-// Local methods
-//
+  //
+  // Local methods
+  //
 
-sbLocalDatabaseMigrationHelper.prototype._init = 
-function sbLDBM_init() {
-  this._getMigrationHandlerList();
-  this._initialized = true;
-}
+  _init: function sbLDBM_init() {
+    this._getMigrationHandlerList();
+    this._initialized = true;
+  },
 
-sbLocalDatabaseMigrationHelper.prototype._getMigrationHandlerList = 
-function sbLDBM_getMigrationHandlerList() {
-  this._migrationHandlers = [];
-  
-  for(let contractID in Cc) {
-    if(contractID.indexOf(SBLDBCOMP + "Migration/Handler") == 0) {
-      let migrationHandler = 
-        Cc[contractID].createInstance(Ci.sbILocalDatabaseMigrationHandler);
-      let migrationHandlerKey = migrationHandler.fromVersion + 
-                                "," + 
-                                migrationHandler.toVersion;
-      this._migrationHandlers[migrationHandlerKey] = migrationHandler;
-      
-      if(this._lowestSchemaVersion > migrationHandler.fromVersion) {
-        this._lowestSchemaVersion = migrationHandler.fromVersion;
+  _getMigrationHandlerList: function sbLDBM_getMigrationHandlerList() {
+    this._migrationHandlers = {};
+    
+    for(let contractID in Cc) {
+      if(contractID.indexOf(SBLDBCOMP + "Migration/Handler") == 0) {
+        let migrationHandler = 
+          Cc[contractID].createInstance(Ci.sbILocalDatabaseMigrationHandler);
+        let migrationHandlerKey = migrationHandler.fromVersion + 
+                                  "," + 
+                                  migrationHandler.toVersion;
+        this._migrationHandlers[migrationHandlerKey] = migrationHandler;
+        
+        if(this._lowestSchemaVersion > migrationHandler.fromVersion) {
+          this._lowestSchemaVersion = migrationHandler.fromVersion;
+        }
       }
     }
+  },
+
+  // 
+  // sbILocalDatabaseMigration
+  // 
+
+  get latestSchemaVersion() {
+    return this._latestSchemaVersion;
+  },
+
+  canMigrate: function sbLDBM_canMigrate(aFromVersion, aToVersion) {
+    if(!this._initialized) {
+      this._init();
+    }
+    
+    //XXXAus: PLACE HOLDER
+    
+    return false;
+  },
+
+  migrate: function sbLDBM_migrate(aFromVersion, aToVersion, aLibrary) {
+    if(!this._initialized) {
+      this._init();
+    }
+    
+    //XXXAus: PLACE HOLDER
+    
+    return;
+  },
+
+
+  //
+  // nsISupports
+  //
+
+  QueryInterface: XPCOMUtils.generateQI([
+    Ci.sbILocalDatabaseMigrationHelper,
+    Ci.sbIJobProgress,
+    Ci.nsIClassInfo,
+    Ci.nsISecurityCheckedComponent,
+    Ci.sbISecurityAggregator
+  ]),
+
+
+  // 
+  // nsIClassInfo
+  //
+
+  getInterfaces: function sbLDBM_getInterfaces(count) {
+    var interfaces = [Ci.sbILocalDatabaseMigrationHelper, 
+                      Ci.nsIClassInfo,
+                      Ci.sbIJobProgress,
+                      Ci.nsISecurityCheckedComponent,
+                      Ci.sbISecurityAggregator,
+                      Ci.nsISupports];
+    
+    count.value = interfaces.length;
+    
+    return interfaces;
+  },
+
+  getHelperForLanguage: function sbLDBM_getHelperForLanguage(aLanguage) {
+    return null;
+  },
+
+
+  //
+  // nsISecurityCheckedComponent -- implemented by the security mixin
+  //
+
+  _initSCC: function sbLDBM__initSCC() {
+    this._securityMixin = Cc["@songbirdnest.com/remoteapi/security-mixin;1"]
+                            .createInstance(Ci.nsISecurityCheckedComponent);
+
+    // initialize the security mixin with the cleared methods and props
+    this._securityMixin
+             .init(this, this._publicInterfaces, this._publicInterfaces.length,
+                         this._publicMethods, this._publicMethods.length,
+                         this._publicRProps, this._publicRProps.length,
+                         this._publicWProps, this._publicWProps.length,
+                         false);
+
+    this._initializedSCC = true;
+  },
+
+  canCreateWrapper: function sbLDBM_canCreateWrapper(iid) {
+    if (! this._initializedSCC)
+      this._initSCC();
+    return this._securityMixin.canCreateWrapper(iid);
+  },
+
+  canCallMethod: function sbLDBM_canCallMethod(iid, methodName) {
+    if (! this._initializedSCC)
+      this._initSCC();
+    return this._securityMixin.canCallMethod(iid, methodName);
+  },
+
+  canGetProperty: function sbLDBM_canGetProperty(iid, propertyName) {
+    if (! this._initializedSCC)
+      this._initSCC();
+    return this._securityMixin.canGetProperty(iid, propertyName);
+  },
+
+  canSetProperty: function sbLDBM_canSetProperty(iid, propertyName) {
+    if (! this._initializedSCC)
+      this._initSCC();
+    return this._securityMixin.canSetProperty(iid, propertyName);
   }
-}
-
-// 
-// sbILocalDatabaseMigration
-// 
-
-sbLocalDatabaseMigrationHelper.prototype.__defineGetter__("latestSchemaVersion",
-function() {
-  return this._latestSchemaVersion;
-});
-
-sbLocalDatabaseMigrationHelper.prototype.canMigrate = 
-function sbLDBM_canMigrate(aFromVersion, aToVersion) {
-  if(!this._initialized) {
-    this._init();
-  }
-  
-  //XXXAus: PLACE HOLDER
-  
-  return false;
-}
-
-sbLocalDatabaseMigrationHelper.prototype.migrate = 
-function sbLDBM_migrate(aFromVersion, aToVersion, aLibrary) {
-  if(!this._initialized) {
-    this._init();
-  }
-  
-  //XXXAus: PLACE HOLDER
-  
-  return;
-}
-
-
-//
-// nsISupports
-//
-
-sbLocalDatabaseMigrationHelper.prototype.QueryInterface =
-XPCOMUtils.generateQI([
-  Ci.sbILocalDatabaseMigrationHelper,
-  Ci.sbIJobProgress,
-  Ci.nsIClassInfo
-]);
-
-// 
-// nsIClassInfo
-//
-
-sbLocalDatabaseMigrationHelper.prototype.getInterfaces = 
-function sbLDBM_getInterfaces(count) {
-  var interfaces = [Ci.sbILocalDatabaseMigrationHelper, 
-                    Ci.nsIClassInfo,
-                    Ci.sbIJobProgress,
-                    Ci.nsISupports];
-  
-  count.value = interfaces.length;
-  
-  return interfaces;
-}
-
+};
 
 //
 // Module
