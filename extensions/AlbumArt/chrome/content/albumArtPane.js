f3abb31f048d1e133a7dc1b885376a2107f0d245
steven <steven@5aad4ff5-4b58-4159-ba3f-0e6328df4cad>
2008-09-05 20:24:37 +0000

    Bug 12068 12071 12072 - Make the title bar in the album art display pane clickable and look like a button, also update the title to what is represented in the display pane for album art. (r=nick)
    git-svn-id: http://publicsvn.songbirdnest.com/client/trunk@10820 5aad4ff5-4b58-4159-ba3f-0e6328df4cad

--- extensions/AlbumArt/chrome/content/albumArtPane.js
+++ extensions/AlbumArt/chrome/content/albumArtPane.js
@@ -39,26 +39,74 @@ Cu.import("resource://gre/modules/XPCOMUtils.jsm");
 
 // Display pane constants
 const DISPLAY_PANE_CONTENTURL = "chrome://albumart/content/albumArtPane.xul";
-const DISPLAY_PANE_ICON       = "http://songbirdnest.com/favicon.ico";
+const DISPLAY_PANE_ICON       = "chrome://albumart/skin/icon-albumart.png";
 
 // Default cover for items missing the cover
 const DEFAULT_COVER = "chrome://songbird/skin/album-art/default-cover.png";
 
+// Constants for toggling between Selected and Playing states
+// These releate to the deck
+const STATE_SELECTED = 0;
+const STATE_PLAYING  = 1;
+
 /******************************************************************************
  *
- * \class AlbumArtPaneControler 
+ * \class AlbumArt 
  * \brief Controller for the Album Art Pane.
  *
  *****************************************************************************/
-var AlbumArtPaneController = {
+var AlbumArt = {
   _coverBind: null,                 // Data remote for the now playing image.
   _playListPlaybackService: null,   // Get notifications of track changes
 
-  // Cache some things so we don't have to keep loading the same image
-  _lastImageSrc: "",       // keep track of what the last image src was
-  _imageWidth: 0,          // hold this for resizing
-  _imageHeight: 0,         // and this.
+  // Array of state information for each display (selected/playing/etc)
+  _stateInfo: [ { imageID: "sb-albumart-selected",
+                  titleID: "selected",
+                  defaultTitle: "Now Selected",
+                  lastImageSrc: "",
+                  imageWidth: 0,
+                  imageHeight: 0
+                },
+                { imageID: "sb-albumart-playing",
+                  titleID: "playing",
+                  defaultTitle: "Now Playing",
+                  lastImageSrc: "",
+                  imageWidth: 0,
+                  imageHieght: 0
+                }
+              ],
+  _currentState: STATE_PLAYING, // Default to now playing state (display)
 
+
+  /**
+   * \brief makeTitleBarButton - This modifies the display pane title bar so
+   *        that it acts like a button to toggle between the different displays.
+   */
+  makeTitleBarButton: function() {
+    var displayPaneManager = Cc["@songbirdnest.com/Songbird/DisplayPane/Manager;1"]
+                               .getService(Ci.sbIDisplayPaneManager);
+    var dpInstantiator = displayPaneManager.getInstantiatorForWindow(window);
+
+    if (dpInstantiator) {
+      var displayPane = dpInstantiator.displayPane;
+      if (displayPane) {
+        // Make the title bar of the display pane act like a button
+        displayPane.makeClickableTitleBar(AlbumArt.titleClick);
+      }
+    }
+  },
+  
+  /**
+   * \brief titleClick - The handles the event when the user clicks the title
+   *        bar of the display pane, for primary button clicks we toggle the
+   *        view.
+   */
+  titleClick: function(event) {
+    if (event.button == 0) {
+      AlbumArt.toggleView();
+    }
+  },
+  
   /**
    * \brief onImageDblClick - This function is called when the user double
    *        clicks the image.
@@ -75,6 +123,112 @@ var AlbumArtPaneController = {
                    null,
                    null);
     }
+  },
+
+  /**
+   * \brief setPanetitle - This function updates the pane title depending on
+   *        the current state.
+   */
+  setPaneTitle: function() {
+    // Sanity check
+    if (AlbumArt._currentState >= AlbumArt._stateInfo.length ||
+        AlbumArt._currentState < 0) {
+      return;
+    }
+    
+    var titleInfo = AlbumArt._stateInfo[AlbumArt._currentState];
+    var titleString = SBString("albumart.displaypane.title." + titleInfo.titleID,
+                               titleInfo.defaultTitle);
+    
+    // Set the title correctly, localize since we can not localize from the
+    // install.rdf.
+    var paneMgr = Cc["@songbirdnest.com/Songbird/DisplayPane/Manager;1"]
+                    .getService(Ci.sbIDisplayPaneManager);
+    paneMgr.updateContentInfo(DISPLAY_PANE_CONTENTURL,
+                              titleString,
+                              DISPLAY_PANE_ICON);
+  },
+
+  /**
+   * \brief switchState - This function will switch the deck to the desired
+   *        display, this allows us to easily switch between the "Selected" and
+   *        "Playing" displays of the album art.
+   * \param aNewState is the index of the deck to make active.
+   */
+  switchState: function (aNewState) {
+    AlbumArt._currentState = aNewState;
+    var albumArtDeck = document.getElementById('sb-albumart-deck');
+    albumArtDeck.setAttribute("selectedIndex", this._currentState);
+    AlbumArt.setPaneTitle();
+  },
+
+  /**
+   * \brief resizeImage - This function will take care of properly resizing an
+   *        image to keep the proper aspect ratio when our display pane resizes.
+   *        We use a cache because currently <image> tags do not hold the size
+   *        information as we need it.
+   * \param aCacheIndex - An index to which image to resize, this will generally
+   *        be linked to the deck index, 0 = Selected for example.
+   */
+  resizeImage: function (aCacheIndex) {
+    var imageCache = AlbumArt._stateInfo[aCacheIndex];
+    var mImage = document.getElementById(imageCache.imageID);
+    var currentSrc = mImage.getAttribute("src");
+    
+    // Do not do anything if we don't have an image to resize
+    if (currentSrc == "") {
+      imageCache.lastImageSrc = "";
+      return;
+    }
+
+    // See if we have already cached this information
+    if (currentSrc != imageCache.lastImageSrc) {
+      // We do this to get the proper height and width, since it will not be
+      // resized when loaded with the Image() object. The <image> elements
+      // change the height and width when the image is resized to fit the
+      // content.
+      var newImg = new Image();
+      newImg.src = currentSrc;
+      imageCache.imageHeight = newImg.height;
+      imageCache.imageWidth = newImg.width;
+    }
+
+    // Do not do anything with invalid images.
+    if (imageCache.imageWidth <= 0 || imageCache.imageHeight <= 0) {
+      return;
+    }
+    
+    // Window is our displaypane, so grab the size
+    var windowWidth = window.innerWidth;
+    var windowHeight = window.innerHeight;
+    
+    // Default to the size of the image
+    var newWidth = imageCache.imageWidth;
+    var newHeight = imageCache.imageHeight;
+    
+    // Only resize if the image is bigger than the display pane (window)
+    if ( (newWidth > windowWidth) ||
+         (newHeight > windowHeight) ) {
+      // Figure out the ratios of the image and window.
+      var windowRatio = windowWidth / windowHeight;
+      var imageRatio = imageCache.imageWidth / imageCache.imageHeight;
+      
+      // If windowRatio is greater than the imageRatio then we want to resize
+      // the height, otherwize we resize the width to keep the aspect ratio.
+      // We also do not want to make the image bigger than it actually is.
+      if (imageRatio >= windowRatio) {
+        newWidth = (windowWidth <= imageCache.imageWidth ?
+                      windowWidth : imageCache.imageWidth);
+        newHeight = (imageCache.imageHeight * newWidth / imageCache.imageWidth);
+      } else {
+        newHeight = (windowHeight <= imageCache.imageHeight ?
+                      windowHeight : imageCache.imageHeight);
+        newWidth = (imageCache.imageWidth * newHeight / imageCache.imageHeight);
+      }
+    }
+
+    mImage.style.width = newWidth + "px";
+    mImage.style.height = newHeight + "px";
   },
 
   /**
@@ -115,83 +269,18 @@ var AlbumArtPaneController = {
    *        the display pane is resized.
    */
   onResize: function () {
-    if (!AlbumArtPaneController.isPaneSquare()) {
+    if (!AlbumArt.isPaneSquare()) {
       // Abort because this function will be called again.
       return;
     }
 
-    var albumArtPlayingImage = document.getElementById('sb-albumart-playing');
-    var currentSrc = albumArtPlayingImage.getAttribute("src");
-
-    // Do not do anything if we don't have an image to resize
-    if (currentSrc == "") {
-      this._lastImageSrc = "";
-      return;
-    }
-
-    // See if we have already cached this information
-    if (currentSrc != this._lastImageSrc) {
-      // We do this to get the proper height and width, since it will not be
-      // resized when loaded with the Image() object. The <image> elements
-      // change the height and width when the image is resized to fit the
-      // content.
-      var newImg = new Image();
-      newImg.src = currentSrc;
-      this._imageHeight = newImg.height;
-      this._imageWidth = newImg.width;
-    }
-
-    // Do not do anything with invalid images.
-    if (this._imageWidth <= 0 || this._imageHeight <= 0) {
-      return;
-    }
-    
-    // Window is our displaypane, so grab the size
-    var windowWidth = window.innerWidth;
-    var windowHeight = window.innerHeight;
-    
-    // Default to the size of the image
-    var newWidth = this._imageWidth;
-    var newHeight = this._imageHeight;
-    
-    // Only resize if the image is bigger than the display pane (window)
-    if ( (this._imageWidth > windowWidth) ||
-         (this._imageHeight > windowHeight) ) {
-      // Figure out the ratios of the image and window.
-      var windowRatio = windowWidth / windowHeight;
-      var imageRatio = this._imageWidth / this._imageHeight;
-      
-      // If windowRatio is greater than the imageRatio then we want to resize
-      // the height, otherwize we resize the width to keep the aspect ratio.
-      // We also do not want to make the image bigger than it actually is.
-      if (imageRatio >= windowRatio) {
-        newWidth = (windowWidth <= this._imageWidth ?
-                      windowWidth : this._imageWidth);
-        newHeight = (this._imageHeight * newWidth / this._imageWidth);
-      } else {
-        newHeight = (windowHeight <= this._imageHeight ?
-                      windowHeight : this._imageHeight);
-        newWidth = (this._imageWidth * newHeight / this._imageHeight);
-      }
-    }
-
-    albumArtPlayingImage.style.width = newWidth + "px";
-    albumArtPlayingImage.style.height = newHeight + "px";
-  },
-
-  /**
-   * \brief onImageLoad - This is called when the image is done loading, we
-   *        watch this so that we do not call the resize until we have a full
-   *        image to resize.
-   */
-  onImageLoad: function () {
-    var albumArtPlayingImage = document.getElementById('sb-albumart-playing');
-    if (albumArtPlayingImage.getAttribute("src")) {
-      // Resize the image to keep the aspect ratio.
-      AlbumArtPaneController.onResize();
+    // Resize each image in our deck even if not shown so that we don't have
+    // to resize every time the user switches the display.
+    for (var cIndex = 0; cIndex < AlbumArt._stateInfo.length; cIndex++) {
+      AlbumArt.resizeImage(cIndex);
     }
   },
-  
+
   /**
    * \brief observe - This is called when the dataremote value changes. We watch
    *        this so that we can respond to when the image is updated for an
@@ -224,7 +313,20 @@ var AlbumArtPaneController = {
       albumArtNotPlayingBox.setAttribute("hidden", true);
     }
     albumArtPlayingImage.setAttribute("src", aData);
-    AlbumArtPaneController.onResize();
+    AlbumArt.onResize();
+  },
+  
+  /**
+   * \brief toggleView- This function will switch to the next deck, and if it is
+   *        already at the last deck then it will switch to the first.
+   */
+  toggleView: function () {
+    var newState = (AlbumArt._currentState + 1);
+    if (newState >= AlbumArt._stateInfo.length) {
+      // Wrap around
+      newState = 0;
+    }
+    AlbumArt.switchState(newState);
   },
   
   /**
@@ -233,52 +335,51 @@ var AlbumArtPaneController = {
    *        message.
    */
   onLoad: function () {
-    // Set the title correctly, localize since we can not localize from the
-    // install.rdf.
-    var paneMgr = Cc["@songbirdnest.com/Songbird/DisplayPane/Manager;1"]
-                    .getService(Ci.sbIDisplayPaneManager);
-    paneMgr.updateContentInfo(DISPLAY_PANE_CONTENTURL,
-                              SBString("albumart.displaypane.title",
-                                       "Album Art"),
-                              DISPLAY_PANE_ICON);
-
-    // Listen to when an image resizes so we can keep the aspect ratio.
-    var albumArtPlayingImage = document.getElementById('sb-albumart-playing');
-    albumArtPlayingImage.addEventListener("load",
-                                          AlbumArtPaneController.onImageLoad,
-                                          false);
+    // Ensure we have the correct title and deck displayed.
+    AlbumArt.switchState(AlbumArt._currentState);
     
     // Do an initial load of the default cover since it is a big image, and then
-    // set the width and height of the image to the width and height of the
+    // set the width and height of the images to the width and height of the
     // window so the image does not stretch out of bounds on the first load.
-    this.isPaneSquare(); // Make sure we are square (this will resize)
+    AlbumArt.isPaneSquare(); // Make sure we are square (this will resize)
     var newImg = new Image();
     newImg.src = DEFAULT_COVER;
     var windowWidth = window.innerWidth;
     var windowHeight = window.innerHeight;
-    albumArtPlayingImage.style.width = windowWidth + "px";
-    albumArtPlayingImage.style.height = windowHeight + "px";
+    for (var cIndex = 0; cIndex < AlbumArt._stateInfo.length; cIndex++) {
+      var imageCache = AlbumArt._stateInfo[cIndex];
+      
+      // Listen to when an image loads so we can keep the aspect ratio.
+      var mImage = document.getElementById(imageCache.imageID);
+      mImage.addEventListener("load", AlbumArt.onResize, false);
+      mImage.style.width = windowWidth + "px";
+      mImage.style.height = windowHeight + "px";
+    }
 
     // Setup the dataremote for the now playing image.
     var createDataRemote =  new Components.Constructor(
                                   "@songbirdnest.com/Songbird/DataRemote;1",
                                   Components.interfaces.sbIDataRemote,
                                   "init");
-    this._coverBind = createDataRemote("metadata.imageURL", null);
-    this._coverBind.bindObserver(AlbumArtPaneController, false);
+    AlbumArt._coverBind = createDataRemote("metadata.imageURL", null);
+    AlbumArt._coverBind.bindObserver(AlbumArt, false);
 
     // Load the playListPlaybackService so we can monitor track changes for
     // faster image changing.
-    this._playListPlaybackService = 
+    AlbumArt._playListPlaybackService = 
                           Cc["@songbirdnest.com/Songbird/PlaylistPlayback;1"]
                             .getService(Ci.sbIPlaylistPlayback);
-    this._playListPlaybackService.addListener(AlbumArtPaneController);
+    AlbumArt._playListPlaybackService.addListener(AlbumArt);
 
     // Listen for resizes of the display pane so that we can keep the aspect
     // ratio of the image.
     window.addEventListener("resize",
-                            AlbumArtPaneController.onResize,
+                            AlbumArt.onResize,
                             false);
+    
+    // Make the title bar of the display pane act like a button so we can
+    // toggle between Now Playing and Now Selected views.
+    AlbumArt.makeTitleBarButton();
   },
   
   /**
@@ -286,19 +387,21 @@ var AlbumArtPaneController = {
    *        can shut every thing down.
    */
   onUnload: function () {
-    AlbumArtPaneController._playListPlaybackService.removeListener(AlbumArtPaneController);
-    AlbumArtPaneController._playListPlaybackService = null;
+    AlbumArt._playListPlaybackService.removeListener(AlbumArt);
+    AlbumArt._playListPlaybackService = null;
     
-    AlbumArtPaneController._coverBind.unbind();
-    AlbumArtPaneController._coverBind = null;
+    AlbumArt._coverBind.unbind();
+    AlbumArt._coverBind = null;
     
-    var albumArtPlayingImage = document.getElementById('sb-albumart-playing');
-    albumArtPlayingImage.removeEventListener("load",
-                                             AlbumArtPaneController.onImageLoad,
-                                             false);
+    // Remove the load listeners
+    for (var cIndex = 0; cIndex < AlbumArt._stateInfo.length; cIndex++) {
+      var imageCache = AlbumArt._stateInfo[cIndex];
+      var mImage = document.getElementById(imageCache.imageID);
+      mImage.removeEventListener("load", AlbumArt.onResize, false);
+    }
     
     window.removeEventListener("resize",
-                               AlbumArtPaneController.onResize,
+                               AlbumArt.onResize,
                                false);
   },
 
@@ -306,19 +409,23 @@ var AlbumArtPaneController = {
    * sbIPlaylistPlaybackListener
    ********************************/
   onStop: function() {
-    AlbumArtPaneController.observe("", "metadata.imageURL", "");
+    // Basicly clear the now playing image
+    AlbumArt.observe("", "metadata.imageURL", "");
   },
   onBeforeTrackChange: function(aItem, aView, aIndex) {
     var newImageURL = aItem.getProperty(SBProperties.primaryImageURL);
     if (!newImageURL || newImageURL == "") {
       newImageURL = DEFAULT_COVER;
     }
-    AlbumArtPaneController.observe("", "metadata.imageURL", newImageURL);
+    AlbumArt.observe("", "metadata.imageURL", newImageURL);
   },
   onTrackIndexChange: function(aItem, aView, aIndex) { },
   onBeforeViewChange: function(aView) { },
   onViewChange: function(aView) { },
   onTrackChange: function(aItem, aView, aIndex) { },
 
+  /*********************************
+   * nsISupports
+   ********************************/
   QueryInterface: XPCOMUtils.generateQI([Ci.sbIPlaylistPlaybackListener])
 };
