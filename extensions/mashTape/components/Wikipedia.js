9b9e354df788035abf72eb227c4c75f2b1f371b4
stevel <stevel@5aad4ff5-4b58-4159-ba3f-0e6328df4cad>
2008-10-03 22:08:38 +0000

    QA doesn't like British English, even though the Brits invented the damn language (hgrev: 01145a4d6522)
    git-svn-id: http://publicsvn.songbirdnest.com/client/trunk@11165 5aad4ff5-4b58-4159-ba3f-0e6328df4cad

--- extensions/mashTape/components/Wikipedia.js
+++ extensions/mashTape/components/Wikipedia.js
@@ -7,7 +7,7 @@ const Cc = Components.classes;
 const Ci = Components.interfaces;
 const Cr = Components.results;
 
-const DESCRIPTION = "mashTape Provider: Wikipedia (localised) Artist Info Provider";
+const DESCRIPTION = "mashTape Provider: Wikipedia (localized) Artist Info Provider";
 const CID         = "{a7780910-8c10-11dd-ad8b-0800200c9a66}";
 const CONTRACTID  = "@songbirdnest.com/mashTape/provider/info/WikipediaLocal;1";
 
@@ -71,7 +71,7 @@ ArtistInfo.prototype = {
 	QueryInterface: XPCOMUtils.generateQI([Ci.sbIMashTapeInfoProvider,
 			Ci.sbIMashTapeProvider]),
 
-	providerName: "Wikipedia Auto-Localised",
+	providerName: "Wikipedia Auto-Localized",
 	providerType: "info",
 	numSections: 4,
 	providerIconBio : "chrome://mashtape/content/tabs/wikipedia.png",
