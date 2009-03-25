4247363e978b12f04ca0d1d645b821c88cbfd63e
mook <mook@5aad4ff5-4b58-4159-ba3f-0e6328df4cad>
2009-03-25 20:56:51 +0000

    bug 15783 - initial drop of library management prefs v3 (r=steve)
    git-svn-id: http://publicsvn.songbirdnest.com/client/trunk@13234 5aad4ff5-4b58-4159-ba3f-0e6328df4cad

---
+++ app/content/scripts/preferences/manageMediaPrefs.js
@@ -0,0 +1,225 @@
+/* -*- Mode: Java; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set sw=2 :miv */
+/*
+//
+// BEGIN SONGBIRD GPL
+//
+// This file is part of the Songbird web player.
+//
+// Copyright(c) 2005-2009 POTI, Inc.
+// http://songbirdnest.com
+//
+// This file may be licensed under the terms of of the
+// GNU General Public License Version 2 (the "GPL").
+//
+// Software distributed under the License is distributed
+// on an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either
+// express or implied. See the GPL for the specific language
+// governing rights and limitations.
+//
+// You should have received a copy of the GPL along with this
+// program. If not, go to http://www.gnu.org/licenses/gpl.html
+// or write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+//
+// END SONGBIRD GPL
+//
+ */
+
+/**
+ * \file  manageMediaPref.js
+ * \brief JavaScript source for the media management preferences.
+ */
+
+//------------------------------------------------------------------------------
+//
+// imported JavaScript modules
+//
+//------------------------------------------------------------------------------
+
+Components.utils.import("resource://app/jsmodules/ArrayConverter.jsm");
+Components.utils.import("resource://app/jsmodules/SBJobUtils.jsm");
+Components.utils.import("resource://app/jsmodules/StringUtils.jsm");
+Components.utils.import("resource://app/jsmodules/SBProperties.jsm");
+Components.utils.import("resource://gre/modules/XPCOMUtils.jsm");
+
+//------------------------------------------------------------------------------
+//
+// Global constants
+//
+//------------------------------------------------------------------------------
+
+// Component manager defs.
+if (typeof(Cc) == "undefined")
+  var Cc = Components.classes;
+if (typeof(Ci) == "undefined")
+  var Ci = Components.interfaces;
+if (typeof(Cr) == "undefined")
+  var Cr = Components.results;
+if (typeof(Cu) == "undefined")
+  var Cu = Components.utils;
+
+
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+//
+// Manage media preference pane services.
+//
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+
+var manageMediaPrefsPane = {
+  //----------------------------------------------------------------------------
+  //
+  // Event handling services.
+  //
+  //----------------------------------------------------------------------------
+
+  /**
+   * Handles the preference pane load event.
+   */
+
+  doPaneLoad: function manageMediaPrefsPane_doPaneLoad() {
+    function forceCheck(event) {
+      const instantApply =
+        Application.prefs.getValue("browser.preferences.instantApply", true);
+      if (event.type == "dialogcancel" && !instantApply) {
+        // cancel dialog, don't do anything
+        return;
+      }
+      // XXX mook: apply preferences here
+    }
+    window.addEventListener('dialogaccept', forceCheck, false);
+    window.addEventListener('dialogcancel', forceCheck, false);
+    
+    document.getElementById("manage_media_pref_library_folder")
+            .addEventListener("change", this, false);
+    
+    // fake a change event to update some display
+    var event = document.createEvent("Events");
+    event.initEvent("change", true, true);
+    document.getElementById("manage_media_pref_library_folder")
+            .dispatchEvent(event);
+
+    this._checkForValidPref();
+  },
+
+
+  /**
+   * Handle the browse command event specified by aEvent.
+   *
+   * \param aEvent              Browse command event.
+   */
+
+  doBrowseCommand: function manageMediaPrefsPane_doBrowseCommand(aEvent) {
+    const title = SBString("prefs.manageMedia.browse.title");
+    var folderPicker = Cc["@mozilla.org/filepicker;1"]
+                         .createInstance(Ci.nsIFilePicker);
+    folderPicker.init(window, title, Ci.nsIFilePicker.modeGetFolder);
+    folderPicker.displayDirectory = this._libraryFolder;
+
+    if (folderPicker.show() == Ci.nsIFilePicker.returnOK) {
+      var selectedFolder = folderPicker.file;
+      if (false) {
+        selectedFolder = null; // use default
+      }
+      this._libraryFolder = selectedFolder;
+    }
+  },
+  
+  /**
+   * Handle the toggle management mode on/off command
+   */
+  doEnableCommand: function manageMediaPrefsPane_doEnableCommand(aEvent) {
+    var prefElem = document.getElementById(aEvent.target.getAttribute("preference"));
+    if (prefElem) {
+      prefElem.value = !prefElem.value;
+    }
+    this._checkForValidPref();
+  },
+  
+  /**
+   * Handle DOM events (mostly, pref change)
+   */
+  handleEvent: function manageMediaPrefsPane_handleEvent(aEvent) {
+    switch (String(aEvent.type)) {
+      case "change":
+        switch (aEvent.target.id) {
+          case "manage_media_pref_library_folder":
+            let prefElem = aEvent.target;
+            let fileField = document.getElementById("manage_media_library_file");
+            fileField.file = prefElem.value;
+            // we want to use the full path
+            fileField.label = prefElem.value.path;
+            // update the one for formatting as well
+            let label = document.getElementById("manage_media_format_dir_label");
+            let formatter = document.getElementById("manage_media_format_dir_formatter");
+            label.value = SBFormattedString("prefs.media_management.file_label",
+                                            [prefElem.value.leafName,
+                                             formatter.defaultSeparator]);
+            break;
+        }
+        break;
+    }
+  },
+
+  //----------------------------------------------------------------------------
+  //
+  // Internal services.
+  //
+  //----------------------------------------------------------------------------
+  
+  /**
+   * The library folder to dump things in
+   */
+  
+  get _libraryFolder() {
+    try {
+      return document.getElementById("manage_media_pref_library_folder").value;
+    } catch (ex) {
+      // invalid? boo. use default.
+      var file = Cc["@mozilla.org/file/directory_service;1"]
+                   .getService(Ci.nsIProperties)
+                   .get("Music", Ci.nsIFile);
+      // XXX Mook: this needs more thought, since it could mean we clobber files!
+      //document.getElementById("manage_media_pref_library_folder").value = file;
+      return file;
+    }
+  },
+  
+  set _libraryFolder(aValue) {
+    if (!aValue || !(aValue instanceof Ci.nsIFile)) {
+      aValue = null;
+    }
+    if (aValue) {
+      document.getElementById("manage_media_pref_library_folder").value = aValue;
+    } else {
+      // set to default folder
+      aValue = Cc["@mozilla.org/file/directory_service;1"]
+                 .getService(Ci.nsIProperties)
+                 .get("Music", Ci.nsIFile);
+      // XXX Mook: this needs more thought, since it could mean we clobber files!
+      //document.getElementById("manage_media_pref_library_folder").value = aValue;
+    }
+  },
+  
+  /**
+   * Check if the current value of the preference is valid; pops up a
+   * notification if it is not.
+   * @return true if valid, false if not.
+   */
+  _checkForValidPref: function manageMediaPrefsPane__checkForValidPref() {
+    var button = document.getElementById("manage_media_global_cmd");
+    var prefElem = document.getElementById(button.getAttribute("preference"));
+    var enabled = prefElem.value;
+    if (enabled) {
+      button.label = button.getAttribute("label-disable");
+    } else {
+      button.label = button.getAttribute("label-enable");
+    }
+    document.getElementById("manage_media_global_description").hidden = enabled;
+  },
+  
+  QueryInterface: XPCOMUtils.generateQI([Ci.nsIDOMEventListener])
+};
+
