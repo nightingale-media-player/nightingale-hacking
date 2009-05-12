23676418c46f121b31cabd344278a89b523332ae
stevel <stevel@5aad4ff5-4b58-4159-ba3f-0e6328df4cad>
2009-05-12 17:57:05 +0000

    bug 16393 - Selecting iTunes import at first run also enables watch folders (r=kreeger)
    git-svn-id: http://publicsvn.songbirdnest.com/client/trunk@13573 5aad4ff5-4b58-4159-ba3f-0e6328df4cad

--- app/content/scripts/firstRun/firstRunImportMedia.js
+++ app/content/scripts/firstRun/firstRunImportMedia.js
@@ -140,6 +140,13 @@ firstRunImportMediaSvc.prototype = {
    */
 
   saveSettings: function firstRunImportMediaSvc_saveSettings() {
+    // If the user didn't visit this first-run page, then skip saving settings
+    // or setting metrics
+    if (document.getElementById("first_run_itunes_page").getAttribute("next")
+        != "first_run_import_media_page") {
+      return;
+    }
+
     // Dispatch processing of the import settings radio group.
     var importRadioGroupElem = this._getElement("import_radiogroup");
 	var metricsImportType;
