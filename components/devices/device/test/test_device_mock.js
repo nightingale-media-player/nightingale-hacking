dc51aa6088b7882f85bbc5262efad5de4c776721
erik <erik@5aad4ff5-4b58-4159-ba3f-0e6328df4cad>
2009-06-20 00:26:57 +0000

    Fixed typo in the device device unit test. r=mook
    git-svn-id: http://publicsvn.songbirdnest.com/client/trunk@14013 5aad4ff5-4b58-4159-ba3f-0e6328df4cad

--- components/devices/device/test/test_device_mock.js
+++ components/devices/device/test/test_device_mock.js
@@ -33,7 +33,7 @@ function runTest () {
   var device = Components.classes["@songbirdnest.com/Songbird/Device/DeviceTester/MockDevice;1"]
                          .createInstance(Components.interfaces.sbIDevice);
   assertEqual(device.name, "Bob's Mock Device");
-  assertEqual(device.productame, "Mock Device");
+  assertEqual(device.productName, "Mock Device");
   
   assertEqual("" + device.id, "" + device.id, "device ID not equal");
   assertEqual("" + device.controllerId, "" + device.controllerId, "controller ID not equal");
