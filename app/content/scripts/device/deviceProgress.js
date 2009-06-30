8b39b97a480395c00f9784e0c7cecbcf06fcc5b1
stevel <stevel@5aad4ff5-4b58-4159-ba3f-0e6328df4cad>
2009-06-30 15:23:35 +0000

    bug 16951 - Restore Eject/Info buttons back to device summary pages (r=kreeger 'that dog will hunt')
    git-svn-id: http://publicsvn.songbirdnest.com/client/trunk@14114 5aad4ff5-4b58-4159-ba3f-0e6328df4cad

--- app/content/scripts/device/deviceProgress.js
+++ app/content/scripts/device/deviceProgress.js
@@ -220,6 +220,7 @@ var DPW = {
   _progressInfoBox: null,
   _syncButton: null,
   _cancelButton: null,
+  _ejectInfoButtons: null,
   _finishButton: null,
   _progressMeter: null,
   _progressTextLabel: null,
@@ -246,8 +247,9 @@ var DPW = {
     }
 
     // Get some widget elements.
-    this._progressInfoBox = this._getElement("progress_information_box");
-    this._syncButton   = this._getElement("sync_operation_button");
+    this._progressInfoBox  = this._getElement("progress_information_box");
+    this._syncButton       = this._getElement("sync_operation_button");
+    this._ejectInfoButtons = this._getElement("ejectinfo_button_box");
     this._cancelButton = this._getElement("cancel_operation_button");
     this._finishButton = this._getElement("finish_progress_button");
     this._progressMeter = this._getElement("progress_meter");
@@ -293,6 +295,7 @@ var DPW = {
     this._progressInfoBox = null;
     this._idleBox = null;
     this._syncButton   = null;
+    this._ejectInfoButtons = null;
     this._cancelButton = null;
     this._finishButton = null;
     this._progressMeter = null;
@@ -344,6 +347,7 @@ var DPW = {
 
     // Only show sync button if not showing progress.
     this._syncButton.hidden = this._showProgress;
+    this._ejectInfoButtons.hidden = this._showProgress;
 
     // Set cancel and hide button hidden property.
     this._cancelButton.hidden = cancelButtonHidden;
