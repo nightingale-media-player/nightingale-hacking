2c0589e0557f110dfdbc73dc65870ded87083709
nick <nick@5aad4ff5-4b58-4159-ba3f-0e6328df4cad>
2009-08-04 20:24:05 +0000

    Fixing bug 17443 - [cdrip] Create CD Rip preference pane. r=stevo.
    git-svn-id: http://publicsvn.songbirdnest.com/client/trunk@14500 5aad4ff5-4b58-4159-ba3f-0e6328df4cad

---
+++ app/content/scripts/preferences/cdripPrefs.js
@@ -0,0 +1,67 @@
+/*
+//
+// BEGIN SONGBIRD GPL
+//
+// This file is part of the Songbird web player.
+//
+// Copyright(c) 2005-2009 POTI, Inc.
+// http://songbirdnest.com
+//
+// This file may be licensed under the terms of of the
+// GNU General Public License Version 2 (the "GPL").
+//
+// Software distributed under the License is distributed
+// on an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either
+// express or implied. See the GPL for the specific language
+// governing rights and limitations.
+//
+// You should have received a copy of the GPL along with this
+// program. If not, go to http://www.gnu.org/licenses/gpl.html
+// or write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+//
+// END SONGBIRD GPL
+//
+*/
+
+//
+// \file cdripPrefs.js
+// \brief Javascript source for the CD Rip preferences UI.
+//
+
+if (typeof(Cc) == "undefined")
+  var Cc = Components.classes;
+if (typeof(Ci) == "undefined")
+  var Ci = Components.interfaces;
+if (typeof(Cr) == "undefined")
+  var Cr = Components.results;
+if (typeof(Cu) == "undefined")
+  var Cu = Components.utils;
+
+//------------------------------------------------------------------------------
+//
+// CD Rip preference pane UI controller.
+//
+//------------------------------------------------------------------------------
+
+var CDRipPrefsUI =
+{
+  initialize: function CDRipPrefsUI_initialize() {
+    //
+    // TODO: Write Me - see bug 17444.
+    //
+  },
+};
+
+//------------------------------------------------------------------------------
+//
+// CD Rip preference pane services.
+//
+//------------------------------------------------------------------------------
+
+var CDRipPrefsPane =
+{
+  doPaneLoad: function CDRipPrefsPane_doPaneLoad() {
+    CDRipPrefsUI.initialize();
+  }
+}
