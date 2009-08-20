2e7abe1e3cd394667bb1a58ee6c5244546f49b4c
nick <nick@5aad4ff5-4b58-4159-ba3f-0e6328df4cad>
2009-08-20 22:19:32 +0000

    Fixing bug 17645 - [cdrip] Land the mock cd controller 9000 extension in the SVN tree. r=redfive.
    git-svn-id: http://publicsvn.songbirdnest.com/client/trunk@14599 5aad4ff5-4b58-4159-ba3f-0e6328df4cad

---
+++ extensions/mockcdcontroller/chrome/content/main.js
@@ -0,0 +1,63 @@
+/*
+ //
+// BEGIN SONGBIRD GPL
+//
+// This file is part of the Songbird web player.
+//
+// Copyright(c) 2005-2009 POTI, Inc.
+// http://songbirdnest.com
+//
+// This file may be licensed under the terms of of the
+// GNU General Public License Version 2 (the "GPL").
+//
+// Software distributed under the License is distributed
+// on an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either
+// express or implied. See the GPL for the specific language
+// governing rights and limitations.
+//
+// You should have received a copy of the GPL along with this
+// program. If not, go to http://www.gnu.org/licenses/gpl.html
+// or write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+//
+// END SONGBIRD GPL
+//
+*/
+
+// Make a namespace.
+if (typeof MockCDController == 'undefined') {
+  var MockCDController = {};
+}
+
+/**
+ * UI controller that is loaded into the main player window
+ */
+MockCDController.Controller =
+{
+  onLoad: function()
+  {
+    var showCommand =
+      document.getElementById("mockcdcontroller-showcontroller-cmd");
+
+    var self = this;
+    showCommand.addEventListener(
+      "command",
+      function() { self.showCDControllerPane(); },
+      false);
+
+    setTimeout(function() {self.showCDControllerPane(); }, 200);
+  },
+
+  showCDControllerPane: function()
+  {
+    // TODO: Show this thang
+    window.openDialog("chrome://mockcdcontroller/content/mockCDControllerDialog.xul",
+                      "cd-controller-pane",
+                      "chrome,centerscreen,resizable=false");
+  }
+};
+
+window.addEventListener(
+  "load",
+  function(e) { MockCDController.Controller.onLoad(e); },
+  false)
