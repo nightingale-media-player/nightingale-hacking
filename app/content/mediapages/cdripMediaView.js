fa8466902fd190aa809910034fcde3e4cdacddfe
stevel <stevel@5aad4ff5-4b58-4159-ba3f-0e6328df4cad>
2009-08-31 22:40:33 +0000

    bug 17716 - don't attempt to write metadata to files for cdrip tracks (r=specialed)
    git-svn-id: http://publicsvn.songbirdnest.com/client/trunk@14708 5aad4ff5-4b58-4159-ba3f-0e6328df4cad

--- app/content/mediapages/cdripMediaView.js
+++ app/content/mediapages/cdripMediaView.js
@@ -1,28 +1,26 @@
 /*
-//
-// BEGIN SONGBIRD GPL
-//
-// This file is part of the Songbird web player.
-//
-// Copyright(c) 2005-2009 POTI, Inc.
-// http://songbirdnest.com
-//
-// This file may be licensed under the terms of of the
-// GNU General Public License Version 2 (the "GPL").
-//
-// Software distributed under the License is distributed
-// on an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either
-// express or implied. See the GPL for the specific language
-// governing rights and limitations.
-//
-// You should have received a copy of the GPL along with this
-// program. If not, go to http://www.gnu.org/licenses/gpl.html
-// or write to the Free Software Foundation, Inc.,
-// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
-//
-// END SONGBIRD GPL
-//
-*/
+ *=BEGIN SONGBIRD GPL
+ *
+ * This file is part of the Songbird web player.
+ *
+ * Copyright(c) 2005-2009 POTI, Inc.
+ * http://www.songbirdnest.com
+ *
+ * This file may be licensed under the terms of of the
+ * GNU General Public License Version 2 (the ``GPL'').
+ *
+ * Software distributed under the License is distributed
+ * on an ``AS IS'' basis, WITHOUT WARRANTY OF ANY KIND, either
+ * express or implied. See the GPL for the specific language
+ * governing rights and limitations.
+ *
+ * You should have received a copy of the GPL along with this
+ * program. If not, go to http://www.gnu.org/licenses/gpl.html
+ * or write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ *=END SONGBIRD GPL
+ */
 
 const SB_NS = 'http://songbirdnest.com/data/1.0#';
 
@@ -345,7 +343,11 @@ window.cdripController =
 
     // Clear the playlist filters. This is a filter-free view.
     this._clearFilterLists();
-    
+
+    // Set a flag so sb-playlist won't try to writeback metadata to files
+    this._mediaListView.mediaList.setProperty(
+      "http://songbirdnest.com/data/1.0#dontWriteMetadata", 1);
+
     // Setup our columns for the CD View
     this._mediaListView.mediaList.setProperty(SBProperties.columnSpec,
       "http://songbirdnest.com/data/1.0#shouldRip 25 " +
