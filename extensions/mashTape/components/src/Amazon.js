fc9525ed2f9f9ce1fcb360547bb232ff7004496d
stevel <stevel@5aad4ff5-4b58-4159-ba3f-0e6328df4cad>
2009-09-25 21:57:48 +0000

    bug 18201 - sign Amazon API requests with AWS key
    git-svn-id: http://publicsvn.songbirdnest.com/client/trunk@15018 5aad4ff5-4b58-4159-ba3f-0e6328df4cad

--- extensions/mashTape/components/src/Amazon.js
+++ extensions/mashTape/components/src/Amazon.js
@@ -10,10 +10,48 @@ const DESCRIPTION = "mashTape Provider: Amazon Reviews Provider";
 const CID         = "{96a0c280-b4d3-11dd-ad8b-0800200c9a66}";
 const CONTRACTID  = "@songbirdnest.com/mashTape/provider/review/Amazon;1";
 
+const AWSACCESSKEYID = "1DQMN6E25AX32KXFT902";
+const AWSSECRETKEYID = "l981L1Q2PzJpFVLrCnJakS1+U1IvsD6ggVQDDO9w";
+
 function Amazon() {
 	this.wrappedJSObject = this;
 	Components.utils.import("resource://mashtape/mtUtils.jsm");
 }
+
+function _buildQuery(queryParams) {
+  // Get the timestamp
+  var timestamp = new Date();
+  timestamp = timestamp.toISO8601String(5);
+  queryParams["Timestamp"] = timestamp;
+  queryParams["AWSAccessKeyId"] = AWSACCESSKEYID;
+  queryParams["Service"] = "AWSECommerceService";
+
+  // Sort the array of query parameters
+  var keys = new Array();
+  for (var key in queryParams) {
+    keys.push(key);
+  }
+  keys = keys.sort();
+
+  // Build the query string
+  var queryString = "";
+  for each (var key in keys)
+    queryString += "&" + key + "=" + encodeURIComponent(queryParams[key]);
+  // Slice off the first "&"
+  queryString = queryString.slice(1);
+
+  // Sign it
+  var stringToSign = "GET\necs.amazonaws.com\n/onca/xml\n";
+  stringToSign += queryString;
+  var shaObj = new jsSHA(stringToSign, "ASCII");
+  var signature = shaObj.getHMAC(AWSSECRETKEYID, "ASCII", "SHA-256", "B64");
+
+  // Make the URL
+  var url = "http://ecs.amazonaws.com/onca/xml?" + queryString +
+            "&Signature=" + encodeURIComponent(signature + "=");
+  return url;
+}
+
 
 Amazon.prototype.constructor = Amazon;
 Amazon.prototype = {
@@ -35,8 +73,15 @@ Amazon.prototype = {
 		// Fire off an ItemSearch to get ASINs to do ItemLookups on
 		var searchReq = Cc["@mozilla.org/xmlextras/xmlhttprequest;1"]
 			.createInstance(Ci.nsIXMLHttpRequest);
-		var url = "http://ecs.amazonaws.com/onca/xml?Service=AWSECommerceService&AWSAccessKeyId=1DQMN6E25AX32KXFT902&Operation=ItemSearch&SearchIndex=Music&Artist=" + encodeURIComponent(artist) + "&Title=" + encodeURIComponent(album);
-		mtUtils.log("Amazon", "ItemSearch URL: " + url);
+
+    var queryParams = new Array();
+    queryParams["Artist"] = artist;
+    queryParams["Title"] = album;
+    queryParams["Operation"] = "ItemSearch";
+    queryParams["SearchIndex"] = "Music";
+    var url = _buildQuery(queryParams);
+
+    mtUtils.log("Amazon", "ItemSearch URL: " + url);
 		var reviewFn = this.getReviews;
 		searchReq.open("GET", url, true);
 		searchReq.onreadystatechange = function(ev) {
@@ -80,7 +125,17 @@ Amazon.prototype = {
 		var asin = data.asins[idx];
 		var reviewReq = Cc["@mozilla.org/xmlextras/xmlhttprequest;1"]
 			.createInstance(Ci.nsIXMLHttpRequest);
-		var url = "http://ecs.amazonaws.com/onca/xml?Service=AWSECommerceService&Version=2005-03-23&Operation=ItemLookup&SubscriptionId=1BTS253HGNBMVK0CP6G2%20%20&ItemId=" + asin + "&ResponseGroup=EditorialReview,Reviews&ReviewSort=-HelpfulVotes";
+
+    var queryParams = new Array();
+    queryParams["Version"] = "2005-03-23";
+    queryParams["Operation"] = "ItemLookup";
+    queryParams["SubscriptionId"] = "1BTS253HGNBMVK0CP6G2%20%20";
+    queryParams["ItemId"] = asin;
+    queryParams["ResponseGroup"] = "EditorialReview,Reviews";
+    queryParams["ReviewSort"] = "-HelpfulVotes";
+    var url = _buildQuery(queryParams);
+
+    dump("url: " + url + "\n");
 		mtUtils.log("Amazon", "ItemLookup (" + asin + ") URL: " + url);
 		reviewReq.open("GET", url, true);
 		reviewReq.onreadystatechange = function(ev) {
