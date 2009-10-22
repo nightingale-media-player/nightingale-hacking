d66a40e6345eafa3836c6865fb5198b41bc73138
msmith <msmith@5aad4ff5-4b58-4159-ba3f-0e6328df4cad>
2009-10-22 21:40:47 +0000

    Bug 18385 - Revert revisions 15514 and 15515, since they break the app badly.
    git-svn-id: http://publicsvn.songbirdnest.com/client/trunk@15516 5aad4ff5-4b58-4159-ba3f-0e6328df4cad

--- components/library/localdatabase/src/sbLocalDatabaseMediaListView.h
+++ components/library/localdatabase/src/sbLocalDatabaseMediaListView.h
@@ -159,11 +159,9 @@ private:
   nsresult ShouldCauseInvalidation(sbIPropertyArray* aProperties,
                                    PRBool* aShouldCauseInvalidation);
 
-  nsresult UpdateListener(PRBool aRemoveListener);
+  nsresult UpdateListener(PRBool aRemoveListener = PR_TRUE);
 
   void NotifyListenersInternal(ListenerFunc aListenerFunc);
-
-  nsresult SetSortInternal(sbIPropertyArray* aSort);
 
 private:
   nsRefPtr<sbLocalDatabaseLibrary> mLibrary;
@@ -219,6 +217,10 @@ private:
 
   // True when we should invalidate when batching ends
   PRPackedBool mInvalidatePending;
+
+  // If true, changing the sort/search/filter will not update the view array
+  // configuration or listener settings
+  PRPackedBool mInitializing;
 
   nsRefPtr<sbLocalDatabaseMediaListViewSelection> mSelection;
   /**
