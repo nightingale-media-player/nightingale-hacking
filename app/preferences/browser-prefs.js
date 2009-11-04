df642357f09d9bacbc8eedfad79e26e24582e5d5
palant <palant@5aad4ff5-4b58-4159-ba3f-0e6328df4cad>
2009-11-04 09:04:07 +0000

    Bug 18684 - Need browser.zoom.updateBackgroundTabs pref (XULRunner 1.9.2). r=erik
    git-svn-id: http://publicsvn.songbirdnest.com/client/trunk@15667 5aad4ff5-4b58-4159-ba3f-0e6328df4cad

--- app/preferences/browser-prefs.js
+++ app/preferences/browser-prefs.js
@@ -1,3 +1,4 @@
 /* These preferences are from firefox.js */
 pref("browser.zoom.siteSpecific", true);
+pref("browser.zoom.updateBackgroundTabs", true);
 pref("browser.EULA.override", true); /* songbird has its own EULA dialog */
