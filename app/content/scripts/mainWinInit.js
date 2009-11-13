bd0987c36b1f2be75ac8cd1ef7c5131da70311d1
erik <erik@5aad4ff5-4b58-4159-ba3f-0e6328df4cad>
2009-11-13 01:04:41 +0000

    Bug 17867, patch 13000 - Encapsulated global hotkey preferences inside the hotkey services component. r=aus
    git-svn-id: http://publicsvn.songbirdnest.com/client/trunk@15797 5aad4ff5-4b58-4159-ba3f-0e6328df4cad

--- app/content/scripts/mainWinInit.js
+++ app/content/scripts/mainWinInit.js
@@ -86,7 +86,13 @@ function SBInitialize()
     // See bug #5185.
     setTimeout("setMinMaxCallback()", 25);
 
+/*ZZZ*/
+if (0) {
+/*ZZZ*/
     initJumpToFileHotkey();
+/*ZZZ*/
+}
+/*ZZZ*/
 
     if (window.addEventListener)
       window.addEventListener("keydown", checkQuitKey, true);
@@ -109,8 +115,14 @@ function SBUninitialize()
   window.gServicePane = null;
   window.gBrowser = null;
 
+/*ZZZ*/
+if (0) {
+/*ZZZ*/
   resetJumpToFileHotkey();
   closeJumpTo();
+/*ZZZ*/
+}
+/*ZZZ*/
 
   resetMinMaxCallback();
 }
--- components/integration/src/sbGlobalHotkeys.js
+++ components/integration/src/sbGlobalHotkeys.js
@@ -371,9 +371,15 @@ sbDefaultGlobalHotkeyService.prototype =
     SBDataSetStringValue("globalhotkey.8.key.readable",    "stop");
     SBDataSetStringValue("globalhotkey.8.action",          "playback.stop");
 
+/*ZZZ*/
+if (0) {
+/*ZZZ*/
     SBDataSetStringValue("globalhotkey.9.key",            "meta-$74");
     SBDataSetStringValue("globalhotkey.9.key.readable",   metaKey + "-J");
     SBDataSetStringValue("globalhotkey.9.action",         "jumpto.open");
+/*ZZZ*/
+}
+/*ZZZ*/
   },
   
   /**
