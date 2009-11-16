b31b9b9247697a25764d6bb613ab99fd8f6e7857
steven <steven@5aad4ff5-4b58-4159-ba3f-0e6328df4cad>
2009-11-16 20:00:26 +0000

    Bug 18660 - Make the icons in the Device information panels become clickable when an error occurs for the media type that failed to sync. (r=Mook)
    git-svn-id: http://publicsvn.songbirdnest.com/client/trunk@15860 5aad4ff5-4b58-4159-ba3f-0e6328df4cad

--- app/content/scripts/device/deviceInfoPanel.js
+++ app/content/scripts/device/deviceInfoPanel.js
@@ -74,6 +74,7 @@ var DIPW = {
   _deviceID: null,
   _panelBar: null,
   _lastOperation: null,
+  _deviceErrorMonitor : null,
 
   /**
    * \brief Initialize the device info panel services for the device info panel
@@ -95,7 +96,15 @@ var DIPW = {
 
     // Initialize the device services.
     this._deviceInitialize();
+
+    // Grab the device error monitor service so we can check for errors on this
+    // device when we need to.
+    this._deviceErrorMonitor =
+                          Cc["@songbirdnest.com/device/error-monitor-service;1"]
+                            .getService(Ci.sbIDeviceErrorMonitor);
+
     this._update();
+
   },
 
   /**
@@ -103,6 +112,10 @@ var DIPW = {
    */
 
   finalize: function DIPW_finalize() {
+    // Clean up the device error monitor
+    if (this._deviceErrorMonitor) {
+      this._deviceErrorMonitor = null;
+    }
     // Finalize the device services.
     this._deviceFinalize();
 
@@ -127,22 +140,31 @@ var DIPW = {
    *  react to the current state of the device. There needs to be some updates
    *  with the device event states before that can be completed, so for now we
    *  are just fudging it.
+   *
+   * Later we will have the following states:
+   *  STATE_PREPARING_SYNC          // Do nothing.
+   *  STATE_SYNCING                 // Set up what types we are syncing, show panel.
+   *    STATE_PREPARING_SYNC_MUSIC  // Put music panel in sync state.
+   *      STATE_SYNCING_MUSIC       // Do nothing
+   *    STATE_PREPARING_SYNC_VIDEO  // Finish up music, Put video panel in sync state.
+   *      STATE_SYNCING_VIDEO       // Do nothing
+   *    STATE_SYNCING_PLAYLISTS     // Finish up video, Add complete panel.
+   *  STATE_IDLE                    // Finish up complete panel.
    */
 
   _update: function DIPW__update() {
     // Get the current device state.
     var deviceState = this._device.state;
-
     switch (deviceState) {
+      // case Ci.sbIDevice.STATE_PREPARING_SYNC: // Ignore
       case Ci.sbIDevice.STATE_SYNCING:
         // If we are syncing set up the panels for the types we are going to
         // sync.
 
-        // Setup the panel (Only Music for now)
+        // Setup the panel
         this._panelBar.removeAllPanels();
-        this._panelBar.appendPanel(SBString("device.infoPanel.sync_music"),
-                                   "chrome://songbird/skin/device/music.png",
-                                   true);
+        // "audio" will be changed to the contentType later on.
+        this._updateMediaInfoPanelState("audio", deviceState);
 
         // Make sure we are visible
         if (!this._panelBar.isShown)
@@ -150,29 +172,178 @@ var DIPW = {
 
         this._lastOperation = Ci.sbIDevice.STATE_SYNCING;
         break;
+
       case Ci.sbIDevice.STATE_IDLE:
         // If we are IDLE after a SYNC then it is ok to disconnect
+        // Later change this to STATE_SYCNING_PLAYLISTS
         if (this._lastOperation != Ci.sbIDevice.STATE_SYNCING)
           break;
 
-        // First update the music panel if it exists
-        var musicPanel = this._panelBar.getActivePanel();
-        if (musicPanel) {
-          musicPanel.label = SBString("device.infoPanel.sync_music_complete");
-          musicPanel.icon = "chrome://songbird/skin/info-bar/check.png";
-        }
+        // This is here temporarly until we have the VIDEO/MUSIC sync states.
+        this._updateMediaInfoPanelState("audio", deviceState);
 
         // Now add a Ok to disconnect panel
-        this._panelBar.appendPanel(SBString("device.status.progress_complete"),
-                                   "chrome://songbird/skin/info-bar/check.png",
+        this._updateMediaInfoPanel("complete",
+                                   SBString("device.status.progress_complete"),
+                                   "success",
                                    true);
         this._lastOperation = null;
         break;
+      // TODO: Add other states
+      // case Ci.sbIDevice.STATE_PREPARING_SYNC_MUSIC:
+      // case Ci.sbIDevice.STATE_SYNCING_MUSIC:
+      // case Ci.sbIDevice.STATE_PREPARING_SYNC_VIDEO:
+      // case Ci.sbIDevice.STATE_SYNCING_VIDEO:
+      // case Ci.sbIDevice.STATE_SYNCING_PLAYLISTS:
+
       default:
         this._lastOperation = null;
         if (this._panelBar.isShown)
           this._panelBar.animateOut();
         break;
+    }
+  },
+
+  /**
+   * \brief Remove a panel of a particular media type, we should only ever have
+   *        one panel of each type so this will ensure that doubles are removed.
+   *
+   * \param aMediaType             Type of media panel (contentType).
+   */
+
+  _removeMediaInfoPanel: function DIPW__removeMediaInfoPanel(aMediaType) {
+    var mediaPanel;
+    while ((mediaPanel = this._findMediaInfoPanel(aMediaType)) != null) {
+      var index = mediaPanel.index;
+      this._panelBar.removePanelAt(index);
+    }
+  },
+
+  /**
+   * \brief Updates a media panel with the proper state.
+   *
+   * \param aMediaType             Type of media panel (contentType).
+   * \param aState                 State of the device for this media type.
+   */
+
+  _updateMediaInfoPanelState: function DIPW__updateMediaInfoPanelState(aMediaType,
+                                                                       aState) {
+    var baseString = "device.infoPanel.sync_" + aMediaType;
+
+    switch (aState) {
+      case Ci.sbIDevice.STATE_IDLE:
+        var hasErrors = this._checkForDeviceErrors(aMediaType);
+        if (hasErrors) {
+          var mediaPanel = this._updateMediaInfoPanel(aMediaType,
+                                     SBString(baseString + "_errors"),
+                                     "error",
+                                     true);
+          mediaPanel.addEventListener("sbInfoPanel-icon-click",
+                                      this,
+                                      false);
+          mediaPanel.setAttribute("icon-clickable", true);
+        }
+        else {
+          var mediaPanel = this._updateMediaInfoPanel(aMediaType,
+                                     SBString(baseString + "_complete"),
+                                     "success",
+                                     true);
+          mediaPanel.removeAttribute("icon-clickable");
+        }
+        break;
+      case Ci.sbIDevice.STATE_SYNCING:
+        var mediaPanel = this._updateMediaInfoPanel(aMediaType,
+                                   SBString(baseString),
+                                   "sync",
+                                   true);
+        mediaPanel.removeAttribute("icon-clickable");
+        break;
+    }
+  },
+
+  /**
+   * \brief Updates a media panel information.
+   *
+   * \param aMediaType             Type of media panel (contentType).
+   * \param aMediaLabel            Text to display in the panel.
+   * \param aMediaIcon             Icon to display in the panel.
+   * \param aIsActive              Make this panel active or not.
+   */
+
+  _updateMediaInfoPanel: function DIPW__updateMediaInfoPanel(aMediaType,
+                                                             aMediaLabel,
+                                                             aSyncState,
+                                                             aIsActive) {
+    var mediaPanel = this._findMediaInfoPanel(aMediaType);
+
+    if (mediaPanel == null) {
+      mediaPanel = this._panelBar.appendPanel(aMediaLabel, "", aIsActive);
+      mediaPanel.setAttribute("mediatype", aMediaType);
+    }
+    else {
+      mediaPanel.label = aMediaLabel;
+      mediaPanel.active = aIsActive;
+    }
+
+    if (aSyncState)
+      mediaPanel.setAttribute("state", aSyncState);
+    else
+      mediaPanel.removeAttribute("state");
+
+    return mediaPanel;
+  },
+
+  /**
+   * \brief Finds a panel of a particular type in the panel bar. Normally there
+   *        should only be one or none of each type.
+   *
+   * \param aMediaType             Type of media panel (contentType).
+   */
+
+  _findMediaInfoPanel: function DIPW__findMediaInfoPanel(aMediaType) {
+    var panelCount = this._panelBar.panelCount;
+    for (var index = 0; index < panelCount; index++) {
+      var aPanel = this._panelBar.getPanelAtIndex(index);
+      if (aPanel &&
+          aPanel.getAttribute("mediatype") == aMediaType)
+        return aPanel;
+    }
+    return null;
+  },
+
+  /**
+   * \brief Handles events. Currently we only listen for click events on the
+   *        icons in the info panels.
+   *
+   * \param aEvent             nsIDOMEvent of the event that occured.
+   */
+
+  handleEvent: function DIPW_handleEvent(aEvent) {
+    // Check if this is a panel icon click event.
+    if (aEvent.type == "sbInfoPanel-icon-click") {
+      var target = aEvent.target
+      // Make sure our target has a media type
+      if (target.hasAttribute("mediatype")) {
+        var aMediaType = target.getAttribute("mediatype");
+        if (this._checkForDeviceErrors(aMediaType)) {
+          // TODO: Update this with the new function in deviceErrorMonitor when
+          // it has been added.
+          var errorItems =
+            this._deviceErrorMonitor.getErrorsForDevice(this._device);
+          WindowUtils.openDialog
+            (window,
+             "chrome://songbird/content/xul/device/deviceErrorDialog.xul",
+             "device_error_dialog",
+             "chrome,centerscreen",
+             false,
+             [ "", this._device, errorItems, "syncing" ],
+             null);
+
+          // We handled this event so stop the propagation
+          aEvent.stopPropagation();
+          aEvent.preventDefault();
+        }
+      }
     }
   },
 
@@ -245,6 +416,22 @@ var DIPW = {
   },
 
   /**
+   * \brief Check if the device has errors for a particular type of media
+   * TODO: Actually filter out the different types once we can in the
+   *      sbIDeviceErrorMonitor.
+   */
+
+  _checkForDeviceErrors: function DIPW__checkForDeviceErrors(aMediaType) {
+    var hasErrors = false;
+    try {
+      hasErrors = this._deviceErrorMonitor.deviceHasErrors(this._device);
+    } catch (err) {
+      Cu.reportError(err);
+    }
+    return hasErrors;
+  },
+
+  /**
    * \brief Listener for device events.
    *
    * \sa sbIDeviceEvent.idl
@@ -255,7 +442,6 @@ var DIPW = {
       case Ci.sbIDeviceEvent.EVENT_DEVICE_STATE_CHANGED:
         this._update();
         break;
-
     }
   }
 
