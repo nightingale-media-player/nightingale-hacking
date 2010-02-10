d8099e9d208f28da59c883873a66a6a4465e0155
nick <nick@5aad4ff5-4b58-4159-ba3f-0e6328df4cad>
2010-02-10 01:05:27 +0000

    Fixing whitespace issue in the itunesagent - no direct code change. r=stevel
    git-svn-id: http://publicsvn.songbirdnest.com/client/trunk@17533 5aad4ff5-4b58-4159-ba3f-0e6328df4cad

--- tools/itunesagent/main.cpp
+++ tools/itunesagent/main.cpp
@@ -70,7 +70,7 @@ int main(int argc, char * argv[]) {
   }
   
   if (processor->TaskFileExists()) {
-   error = processor->WaitForiTunes();
+    error = processor->WaitForiTunes();
     if (!error) {
       error = processor->ProcessTaskFile();
     }
