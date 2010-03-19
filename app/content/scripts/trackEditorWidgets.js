eaba4c9301a043dc1affaba57509984bb38c6299
alfred <alfred@5aad4ff5-4b58-4159-ba3f-0e6328df4cad>
2010-03-19 17:20:49 +0000

    bug 20549 - [video metadata] edited genre metadata for video does not display (r=mook)
    git-svn-id: http://publicsvn.songbirdnest.com/client/trunk@17970 5aad4ff5-4b58-4159-ba3f-0e6328df4cad

--- app/content/scripts/trackEditorWidgets.js
+++ app/content/scripts/trackEditorWidgets.js
@@ -1,27 +1,25 @@
 /*
-//
-// BEGIN SONGBIRD GPL
-// 
-// This file is part of the Songbird web player.
-//
-// Copyright(c) 2005-2008 POTI, Inc.
-// http://songbirdnest.com
-// 
-// This file may be licensed under the terms of of the
-// GNU General Public License Version 2 (the "GPL").
-// 
-// Software distributed under the License is distributed 
-// on an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either 
-// express or implied. See the GPL for the specific language 
-// governing rights and limitations.
-//
-// You should have received a copy of the GPL along with this 
-// program. If not, go to http://www.gnu.org/licenses/gpl.html
-// or write to the Free Software Foundation, Inc., 
-// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
-// 
-// END SONGBIRD GPL
-//
+ *=BEGIN SONGBIRD GPL
+ *
+ * This file is part of the Songbird web player.
+ *
+ * Copyright(c) 2005-2010 POTI, Inc.
+ * http://www.songbirdnest.com
+ *
+ * This file may be licensed under the terms of of the
+ * GNU General Public License Version 2 (the ``GPL'').
+ *
+ * Software distributed under the License is distributed
+ * on an ``AS IS'' basis, WITHOUT WARRANTY OF ANY KIND, either
+ * express or implied. See the GPL for the specific language
+ * governing rights and limitations.
+ *
+ * You should have received a copy of the GPL along with this
+ * program. If not, go to http://www.gnu.org/licenses/gpl.html
+ * or write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ *=END SONGBIRD GPL
  */
 
 if (typeof(Ci) == "undefined")
@@ -584,7 +582,18 @@ TrackEditorTextbox.prototype = {
     if (this._element.getAttribute("autocompletesearch")
                      .indexOf("library-distinct-properties") >= 0) {
       var defvals = this._element.getAttribute("defaultdistinctproperties");
-      var param = this.property + ";" + libraryGuid;
+      var param = this.property;
+
+      // Get content type for genre property and set to auto complete param.
+      if (this.property == SBProperties.genre) {
+        var LSP = Cc["@songbirdnest.com/servicepane/library;1"]
+                    .getService(Ci.sbILibraryServicePaneService);
+        var type =
+          LSP.getNodeContentTypeFromMediaListView(TrackEditor.mediaListView);
+        if (type)
+          param += "$" + type;
+      }
+      param += ";" + libraryGuid;
       if (defvals && defvals != "") {
         param += ";" + defvals;
       }
