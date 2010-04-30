79e979e337d7ce3bc9e7288275d16ef5d48df434
alfred <alfred@5aad4ff5-4b58-4159-ba3f-0e6328df4cad>
2010-04-30 03:51:14 +0000

    bug 19925 - backout the commit to fix the unit test failure.
    git-svn-id: http://publicsvn.songbirdnest.com/client/trunk@18449 5aad4ff5-4b58-4159-ba3f-0e6328df4cad

--- components/moz/strings/components/src/sbStringsModule.cpp
+++ components/moz/strings/components/src/sbStringsModule.cpp
@@ -1,28 +1,30 @@
 /* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
 /* vim: set sw=2 :miv */
 /*
- *=BEGIN SONGBIRD GPL
- *
- * This file is part of the Songbird web player.
- *
- * Copyright(c) 2005-2010 POTI, Inc.
- * http://www.songbirdnest.com
- *
- * This file may be licensed under the terms of of the
- * GNU General Public License Version 2 (the ``GPL'').
- *
- * Software distributed under the License is distributed
- * on an ``AS IS'' basis, WITHOUT WARRANTY OF ANY KIND, either
- * express or implied. See the GPL for the specific language
- * governing rights and limitations.
- *
- * You should have received a copy of the GPL along with this
- * program. If not, go to http://www.gnu.org/licenses/gpl.html
- * or write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
- *
- *=END SONGBIRD GPL
- */
+//
+// BEGIN SONGBIRD GPL
+//
+// This file is part of the Songbird web player.
+//
+// Copyright(c) 2005-2008 POTI, Inc.
+// http://songbirdnest.com
+//
+// This file may be licensed under the terms of of the
+// GNU General Public License Version 2 (the "GPL").
+//
+// Software distributed under the License is distributed
+// on an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either
+// express or implied. See the GPL for the specific language
+// governing rights and limitations.
+//
+// You should have received a copy of the GPL along with this
+// program. If not, go to http://www.gnu.org/licenses/gpl.html
+// or write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+//
+// END SONGBIRD GPL
+//
+*/
 
 //------------------------------------------------------------------------------
 //------------------------------------------------------------------------------
@@ -46,7 +48,6 @@
 // Local imports.
 #include "sbStringBundleService.h"
 #include "sbStringMap.h"
-#include "sbCharsetDetector.h"
 
 // Mozilla imports.
 #include <nsICategoryManager.h>
@@ -135,9 +136,6 @@ sbStringBundleServiceUnregister(nsIComponentManager*         aCompMgr,
 
 NS_GENERIC_FACTORY_CONSTRUCTOR(sbStringMap)
 
-// Songbird charset detect utilities defs.
-NS_GENERIC_FACTORY_CONSTRUCTOR(sbCharsetDetector)
-
 //------------------------------------------------------------------------------
 //
 // Songbird strings components module registration services.
@@ -162,13 +160,6 @@ static nsModuleComponentInfo sbStringsComponents[] =
     SB_STRINGMAP_CID,
     SB_STRINGMAP_CONTRACTID,
     sbStringMapConstructor
-  },
-  // Songbird charset detect utilities component info.
-  {
-    SB_CHARSETDETECTOR_CLASSNAME,
-    SB_CHARSETDETECTOR_CID,
-    SB_CHARSETDETECTOR_CONTRACTID,
-    sbCharsetDetectorConstructor
   }
 };
 
