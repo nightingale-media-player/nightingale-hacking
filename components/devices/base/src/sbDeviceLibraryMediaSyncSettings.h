285d2c985ff0192c81ad35c5340c42b20f04ac07
dbradley <dbradley@5aad4ff5-4b58-4159-ba3f-0e6328df4cad>
2010-05-08 02:03:07 +0000

    Bug 20348 - [devsync] Fix the device sync settings code
    Build bustage fix
    
    r=me it's a one liner
    
    git-svn-id: http://publicsvn.songbirdnest.com/client/trunk@18548 5aad4ff5-4b58-4159-ba3f-0e6328df4cad

--- components/devices/base/src/sbDeviceLibraryMediaSyncSettings.h
+++ components/devices/base/src/sbDeviceLibraryMediaSyncSettings.h
@@ -38,6 +38,7 @@
 struct PRLock;
 class sbIDevice;
 class sbIDeviceLibrary;
+class sbDeviceLibrarySyncSettings;
 
 class sbDeviceLibraryMediaSyncSettings : public sbIDeviceLibraryMediaSyncSettings
 {
