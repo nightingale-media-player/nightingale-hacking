3000ca14136c55e24003428b7e708d9b47b0390f
erik <erik@5aad4ff5-4b58-4159-ba3f-0e6328df4cad>
2010-06-29 21:10:05 +0000

    Bug 19411, patch 15932 - Added support to the storage property unit converter for all 64-bit values. r=slloyd
    Also fixed line endings in the storage property unit converter source
    files.
    
    
    git-svn-id: http://publicsvn.songbirdnest.com/client/trunk@19078 5aad4ff5-4b58-4159-ba3f-0e6328df4cad

--- components/property/src/sbStoragePropertyUnitConverter.cpp
+++ components/property/src/sbStoragePropertyUnitConverter.cpp
@@ -1,30 +1,28 @@
 /*
-//
-// BEGIN SONGBIRD GPL
-//
-// This file is part of the Songbird web player.
-//
-// Copyright(c) 2005-2008 POTI, Inc.
-// http://songbirdnest.com
-//
-// This file may be licensed under the terms of of the
-// GNU General Public License Version 2 (the "GPL").
-//
-// Software distributed under the License is distributed
-// on an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either
-// express or implied. See the GPL for the specific language
-// governing rights and limitations.
-//
-// You should have received a copy of the GPL along with this
-// program. If not, go to http://www.gnu.org/licenses/gpl.html
-// or write to the Free Software Foundation, Inc.,
-// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
-//
-// END SONGBIRD GPL
-//
-*/
+ *=BEGIN SONGBIRD GPL
+ *
+ * This file is part of the Songbird web player.
+ *
+ * Copyright(c) 2005-2010 POTI, Inc.
+ * http://www.songbirdnest.com
+ *
+ * This file may be licensed under the terms of of the
+ * GNU General Public License Version 2 (the ``GPL'').
+ *
+ * Software distributed under the License is distributed
+ * on an ``AS IS'' basis, WITHOUT WARRANTY OF ANY KIND, either
+ * express or implied. See the GPL for the specific language
+ * governing rights and limitations.
+ *
+ * You should have received a copy of the GPL along with this
+ * program. If not, go to http://www.gnu.org/licenses/gpl.html
+ * or write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ *=END SONGBIRD GPL
+ */
 
-// Property Unit Converter for information storage quantities, 
+// Property Unit Converter for information storage quantities,
 // ie: byte, kB, MB, GB
 
 #include "sbStoragePropertyUnitConverter.h"
@@ -32,7 +30,7 @@
 #include <stdlib.h>
 
 // ctor - register all units
-sbStoragePropertyUnitConverter::sbStoragePropertyUnitConverter() 
+sbStoragePropertyUnitConverter::sbStoragePropertyUnitConverter()
 {
   SetStringBundle(NS_LITERAL_STRING("chrome://songbird/locale/songbird.properties"));
   RegisterUnit(STORAGE_UNIT_BYTE,
@@ -52,17 +50,29 @@ sbStoragePropertyUnitConverter::sbStoragePropertyUnitConverter()
     NS_LITERAL_STRING("gb"),
     NS_LITERAL_STRING("&storage.unit.gigabytes"),
     NS_LITERAL_STRING("&storage.unit.gigabytes.short"));
+  RegisterUnit(STORAGE_UNIT_TERABYTE,
+    NS_LITERAL_STRING("tb"),
+    NS_LITERAL_STRING("&storage.unit.terabytes"),
+    NS_LITERAL_STRING("&storage.unit.terabytes.short"));
+  RegisterUnit(STORAGE_UNIT_PETABYTE,
+    NS_LITERAL_STRING("pb"),
+    NS_LITERAL_STRING("&storage.unit.petabytes"),
+    NS_LITERAL_STRING("&storage.unit.petabytes.short"));
+  RegisterUnit(STORAGE_UNIT_EXABYTE,
+    NS_LITERAL_STRING("eb"),
+    NS_LITERAL_STRING("&storage.unit.exabytes"),
+    NS_LITERAL_STRING("&storage.unit.exabytes.short"));
 }
 
 // dtor
-sbStoragePropertyUnitConverter::~sbStoragePropertyUnitConverter() 
+sbStoragePropertyUnitConverter::~sbStoragePropertyUnitConverter()
 {
 }
 
 // convert from the native unit to any of the exposed units
 NS_IMETHODIMP
-sbStoragePropertyUnitConverter::ConvertFromNativeToUnit(PRFloat64 aValue, 
-                                                        PRUint32 aUnitID, 
+sbStoragePropertyUnitConverter::ConvertFromNativeToUnit(PRFloat64 aValue,
+                                                        PRUint32 aUnitID,
                                                         PRFloat64 &_retVal)
 {
   switch (aUnitID) {
@@ -78,6 +88,15 @@ sbStoragePropertyUnitConverter::ConvertFromNativeToUnit(PRFloat64 aValue,
     case STORAGE_UNIT_GIGABYTE:
       aValue /= (1024.0*1024.0*1024.0);
       break;
+    case STORAGE_UNIT_TERABYTE:
+      aValue /= (1024.0*1024.0*1024.0*1024.0);
+      break;
+    case STORAGE_UNIT_PETABYTE:
+      aValue /= (1024.0*1024.0*1024.0*1024.0*1024.0);
+      break;
+    case STORAGE_UNIT_EXABYTE:
+      aValue /= (1024.0*1024.0*1024.0*1024.0*1024.0*1024.0);
+      break;
     default:
       return NS_ERROR_INVALID_ARG;
   }
@@ -87,8 +106,8 @@ sbStoragePropertyUnitConverter::ConvertFromNativeToUnit(PRFloat64 aValue,
 
 // convert from any of the exposed units back to the native unit
 NS_IMETHODIMP
-sbStoragePropertyUnitConverter::ConvertFromUnitToNative(PRFloat64 aValue, 
-                                                        PRUint32 aUnitID, 
+sbStoragePropertyUnitConverter::ConvertFromUnitToNative(PRFloat64 aValue,
+                                                        PRUint32 aUnitID,
                                                         PRFloat64 &_retVal)
 {
   switch (aUnitID) {
@@ -104,6 +123,15 @@ sbStoragePropertyUnitConverter::ConvertFromUnitToNative(PRFloat64 aValue,
     case STORAGE_UNIT_GIGABYTE:
       aValue *= (1024.0*1024.0*1024.0);
       break;
+    case STORAGE_UNIT_TERABYTE:
+      aValue *= (1024.0*1024.0*1024.0*1024.0);
+      break;
+    case STORAGE_UNIT_PETABYTE:
+      aValue *= (1024.0*1024.0*1024.0*1024.0*1024.0);
+      break;
+    case STORAGE_UNIT_EXABYTE:
+      aValue *= (1024.0*1024.0*1024.0*1024.0*1024.0*1024.0);
+      break;
     default:
       return NS_ERROR_INVALID_ARG;
   }
@@ -115,11 +143,14 @@ PRInt32 sbStoragePropertyUnitConverter::GetAutoUnit(const PRFloat64 aValue) {
   // get number of digits
   PRUint32 d;
   if (aValue == 0) d = 1;
-  else d = (PRUint32)(log10(abs(aValue)) + 1);
+  else d = (PRUint32)(log10(fabs(aValue)) + 1);
 
   // and pick most suitable unit
   if (d < 4) return STORAGE_UNIT_BYTE;
   if (d < 7) return STORAGE_UNIT_KILOBYTE;
   if (d < 10) return STORAGE_UNIT_MEGABYTE;
-  return STORAGE_UNIT_GIGABYTE;
+  if (d < 13) return STORAGE_UNIT_GIGABYTE;
+  if (d < 16) return STORAGE_UNIT_TERABYTE;
+  if (d < 19) return STORAGE_UNIT_PETABYTE;
+  return STORAGE_UNIT_EXABYTE;
 }
--- components/property/src/sbStoragePropertyUnitConverter.h
+++ components/property/src/sbStoragePropertyUnitConverter.h
@@ -1,28 +1,26 @@
 /*
-//
-// BEGIN SONGBIRD GPL
-//
-// This file is part of the Songbird web player.
-//
-// Copyright(c) 2005-2008 POTI, Inc.
-// http://songbirdnest.com
-//
-// This file may be licensed under the terms of of the
-// GNU General Public License Version 2 (the "GPL").
-//
-// Software distributed under the License is distributed
-// on an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either
-// express or implied. See the GPL for the specific language
-// governing rights and limitations.
-//
-// You should have received a copy of the GPL along with this
-// program. If not, go to http://www.gnu.org/licenses/gpl.html
-// or write to the Free Software Foundation, Inc.,
-// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
-//
-// END SONGBIRD GPL
-//
-*/
+ *=BEGIN SONGBIRD GPL
+ *
+ * This file is part of the Songbird web player.
+ *
+ * Copyright(c) 2005-2010 POTI, Inc.
+ * http://www.songbirdnest.com
+ *
+ * This file may be licensed under the terms of of the
+ * GNU General Public License Version 2 (the ``GPL'').
+ *
+ * Software distributed under the License is distributed
+ * on an ``AS IS'' basis, WITHOUT WARRANTY OF ANY KIND, either
+ * express or implied. See the GPL for the specific language
+ * governing rights and limitations.
+ *
+ * You should have received a copy of the GPL along with this
+ * program. If not, go to http://www.gnu.org/licenses/gpl.html
+ * or write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ *=END SONGBIRD GPL
+ */
 
 #ifndef __sbStoragePropertyUnitConverter_H__
 #define __sbStoragePropertyUnitConverter_H__
@@ -39,17 +37,20 @@ public:
     STORAGE_UNIT_KILOBYTE,
     STORAGE_UNIT_MEGABYTE,
     STORAGE_UNIT_GIGABYTE,
+    STORAGE_UNIT_TERABYTE,
+    STORAGE_UNIT_PETABYTE,
+    STORAGE_UNIT_EXABYTE
   };
-  
+
   virtual PRInt32 GetAutoUnit(PRFloat64 aValue);
 
 protected:
 
-  NS_IMETHOD ConvertFromNativeToUnit(PRFloat64 aValue, 
-                                     PRUint32 aUnitID, 
+  NS_IMETHOD ConvertFromNativeToUnit(PRFloat64 aValue,
+                                     PRUint32 aUnitID,
                                      PRFloat64 &_retVal);
-  NS_IMETHOD ConvertFromUnitToNative(PRFloat64 aValue, 
-                                     PRUint32 aUnitID, 
+  NS_IMETHOD ConvertFromUnitToNative(PRFloat64 aValue,
+                                     PRUint32 aUnitID,
                                      PRFloat64 &_retVal);
 };
 
