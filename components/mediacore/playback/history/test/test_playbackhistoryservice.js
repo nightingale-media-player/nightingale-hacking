fb992739eeab86d9a3234bce50476fdcaf8ef42b
erik <erik@5aad4ff5-4b58-4159-ba3f-0e6328df4cad>
2010-07-20 17:03:53 +0000

    Bug 21734, patch 16099 - Fixed the playback history service unit test to pass an sbIPropertyArray instead of a JS array to createMediaItem. r=mook
    git-svn-id: http://publicsvn.songbirdnest.com/client/trunk@19175 5aad4ff5-4b58-4159-ba3f-0e6328df4cad

--- components/mediacore/playback/history/test/test_playbackhistoryservice.js
+++ components/mediacore/playback/history/test/test_playbackhistoryservice.js
@@ -1,36 +1,40 @@
 /*
-//
-// BEGIN SONGBIRD GPL
-//
-// This file is part of the Songbird web player.
-//
-// Copyright(c) 2005-2008 POTI, Inc.
-// http://songbirdnest.com
-//
-// This file may be licensed under the terms of of the
-// GNU General Public License Version 2 (the "GPL").
-//
-// Software distributed under the License is distributed
-// on an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either
-// express or implied. See the GPL for the specific language
-// governing rights and limitations.
-//
-// You should have received a copy of the GPL along with this
-// program. If not, go to http://www.gnu.org/licenses/gpl.html
-// or write to the Free Software Foundation, Inc.,
-// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
-//
-// END SONGBIRD GPL
-//
-*/
+ *=BEGIN SONGBIRD GPL
+ *
+ * This file is part of the Songbird web player.
+ *
+ * Copyright(c) 2005-2010 POTI, Inc.
+ * http://www.songbirdnest.com
+ *
+ * This file may be licensed under the terms of of the
+ * GNU General Public License Version 2 (the ``GPL'').
+ *
+ * Software distributed under the License is distributed
+ * on an ``AS IS'' basis, WITHOUT WARRANTY OF ANY KIND, either
+ * express or implied. See the GPL for the specific language
+ * governing rights and limitations.
+ *
+ * You should have received a copy of the GPL along with this
+ * program. If not, go to http://www.gnu.org/licenses/gpl.html
+ * or write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ *=END SONGBIRD GPL
+ */
+
+Components.utils.import("resource://app/jsmodules/sbProperties.jsm");
 
 function runTest() {
 
   var ios = Cc["@mozilla.org/network/io-service;1"].getService(Ci.nsIIOService);
 
   var testItemURL = ios.newURI("file:///alpha.mp3", null, null);
-  var testItemProperties = 
-    { artistName: "A", albumName: "Alpha", trackName: "Track Alpha", trackNumber: "1", year: "2000"};
+  var testItemProperties =
+    SBProperties.createArray({ artistName:  "A",
+                               albumName:   "Alpha",
+                               trackName:   "Track Alpha",
+                               trackNumber: "1",
+                               year:        "2000" });
   
   var history = Cc["@songbirdnest.com/Songbird/PlaybackHistoryService;1"]
                   .getService(Ci.sbIPlaybackHistoryService);
