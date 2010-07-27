ed68611ffa675ac30db955eca79f2dee63ce4671
alfred <alfred@5aad4ff5-4b58-4159-ba3f-0e6328df4cad>
2010-07-27 04:04:02 +0000

    bug 21690 - Add the preference to disable live search. live search is on by default (r=mook)
    git-svn-id: http://publicsvn.songbirdnest.com/client/trunk@19198 5aad4ff5-4b58-4159-ba3f-0e6328df4cad

--- app/content/scripts/searchHandler.js
+++ app/content/scripts/searchHandler.js
@@ -1,27 +1,25 @@
 /*
-//
-// BEGIN SONGBIRD GPL
-// 
-// This file is part of the Songbird web player.
-//
-// Copyright(c) 2005-2008 POTI, Inc.
-// http://songbirdnest.com
-// 
-// This file may be licensed under the terms of of the
-// GNU General Public License Version 2 (the "GPL").
-// 
-// Software distributed under the License is distributed 
-// on an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either 
-// express or implied. See the GPL for the specific language 
-// governing rights and limitations.
-//
-// You should have received a copy of the GPL along with this 
-// program. If not, go to http://www.gnu.org/licenses/gpl.html
-// or write to the Free Software Foundation, Inc., 
-// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
-// 
-// END SONGBIRD GPL
-//
+ *=BEGIN SONGBIRD GPL
+ *
+ * This file is part of the Songbird web player.
+ *
+ * Copyright(c) 2005-2010 POTI, Inc.
+ * http://www.songbirdnest.com
+ *
+ * This file may be licensed under the terms of of the
+ * GNU General Public License Version 2 (the ``GPL'').
+ *
+ * Software distributed under the License is distributed
+ * on an ``AS IS'' basis, WITHOUT WARRANTY OF ANY KIND, either
+ * express or implied. See the GPL for the specific language
+ * governing rights and limitations.
+ *
+ * You should have received a copy of the GPL along with this
+ * program. If not, go to http://www.gnu.org/licenses/gpl.html
+ * or write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ *=END SONGBIRD GPL
  */
 
 /**
@@ -434,8 +432,16 @@ const gSearchHandler = {
       this._previousSearch =  searchBar.value;
     }
     
-    // Activate live search mode for the songbird search engine
-    searchBar.setLiveSearchMode(songbirdEngine, true);
+    var prefs = Cc["@mozilla.org/preferences-service;1"]
+                  .getService(Ci.nsIPrefBranch);
+    var liveSearchEnabled = true;
+    try {
+      liveSearchEnabled = prefs.getBoolPref("songbird.livesearch.enabled");
+    }
+    catch (e) { }
+
+    // Set live search mode for the songbird search engine
+    searchBar.setLiveSearchMode(songbirdEngine, liveSearchEnabled);
     
     // Make sure the songbird search is selected
     if (searchBar.currentEngine != songbirdEngine) { 
