a8448bb9e776235a24692a635c5427dbb6e3a76d
erik <erik@5aad4ff5-4b58-4159-ba3f-0e6328df4cad>
2010-08-25 14:39:10 +0000

    Bug 22133, patch 16466 - Fixed first-run wizard app update download finished page strings. r=slloyd
    Also added forward app update test script.
    
    
    git-svn-id: http://publicsvn.songbirdnest.com/client/trunk@19349 5aad4ff5-4b58-4159-ba3f-0e6328df4cad

--- update/test/test_windows_forward_update.sh
+++ update/test/test_windows_forward_update.sh
@@ -23,26 +23,24 @@
 #=END SONGBIRD GPL
 #
 
+#filter substitution
+
 #
-# Songbird Windows application update test script.
+# Songbird Windows application forward update test script.
 #
 # Options:
 #
 #   -clobber                    Perform a clobber and full rebuild.
 #
 #   This script may be used to test updating of the Songbird application on
-# Windows.  This script tests updating of an older version of Songbird to the
-# locally built version.
+# Windows.  This script tests updating of the locally built Songbird.
 #   This script builds a Songbird mar file, sets up the update files on the
-# specified web server, downloads and installs a base Songbird, patches it to
-# use the specified web server for updates, and launches the base Songbird.
-#   A base version of Songbird is downloaded and installed so that the update
-# can be tested.  The built Songbird installer is not used because it would
-# perform all of the installation steps that the update would do, eliminating
-# the utility of testing the update.
+# specified web server, installs the locally built Songbird as the base
+# Songbird, patches it to use the specified web server for updates, and launches
+# the base Songbird.
 #   To test, follow the instructions below.  When the script completes, the
 # base, non-updated Songbird should be open and presenting an application update
-# dialog for updating to the locally built Songbird.
+# page in the first-run wizard for updating to the locally built Songbird.
 #   IMPORTANT: When the base Songbird installation is complete, exit the
 # installer without launching Songbird.  This is required because this script
 # must change the default update server URL preference before Songbird is first
@@ -76,27 +74,40 @@
 # 4. Checkout songbird and cd into trunk.
 # 5. Execute the following:
 #
-#   ./update/test/test_update.sh
+#   ./update/test/test_windows_forward_update.sh
 #
 # 6. When the base Songbird installer opens, install the base Songbird into
-#    "c:\Program Files\Songbird" (it should be the default).
+#    "c:\Program Files\@INSTALL_DIR@" (it should be the default).
 # 7. IMPORTANT: Remember not to launch Songbird from the installer.
 # 8. Wait for this script to launch the base Songbird.
-# 9. Navigate through the first-run wizard.
-# 10 Verify that the application update dialog is presented.  If not, check
-#    the permisssions of the update files on the HTTP server and try manually
-#    updating.
-# 11. Perform update.
-# 12. Verify that update worked.
+# 9. Verify that the application update is presented in the first-run wizard.
+#    If not, check the permisssions of the update files on the HTTP server and
+#    try manually updating.
+# 10. Perform update.
+# 11. Verify that update worked.
 #
 
-if [ "$1" = "-clobber" ]; then
-  echo "ac_add_options --enable-installer" > songbird.config
-  make -f songbird.mk clobber && \
-    make -f songbird.mk && \
-    make -C compiled/update complete
-else
-  make -C compiled && make -C compiled/update complete
+# Only build if not running the test.
+if [ "$1" != "-test" ]; then
+
+  # Build update.
+  if [ "$1" = "-clobber" ]; then
+    echo "ac_add_options --enable-installer" > songbird.config
+    make -f songbird.mk clobber && \
+      make -f songbird.mk && \
+      make -C compiled/update complete && \
+      make -C compiled/update/test
+  else
+    make -C compiled && \
+      make -C compiled/update complete && \
+      make -C compiled/update/test
+  fi
+
+  # Test update.
+  chmod a+x compiled/update/test/test_windows_forward_update.sh
+  ./compiled/update/test/test_windows_forward_update.sh -test
+  exit 0
+
 fi
 
 cp compiled/_built_installer/Songbird_*_windows-i686-msvc8.complete.mar \
@@ -107,16 +118,16 @@ export MAR_SIZE=`sed -n 5p compiled/_built_installer/Songbird_*_windows-i686-msv
 
 rm -f appupdate.xml
 echo "<updates>" >> appupdate.xml
-echo "<update type=\"major\">" >> appupdate.xml
+echo "<update type=\"major\"  version=\"@UPDATE_VERSION@\" extensionVersion=\"@UPDATE_VERSION@\">" >> appupdate.xml
 echo "<patch type=\"complete\" URL=\"${HTTP_SERVER_URL}/Songbird.mar\" hashFunction=\"SHA1\" hashValue=\"${MAR_HASHVALUE}\" size=\"${MAR_SIZE}\" />" >> appupdate.xml
 echo "</update>" >> appupdate.xml
 echo "</updates>" >> appupdate.xml
 cp appupdate.xml ${HTTP_SERVER_PATH}/appupdate.xml
 chmod a+r ${HTTP_SERVER_PATH}/appupdate.xml
 
-wget http://download.songbirdnest.com/installer/windows/i686-msvc8/Songbird_1.4.3-1438_windows-i686-msvc8.exe
 # DON'T START SONGBIRD FROM INSTALLER
-Songbird_1.4.3-1438_windows-i686-msvc8.exe
-echo "pref(\"app.update.url\", \"${HTTP_SERVER_URL}/appupdate.xml\");" > "c:\Program Files\Songbird\defaults\preferences\aaa-test-update.js"
-"c:\Program Files\Songbird\songbird.exe"
+./compiled/_built_installer/@INSTALLER@
+echo "pref(\"app.update.url\", \"${HTTP_SERVER_URL}/appupdate.xml\");" > "c:\Program Files\@INSTALL_DIR@\defaults\preferences\aaa-test-update.js"
+echo "pref(\"songbird.firstrun.check.app-update\", true);" >> "c:\Program Files\@INSTALL_DIR@\defaults\preferences\aaa-test-update.js"
+"c:\Program Files\@INSTALL_DIR@\songbird.exe"
 
