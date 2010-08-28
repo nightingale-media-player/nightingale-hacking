0c036c8632f19274c33b4d27df5c7e30eaee4268
slloyd <slloyd@5aad4ff5-4b58-4159-ba3f-0e6328df4cad>
2010-08-28 00:50:01 +0000

    Bug 21874 Overlay messaging when the play queue is empty. r=msmith
    git-svn-id: http://publicsvn.songbirdnest.com/client/trunk@19367 5aad4ff5-4b58-4159-ba3f-0e6328df4cad

--- app/content/scripts/playQueue.js
+++ app/content/scripts/playQueue.js
@@ -43,14 +43,53 @@ var playQueue = {
 
   _LOG: DebugUtils.generateLogFunction("sbPlayQueue", 5),
 
+  /**
+   * Called when the display pane loads.
+   */
   onLoad: function playQueue_onLoad() {
-    this._LOG("playQueue.onLoad");
+    this._LOG(arguments.callee.name);
+
+    this._playlist = document.getElementById("playqueue-playlist");
+    this._playlistBox = document.getElementById("playqueue-playlist-box");
+    this._messageLayer = document.getElementById("playqueue-message-layer-outer-box");
+    this._innerMessageBox = document.getElementById("playqueue-message-layer-inner-box");
 
     var playQueueService = Cc["@songbirdnest.com/Songbird/playqueue/service;1"]
                              .getService(Ci.sbIPlayQueueService);
-    var view = playQueueService.mediaList.createView();
-    this._playlist = document.getElementById("playqueue-playlist");
 
+    // Show the empty queue messaging if the queue is empty
+    if (playQueueService.mediaList.isEmpty) {
+      this._showEmptyQueueLayer();
+    } else {
+      this._hideEmptyQueueLayer();
+    }
+
+    // Listen to underlying medialist to enable/disable empty queue messaging
+    var self = this;
+    this._listListener = {
+      onItemAdded: function(aList, aItem, aIndex) {
+        if (self._listEmpty) {
+          self._hideEmptyQueueLayer();
+        }
+      },
+      onAfterItemRemoved: function(aList, aItem, aIndex) {
+        if (aList.isEmpty) {
+          self._showEmptyQueueLayer();
+        }
+      },
+      onListCleared: function(aList) {
+        self._showEmptyQueueLayer();
+      }
+    };
+
+    playQueueService.mediaList.addListener(this._listListener,
+                                           false,
+                                           Ci.sbIMediaList.LISTENER_FLAGS_ITEMADDED |
+                                           Ci.sbIMediaList.LISTENER_FLAGS_AFTERITEMREMOVED |
+                                           Ci.sbIMediaList.LISTENER_FLAGS_LISTCLEARED);
+
+    // Bind the playlist to a a view and commands.
+    var view = playQueueService.mediaList.createView();
     var mgr = Cc["@songbirdnest.com/Songbird/PlaylistCommandsManager;1"]
                 .createInstance(Ci.sbIPlaylistCommandsManager);
 
@@ -63,12 +102,93 @@ var playQueue = {
     this._playlist.bind(view, commands);
   },
 
+  /**
+   * Called on unload.
+   */
   onUnload: function playQueue_onUnload() {
-    this._LOG("playQueue.onUnload");
+    this._LOG(arguments.callee.name);
+
+    if (this._listListener) {
+      var playQueueService = Cc["@songbirdnest.com/Songbird/playqueue/service;1"]
+                               .getService(Ci.sbIPlayQueueService);
+      playQueueService.mediaList.removeListener(this._listListener);
+      this._listListener = null;
+    }
     if (this._playlist) {
       this._playlist.destroy();
       this._playlist = null;
     }
+  },
+
+  /**
+   * Dim playlist (via disabled attribute) and show messaging layer when the
+   * queue is empty.
+   */
+  _showEmptyQueueLayer: function playQueue__showEmptyQueueLayer() {
+    this._LOG(arguments.callee.name);
+    this._playlistBox.setAttribute("disabled", "true");
+    this._listEmpty = true;
+    this._messageLayer.removeAttribute("hidden");
+  },
+
+  /**
+   * Hide messaging layer when queue is not empty.
+   */
+  _hideEmptyQueueLayer: function playQueue__hideEmptyQueueLayer() {
+    this._LOG(arguments.callee.name);
+    this._playlistBox.removeAttribute("disabled");
+    this._messageLayer.setAttribute("hidden", "true");
+    this._listEmpty = false;
+  },
+
+  /**
+   * xxx slloyd This handler is not hooked up to the box due to Bug 13018. Also
+   *            see comments in Bug 21874.
+   *
+   * Event handler for dragenter on the messaging layer. If the item can
+   * be dropped on the playlist, hide the message box until the item is either
+   * dropped or dragged back out of the queue area.
+   */
+  onEmptyQueueDragEnter:
+      function playQueue_onEmptyQueueDragEnter(aEvent) {
+
+    this._LOG(arguments.callee.name);
+    if (this._playlist._canDrop(aEvent)) {
+      this._playlistBox.removeAttribute("disabled");
+      this._innerMessageBox.setAttribute("hidden", "true");
+    }
+  },
+
+  /*
+   * xxx slloyd This handler is not hooked up to the box due to Bug 13018. Also
+   *            see comments in Bug 21874.
+   *
+   * Event handler for dragexit on the messaging layer. Show the message box and
+   * dim the playlist.
+   */
+  onEmptyQueueDragExit:
+      function playQueue_onEmptyQueueDragExit(aEvent) {
+
+    this._LOG(arguments.callee.name);
+    this._playlistBox.setAttribute("disabled", "true");
+    this._innerMessageBox.removeAttribute("hidden");
+  },
+
+  /**
+   * Event handler for dragdrop on the messaging layer. Handle the drop with the
+   * playlist.
+   */
+  onEmptyQueueDragDrop:
+      function playQueue_onEmptyQueueDragDrop(aEvent) {
+
+    this._LOG(arguments.callee.name);
+    var dragService = Cc["@mozilla.org/widget/dragservice;1"]
+                        .getService(Ci.nsIDragService);
+    var session = dragService.getCurrentSession();
+
+    this._playlist._dropOnTree(this._playlist.mediaListView.length,
+                               Ci.sbIMediaListViewTreeViewObserver.DROP_AFTER,
+                               session);
   }
 
 };
