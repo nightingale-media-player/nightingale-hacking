3f304c3bd5f7437c621b9c734c6703d6eeef4018
alfred <alfred@5aad4ff5-4b58-4159-ba3f-0e6328df4cad>
2010-10-15 03:42:20 +0000

    bug 22510 - Browser is loading in media tab upon restart (r=nick)
    git-svn-id: http://publicsvn.songbirdnest.com/client/trunk@19779 5aad4ff5-4b58-4159-ba3f-0e6328df4cad

--- app/content/bindings/browser/SBSessionStore.js
+++ app/content/bindings/browser/SBSessionStore.js
@@ -1,27 +1,27 @@
 // vim: set sw=2 :miv
-//
-// BEGIN SONGBIRD GPL
-//
-// This file is part of the Songbird web player.
-//
-// Copyright(c) 2005-2008 POTI, Inc.
-// http://songbirdnest.com
-//
-// This file may be licensed under the terms of of the
-// GNU General Public License Version 2 (the "GPL").
-//
-// Software distributed under the License is distributed
-// on an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either
-// express or implied. See the GPL for the specific language
-// governing rights and limitations.
-//
-// You should have received a copy of the GPL along with this
-// program. If not, go to http://www.gnu.org/licenses/gpl.html
-// or write to the Free Software Foundation, Inc.,
-// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
-//
-// END SONGBIRD GPL
-//
+/*
+ *=BEGIN SONGBIRD GPL
+ *
+ * This file is part of the Songbird web player.
+ *
+ * Copyright(c) 2005-2010 POTI, Inc.
+ * http://www.songbirdnest.com
+ *
+ * This file may be licensed under the terms of of the
+ * GNU General Public License Version 2 (the ``GPL'').
+ *
+ * Software distributed under the License is distributed
+ * on an ``AS IS'' basis, WITHOUT WARRANTY OF ANY KIND, either
+ * express or implied. See the GPL for the specific language
+ * governing rights and limitations.
+ *
+ * You should have received a copy of the GPL along with this
+ * program. If not, go to http://www.gnu.org/licenses/gpl.html
+ * or write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ *=END SONGBIRD GPL
+ */
 
 /**
  * Session Store
@@ -326,7 +326,7 @@ var SBSessionStore = {
             }
           } else {
             LOG("not first tab, always _blank");
-//            location = "_blank";
+            location = "_blank";
           }
 
           // don't load device pages for a device that isn't mounted
