ab10acb6aee6a1c544197a747fe1a49a2e80c1d0
Matt Parnell <parwok@gmail.com>
2011-09-20 21:16:39 -0500

    some preliminary branding changes to change everything to be branded nightingale...later we'll change some filenames that are blatantly songbird too, but doing this for when we have a working build

--- branding/branding.mk
+++ branding/branding.mk
@@ -1,16 +1,15 @@
-# Songbird official branding.
+# Nightingale official branding.
 
 SB_BRAND_SHORT_NAME  = $(SB_APPNAME)
-SB_BRAND_FULL_NAME   = Songbird Web Player
-SB_BRAND_VENDOR_NAME = POTI, Inc.
+SB_BRAND_FULL_NAME   = Nightingale Web Player
+SB_BRAND_VENDOR_NAME = The Nightingale Community
 SB_BRAND_TRADEMARKS  = \
-  Songbird and 'Play the Web' are registered trademarks of POTI, Inc.\\\\r\\\\n\
   Mozilla is a registered trademark of the Mozilla Foundation.\\\\r\\\\n\
   XULRunner is a trademark of the Mozilla Foundation.
-SB_INSTALLER_ABOUT_URL=http://www.songbirdnest.com/
-SB_INSTALLER_UPDATE_URL=http://getsongbird.com/
-SB_CRASHREPORT_SERVER_URL=https://crashreports.songbirdnest.com/submit
-SB_APP_BUNDLE_BASENAME=com.songbirdnest
+SB_INSTALLER_ABOUT_URL=http://getnightingale.com/
+SB_INSTALLER_UPDATE_URL=http://getnightingale.com/
+SB_CRASHREPORT_SERVER_URL=http://getnightingale.com/forums
+SB_APP_BUNDLE_BASENAME=com.nightingale
 
 SB_BRANDING_DEFINES += \
   -DSB_VENDOR="$(SB_BRAND_VENDOR_NAME)" \
