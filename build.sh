ecc7a13f50be65bce86dfacce00d0e604c78d9ea
Matt Parnell <parwok@gmail.com>
2011-09-26 21:59:11 -0500

    alternative location for gstreamer libs added to search path

--- build.sh
+++ build.sh
@@ -15,6 +15,9 @@ for dir in /usr/lib64 /usr/lib ; do
   if [ -f ${dir}/gstreamer-0.10/libgstcoreelements.so ] ; then
     export GST_PLUGIN_PATH=${dir}/gstreamer-0.10
     break
+  elseif [ -f ${dir}/gstreamer0.10/libgstcoreelements.so ] ; then
+	export GST_PLUIN_PATH=${dir}/gstreamer0.10
+	break
   fi
 done
 
