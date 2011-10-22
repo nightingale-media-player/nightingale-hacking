9b2984bdfe6b7ab53ab323ace7080154c274b4cb
Matt Parnell <parwok@gmail.com>
2011-10-22 02:47:24 -0500

    make the buildscript grab all deps for osx and windows, hopefully no more hacking to get windows working...osx still needs help. also update .gitignore too

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ set -e
 # if you do a debug build, add the appropriate options
 # to your nightingale.config file!
 build="release"
+buildir="$(pwd)"
 
 # Check for the build deps for the system's architecture and OS
 case $OSTYPE in
@@ -12,7 +13,10 @@ case $OSTYPE in
 		arch=`uname -m`
 		depdirn="linux-$arch"
 		svnroot="http://ngale.svn.sourceforge.net/svnroot/ngale/branches/dependencies/Nightingale1.8/$depdirn"
-	    
+        deps=(  mozilla
+                sqlite
+                taglib
+                xulrunner)
 		# use our own gstreamer libs
 		for dir in /usr/lib64 /usr/lib ; do
 			if [ -f ${dir}/gstreamer-0.10/libgstcoreelements.so ] ; then
@@ -23,19 +27,61 @@ case $OSTYPE in
 				break
 			fi
 		done
-	  
+        
 		# !!!! NOTICE: comment the below out if building on/for Windows or Mac or playback probably won't work !!!!
 		grep -sq gstreamer-system nightingale.config || ( echo 'ac_add_options --with-media-core=gstreamer-system' >> nightingale.config )
 		;;
 	msys*)
-		echo "You should comment out line 29 in this build.sh script and rerun it to make sure things work properly!"
+		echo "You should comment out line 31 in this build.sh script and rerun it to make sure things work properly!"
 		depdirn="windows-i686-msvc8"
 		svnroot="http://publicsvn.songbirdnest.com/vendor-binaries/tags/Songbird1.8.1/$depdirn"
+        # I'm not sure we need all these, but I haven't delved into trimming down the deps we download on windows
+        deps=(  flac
+                gettext 
+                glib 
+                gst-plugins-bad 
+                gst-plugins-base 
+                gst-plugins-good 
+                gst-plugins-ugly 
+                gstreamer 
+                libIDL 
+                libgpod 
+                libjpeg 
+                libogg 
+                libtheora 
+                libvorbis  
+                mozilla
+                mozilla-1.9.2
+                sqlite
+                taglib
+                xulrunner
+                xulrunner-1.9.2 )
 		;;
 	darwin*)
-		echo "You should comment out line 29 in this build.sh script and rerun it to make sure things work properly!"
+		echo "You should comment out line 31 in this build.sh script and rerun it to make sure things work properly!"
 		depdirn="macosx-i686"
+        # I'm not sure we need all these, but I haven't delved into trimming down the deps we download on osx
 		svnroot="http://publicsvn.songbirdnest.com/vendor-binaries/tags/Songbird1.8.1/$depdirn"
+        deps=(  flac
+                gettext 
+                glib 
+                gst-plugins-bad 
+                gst-plugins-base 
+                gst-plugins-good 
+                gst-plugins-ugly 
+                gstreamer 
+                libIDL 
+                libgpod 
+                libjpeg 
+                libogg 
+                libtheora 
+                libvorbis  
+                mozilla
+                mozilla-1.9.2
+                sqlite
+                taglib
+                xulrunner
+                xulrunner-1.9.2 )
 		;;
 	*)
 		echo "Can't find deps for $OSTYPE. You may need to build them yourself. Doublecheck the SVN's for \n
@@ -48,28 +94,18 @@ if [ ! -d "dependencies/$depdirn" ] ; then
 	echo "You don't have them...downloading via SVN..."
 	
 	cd dependencies
-	mkdir -p "$depdirn"/{xulrunner/release,mozilla/release,taglib/release,sqlite/release}
-		
-	cd "$depdirn/mozilla/release"
-	svn co "$svnroot/mozilla/$build" ./
-	
-	cd ../../../
-	
-	cd "$depdirn/xulrunner/release"
-	svn co "$svnroot/xulrunner/$build" ./
-	
-	cd ../../../
-	
-	cd "$depdirn/taglib/release"
-	svn co "$svnroot/taglib/$build" ./
-	
-	cd ../../../
-	
-	cd "$depdirn/sqlite/release"
-	svn co "$svnroot/sqlite/$build" ./
-	
-	cd ../../../../
+    mkdir $depdirn
+    cd $depdirn
+            
+    for dep in ${deps[@]}; do
+        mkdir -p "$dep/$build"
+        cd "$dep/$build"
+        svn co "$svnroot/$dep/$build" ./
+        cd ../../
+    done
 fi
+
+cd $buildir
 
 # hopefully we have python2 on this system
 export PYTHON="$(which python2 2>/dev/null || which python)"
