67a799e6cca3c8ed6adc067f19fdd3ac72d9a54b
rsjtdrjgfuzkfg <rsjtdrjgfuzkfg@abwesend.de>
2011-12-11 20:48:47 +0100

    Added Folder Synchronization based on FolderSync. Two new core AddOns: foldersync and foldersync-ng: foldersync: the Folder Synchronisation itself, adjusted to work with Gecko 1.9.0. foldersync-ng: adjustments that do not belong to FolderSync (branding for NG) to make it more easy to keep in track with FolderSync changes in future versions.

---
+++ extensions/foldersync/defaults/preferences/prefs.js
@@ -0,0 +1,15 @@
+// Used to have localized descriptions
+pref("extensions.foldersync@rsjtdrjgfuzkfg.com.description", "chrome://foldersync/locale/central.properties");
+
+/* The debug level: 1: log fatal errors
+ *                  2: log all errors
+ *                  3: log errors and warnings
+ *                  4: log errors, warnings and important debug output
+ *                  5: log everything
+ */
+pref("extensions.FolderSync.debugLevel", 3);
+
+// This stores almost everything as JSON. Managed by foldersync.preferences.
+pref("extensions.FolderSync.JSON","{}")
+
+
