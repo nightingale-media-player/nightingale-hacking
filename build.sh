383c91e249f397da886134cca02224ab963afe12
Matt Parnell <parwok@gmail.com>
2012-02-03 14:46:39 -0800

    http download for linux deps

--- build.sh
+++ build.sh
@@ -8,19 +8,35 @@ set -e
 # to your nightingale.config file!
 build="release"
 buildir="$(pwd)"
+version=1.8
+
+function get_deps() {	
+	cd dependencies
+	
+	mkdir $depdirn
+	cd $depdirn
+			
+	for dep in ${deps[@]}; do
+		mkdir -p "$dep/$build"
+		cd "$dep/$build"
+		svn co "$svnroot/$dep/$build" ./
+		cd ../../
+	done
+}
 
 # Check for the build deps for the system's architecture and OS
 case $OSTYPE in
 	linux*)
-		arch=`uname -m`
+		arch=$(uname -m)
 		depdirn="linux-$arch"
-		svnroot="http://ngale.svn.sourceforge.net/svnroot/ngale/branches/dependencies/Nightingale1.8/$depdirn"
-		# msys error workaround
-		patch=1
-        deps=(  mozilla
-                sqlite
-                taglib
-                xulrunner)
+		
+		if [ ! -d "dependencies/$depdirn" ] ; then
+			cd dependencies
+			wget "https://downloads.sourceforge.net/project/ngale/$version/$arch/$depdirn-$version.tar.lzma"
+			tar xvf $depdirn-$version.tar.lzma
+			cd ../
+		fi
+		
 		# use our own gstreamer libs
 		for dir in /usr/lib64 /usr/lib ; do
 			if [ -f ${dir}/gstreamer-0.10/libgstcoreelements.so ] ; then
@@ -41,7 +57,7 @@ case $OSTYPE in
 		rm ./components/library/localdatabase/content/schema.sql
 		mv tmp.sql ./components/library/localdatabase/content/schema.sql
 		depdirn="windows-i686-msvc8"
-		svnroot="http://publicsvn.songbirdnest.com/vendor-binaries/tags/Songbird1.8.1/$depdirn"
+		svnroot="http://publicsvn.songbirdnest.com/vendor-binaries/trunk/$depdirn"
         # I'm not sure we need all these, but I haven't delved into trimming down the deps we download on windows
         deps=(  flac
                 gettext 
@@ -63,12 +79,16 @@ case $OSTYPE in
                 taglib
                 xulrunner
                 xulrunner-1.9.2 )
+                
+        if [ ! -d "dependencies/$depdirn" ] ; then
+			get_deps
+		fi
 		;;
 	darwin*)
 		echo "You should comment out line 31 in this build.sh script and rerun it to make sure things work properly!"
 		depdirn="macosx-i686"
         # I'm not sure we need all these, but I haven't delved into trimming down the deps we download on osx
-		svnroot="http://publicsvn.songbirdnest.com/vendor-binaries/tags/Songbird1.8.1/$depdirn"
+		svnroot="http://publicsvn.songbirdnest.com/vendor-binaries/trunk/$depdirn"
         deps=(  flac
                 gettext 
                 glib 
@@ -89,36 +109,24 @@ case $OSTYPE in
                 taglib
                 xulrunner
                 xulrunner-1.9.2 )
+        
+        if [ ! -d "dependencies/$depdirn" ] ; then
+			get_deps
+		fi
 		;;
 	*)
 		echo "Can't find deps for $OSTYPE. You may need to build them yourself. Doublecheck the SVN's for \n
-		Songbird and Nightingale 1.8 to be sure."
+		Songbird and Nightingale trunks to be sure."
 		;;
 esac
-
-echo "Checking for the required dependencies..."
-if [ ! -d "dependencies/$depdirn" ] ; then
-	echo "You don't have them...downloading via SVN..."
-	
-	cd dependencies
-    mkdir $depdirn
-    cd $depdirn
-            
-    for dep in ${deps[@]}; do
-        mkdir -p "$dep/$build"
-        cd "$dep/$build"
-        svn co "$svnroot/$dep/$build" ./
-        cd ../../
-    done
-fi
 
 cd $buildir
 
 # hopefully we have python2 on this system
 export PYTHON="$(which python2 2>/dev/null || which python)"
 
-make -f nightingale.mk clobber
-make -f nightingale.mk
+make -f songbird.mk clobber
+make -f songbird.mk
 
 # insert a copy of the above code to locate gstreamer libs on ngale launch so we don't have to symlink anymore
 if [ $patch = 1 ] ; then
