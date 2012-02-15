c15c3910556da59eb4d17d5f99f2ea0a272105b4
rsjtdrjgfuzkfg <rsjtdrjgfuzkfg@abwesend.de>
2012-02-15 17:36:14 +0100

    Consistent use of spaces in build.sh

--- build.sh
+++ build.sh
@@ -12,81 +12,81 @@ version=1.11
 
 # Check for the build deps for the system's architecture and OS
 case $OSTYPE in
-	linux*)
-		arch=$(uname -m)
-		depdirn="linux-$arch"
-		patch=1
-				
-		cd dependencies
-		rm -rf "$depdirn" &> /dev/null
-		
-		if [ -f "$depdirn-$version.tar.lzma" ] ; then
-			tar xvf "$depdirn-$version.tar.lzma"
-		else
-			wget "https://downloads.sourceforge.net/project/ngale/$version-Build-Deps/$arch/$depdirn-$version.tar.lzma"
-			tar xvf "$depdirn-$version.tar.lzma"
-		fi
-		cd ../
-		
-		# use our own gstreamer libs
-		for dir in /usr/lib64 /usr/lib ; do
-			if [ -f ${dir}/gstreamer-0.10/libgstcoreelements.so ] ; then
-				export GST_PLUGIN_PATH=${dir}/gstreamer\-0.10
-				break
-			elif [ -f ${dir}/gstreamer0.10/libgstcoreelements.so ] ; then
-				export GST_PLUIN_PATH=${dir}/gstreamer0.10
-				break
-			fi
-		done
-				
-		# !!!! NOTICE: comment the below out if building on/for Windows or Mac or playback probably won't work !!!!
-		if [ -f nightingale.config ] ; then
-			rm nightingale.config
-		fi
-		echo 'ac_add_options --with-media-core=gstreamer-system' >> nightingale.config
-		;;
-	msys*)
-        depdirn="windows-i686"
-
-		tr -d '\r' < ./components/library/localdatabase/content/schema.sql > tmp.sql
-		rm ./components/library/localdatabase/content/schema.sql
-		mv tmp.sql ./components/library/localdatabase/content/schema.sql
-		
-		cd dependencies
-		
-		if [ ! -d "$depdirn" ] ; then
-			mkdir "$depdirn"
-		fi
-
-		if [ -f "$depdirn-$version.tar.lzma" ] ; then
-			tar --lzma -xvf "$depdirn-$version.tar.lzma" -C "$depdirn"
-		else
-			wget "https://downloads.sourceforge.net/project/ngale/$version-Build-Deps/i686/$depdirn-$version.tar.lzma"
-			tar --lzma -xvf "$depdirn-$version.tar.lzma" -C "$depdirn"
-		fi
-		cd ../
-		;;
-	darwin*)
-		depdirn="macosx-i686"
-        
-        cd dependencies
-
-        if [ ! -d "$depdirn" ] ; then
-            mkdir "$depdirn"
-        fi
-
-        if [ -f "$depdirn-$version.tar.bz2" ] ; then
-            tar --lzma -xvf "$depdirn-$version.tar.bz2" -C "$depdirn"
-        else
-            curl -L -O "https://downloads.sourceforge.net/project/ngale/$version-Build-Deps/osx/$depdirn-$version.tar.bz2"
-            tar -xvf "$depdirn-$version.tar.bz2" -C "$depdirn"
-        fi
-        cd ../
-        ;;
-	*)
-		echo "Can't find deps for $OSTYPE. You may need to build them yourself. Doublecheck the SVN's for \n
-		Songbird and Nightingale trunks to be sure."
-		;;
+  linux*)
+    arch=$(uname -m)
+    depdirn="linux-$arch"
+    patch=1
+    
+    cd dependencies
+    rm -rf "$depdirn" &> /dev/null
+    
+    if [ -f "$depdirn-$version.tar.lzma" ] ; then
+      tar xvf "$depdirn-$version.tar.lzma"
+    else
+      wget "https://downloads.sourceforge.net/project/ngale/$version-Build-Deps/$arch/$depdirn-$version.tar.lzma"
+      tar xvf "$depdirn-$version.tar.lzma"
+    fi
+    cd ../
+    
+    # use our own gstreamer libs
+    for dir in /usr/lib64 /usr/lib ; do
+      if [ -f ${dir}/gstreamer-0.10/libgstcoreelements.so ] ; then
+        export GST_PLUGIN_PATH=${dir}/gstreamer\-0.10
+        break
+      elif [ -f ${dir}/gstreamer0.10/libgstcoreelements.so ] ; then
+        export GST_PLUIN_PATH=${dir}/gstreamer0.10
+        break
+      fi
+    done
+    
+    # !!!! NOTICE: comment the below out if building on/for Windows or Mac or playback probably won't work !!!!
+    if [ -f nightingale.config ] ; then
+      rm nightingale.config
+    fi
+    echo 'ac_add_options --with-media-core=gstreamer-system' >> nightingale.config
+    ;;
+  msys*)
+    depdirn="windows-i686"
+    
+    tr -d '\r' < ./components/library/localdatabase/content/schema.sql > tmp.sql
+    rm ./components/library/localdatabase/content/schema.sql
+    mv tmp.sql ./components/library/localdatabase/content/schema.sql
+    
+    cd dependencies
+    
+    if [ ! -d "$depdirn" ] ; then
+      mkdir "$depdirn"
+    fi
+    
+    if [ -f "$depdirn-$version.tar.lzma" ] ; then
+      tar --lzma -xvf "$depdirn-$version.tar.lzma" -C "$depdirn"
+    else
+      wget "https://downloads.sourceforge.net/project/ngale/$version-Build-Deps/i686/$depdirn-$version.tar.lzma"
+      tar --lzma -xvf "$depdirn-$version.tar.lzma" -C "$depdirn"
+    fi
+    cd ../
+    ;;
+  darwin*)
+    depdirn="macosx-i686"
+    
+    cd dependencies
+    
+    if [ ! -d "$depdirn" ] ; then
+      mkdir "$depdirn"
+    fi
+    
+    if [ -f "$depdirn-$version.tar.bz2" ] ; then
+      tar --lzma -xvf "$depdirn-$version.tar.bz2" -C "$depdirn"
+    else
+      curl -L -O "https://downloads.sourceforge.net/project/ngale/$version-Build-Deps/osx/$depdirn-$version.tar.bz2"
+      tar -xvf "$depdirn-$version.tar.bz2" -C "$depdirn"
+    fi
+    cd ../
+    ;;
+  *)
+    echo "Can't find deps for $OSTYPE. You may need to build them yourself. Doublecheck the SVN's for \n
+    Songbird and Nightingale trunks to be sure."
+    ;;
 esac
 
 # get the vendor build deps...
@@ -94,10 +94,10 @@ cd dependencies
 rm -rf vendor &> /dev/null
 
 if [ -f "vendor-$version.zip" ] ; then
-	unzip "vendor-$version.zip"
+  unzip "vendor-$version.zip"
 else
-	curl -L -O "https://downloads.sourceforge.net/project/ngale/$version-Build-Deps/vendor-$version.zip"
-	unzip "vendor-$version.zip"
+  curl -L -O "https://downloads.sourceforge.net/project/ngale/$version-Build-Deps/vendor-$version.zip"
+  unzip "vendor-$version.zip"
 fi
 cd ../
 
