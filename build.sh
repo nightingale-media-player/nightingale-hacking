94ff03d5d4a928869857f8ad18c97297aef052f6
Matt <matt@Matts-Mac.local>
2012-02-15 00:16:58 -0600

    use the new standard of archived build deps...will upload these here in a sec

--- build.sh
+++ build.sh
@@ -10,20 +10,6 @@ build="release"
 buildir="$(pwd)"
 version=1.11
 
-function get_deps() {	
-	cd dependencies
-	
-	mkdir $depdirn
-	cd $depdirn
-			
-	for dep in ${deps[@]}; do
-		mkdir -p "$dep/$build"
-		cd "$dep/$build"
-		svn co "$svnroot/$dep/$build" ./
-		cd ../../
-	done
-}
-
 # Check for the build deps for the system's architecture and OS
 case $OSTYPE in
 	linux*)
@@ -60,15 +46,18 @@ case $OSTYPE in
 		echo 'ac_add_options --with-media-core=gstreamer-system' >> nightingale.config
 		;;
 	msys*)
+        depdirn="windows-i686"
+
 		tr -d '\r' < ./components/library/localdatabase/content/schema.sql > tmp.sql
 		rm ./components/library/localdatabase/content/schema.sql
 		mv tmp.sql ./components/library/localdatabase/content/schema.sql
 		
 		cd dependencies
-		depdirn="windows-i686"
+		
 		if [ ! -d "$depdirn" ] ; then
 			mkdir "$depdirn"
 		fi
+
 		if [ -f "$depdirn-$version.tar.lzma" ] ; then
 			tar --lzma -xvf "$depdirn-$version.tar.lzma" -C "$depdirn"
 		else
@@ -79,31 +68,19 @@ case $OSTYPE in
 		;;
 	darwin*)
 		depdirn="macosx-i686"
-				# I'm not sure we need all these, but I haven't delved into trimming down the deps we download on osx
-		svnroot="http://publicsvn.songbirdnest.com/vendor-binaries/trunk/$depdirn"
-				deps=(  flac
-								gettext 
-								glib 
-								gst-plugins-bad 
-								gst-plugins-base 
-								gst-plugins-good 
-								gst-plugins-ugly 
-								gstreamer 
-								libIDL 
-								libgpod 
-								libjpeg 
-								libogg 
-								libtheora 
-								libvorbis  
-								mozilla-1.9.2
-								sqlite
-								taglib
-								xulrunner-1.9.2 )
-				
-				if [ ! -d "dependencies/$depdirn" ] ; then
-			get_deps
-		fi
-		;;
+        
+        if [ ! -d "$depdirn" ] ; then
+            mkdir "$depdirn"
+        fi
+
+        if [ -f "$depdirn-$version.tar.bz2" ] ; then
+            tar --lzma -xvf "$depdirn-$version.tar.bz2" -C "$depdirn"
+        else
+            wget "https://downloads.sourceforge.net/project/ngale/$version-Build-Deps/i686/$depdirn-$version.tar.bz2"
+            tar -xvf "$depdirn-$version.tar.bz2" -C "$depdirn"
+        fi
+        cd ../
+        ;;
 	*)
 		echo "Can't find deps for $OSTYPE. You may need to build them yourself. Doublecheck the SVN's for \n
 		Songbird and Nightingale trunks to be sure."
