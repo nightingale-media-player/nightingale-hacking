2f649c26e164d8eb360c5d569473db5d953fee92
Matt Parnell <parwok@gmail.com>
2012-02-29 18:56:25 -0800

    we can use the deps zip file...this stupid commit (of putting these here) causes git conflicts with other branches...sorry!

--- dependencies/vendor/mozbrowser/base/content/test/browser_bug537474.js
+++
@@ -1,8 +0,0 @@
-function test() {
-  var currentWin = content;
-  var newWin =
-    browserDOMWindow.openURI(makeURI("about:"), null,
-                             Ci.nsIBrowserDOMWindow.OPEN_CURRENTWINDOW, null)
-  is(newWin, currentWin, "page loads in the current content window");
-  gBrowser.stop();
-}
