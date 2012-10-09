859e655d3e37f56970c0fc17ee0a0ca021b20b3e
Matt Parnell <parwok@gmail.com>
2012-10-09 03:29:17 +0000

    another one bites the dust
    Conflicts:
    	components/moz/variant/src/sbVariant.cpp
    	components/moz/variant/src/sbVariant.h

--- components/moz/variant/src/sbVariant.h
+++ components/moz/variant/src/sbVariant.h
@@ -33,6 +33,7 @@
 #include <nsIVariant.h>
 #include <nsStringGlue.h>
 #include <xpt_struct.h>
+#include <mozilla/Mutex.h>
 
 /** 
 * Map the nsAUTF8String, nsUTF8String classes to the nsACString and
@@ -181,7 +182,7 @@ private:
 
 protected:
   nsDiscriminatedUnion mData;
-  PRLock*              mDataLock;
+  mozilla::Mutex mDataLock;
 
   PRBool               mWritable;
 };
