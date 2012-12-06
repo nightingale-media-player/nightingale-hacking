53e951bed696170ff575c439bce1ae8126ccc3ad
pavel <pavel@5aad4ff5-4b58-4159-ba3f-0e6328df4cad>
2012-12-06 14:12:52 +0000

    git-svn-id: http://publicsvn.songbirdnest.com/client/trunk@24301 5aad4ff5-4b58-4159-ba3f-0e6328df4cad

--- extensions/artistprofile/components/src/ArtistProfile.js
+++ extensions/artistprofile/components/src/ArtistProfile.js
@@ -11,7 +11,7 @@ const CID         = "{8d445ad0-3e27-11e2-a25f-0800200c9a66}";
 const CONTRACTID  = "@songbirdnest.com/artistprofile;1";
 
 function artistProfile() {
-	this.wrappedJSObject = this;
+  this.wrappedJSObject = this;
   this.ioService = Cc["@mozilla.org/network/io-service;1"]
     .getService(Components.interfaces.nsIIOService);
   var app = Cc["@mozilla.org/fuel/application;1"]
@@ -22,42 +22,44 @@ function artistProfile() {
 
 artistProfile.prototype.constructor = artistProfile;
 artistProfile.prototype = {
-	classDescription: DESCRIPTION,
-	classID:          Components.ID(CID),
-	contractID:       CONTRACTID,
-	QueryInterface: XPCOMUtils.generateQI([Ci.sbIArtistProfile]),
+  classDescription: DESCRIPTION,
+  classID:          Components.ID(CID),
+  contractID:       CONTRACTID,
+  QueryInterface:   XPCOMUtils.generateQI([Ci.sbIArtistProfile]),
 
   hasArtist : function(aMediaItem) {
-    return !!this._getArtistUrl(aMediaItem);      
+    return !!this._getArtistUrl(aMediaItem);
   },
 
-	openArtistProfile : function(aMediaItem,aWindow) {
+  openArtistProfile : function(aMediaItem,aWindow) {
     var artistUrl = this._getArtistUrl(aMediaItem);
     if( !artistUrl ) {
       return;
     }
-     var browser = null;	  
-     try {
-     // Dig through the parent chain to find a happy gBrowser.
-     for ( var win = aWindow;
-       win != null && win != win.parent &&
-       typeof win.gBrowser == 'undefined';
-       win = win.parent ) ; // Yes, it is there on purpose.
-     if ( win ) {
-       browser = win.gBrowser;
-     }
-     } catch(e) {}
-     if( !browser ) {
-       return;
-     }
-     var uri = this.ioService.newURI(artistUrl, null, null);
-     var tab = browser.addTab(uri.spec);
-     function selectNewForegroundTab(bws, tab) {
-       bws.selectedTab = tab;
-     }
-     aWindow.setTimeout(selectNewForegroundTab, 0, browser, tab);
-	},
-  
+    
+    var browser = null;    
+    try {
+    // Dig through the parent chain to find a happy gBrowser.
+    for ( var win = aWindow;
+      win != null && win != win.parent &&
+      typeof win.gBrowser == 'undefined';
+      win = win.parent ); // Yes, it is there on purpose.
+      if( win ) {
+        browser = win.gBrowser;
+      }
+    } catch(e) {}
+      if( !browser ) {
+        return;
+      }
+
+      var uri = this.ioService.newURI(artistUrl, null, null);
+      var tab = browser.addTab(uri.spec);
+      function selectNewForegroundTab(bws, tab) {
+        bws.selectedTab = tab;
+      }
+      aWindow.setTimeout(selectNewForegroundTab, 0, browser, tab);
+  },
+
   _getArtistUrl : function(aMediaItem) {
     var artist = aMediaItem.getProperty(SBProperties.artistName);
     if( !artist || (artist == "") ) {
@@ -68,11 +70,10 @@ artistProfile.prototype = {
     artist = artist.toLowerCase().replace(/\s/gi,"");
     var artistUrl = this.sbmeUrlPattern.replace("{0}",encodeURIComponent(artist));
     return artistUrl;
+    }
   }
-}
 
 var components = [artistProfile];
 function NSGetModule(compMgr, fileSpec) {
-	return XPCOMUtils.generateModule([artistProfile]);
+  return XPCOMUtils.generateModule([artistProfile]);
 }
-
