50cb652cc699ac1e0d1b5be80d62fc326a939f8a
freaktechnik <martin.a.giger@sunrise.ch>
2013-03-17 00:28:32 +0100

    Reopen internal search handler contractID namespace

--- app/content/scripts/searchHandler.js
+++ app/content/scripts/searchHandler.js
@@ -231,10 +231,10 @@ const gSearchHandler = {
       // own listeners as well.
       var contractSearchEngineName = currentEngine.name;
       if(contractSearchEngineName == this.SEARCHENGINE_NAME_SONGBIRD) {
-        contractSearchEngineName = "internal";
+        contractSearchEngineName = "songbird-internal-search";
       }
       var contractID =
-        "@songbirdnest.com/Songbird/songbird-" + contractSearchEngineName + "-search;1";
+        "@songbirdnest.com/Songbird/" + contractSearchEngineName + ";1";
       if (contractID in Cc) {
         var searchEngine = Cc[contractID].getService(Ci.sbISearchEngine);
         searchEngine.doSearch(window, searchBar.value);
