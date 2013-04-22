99d793d46070cf744a2d7ccf4fe4832420dacb87
John Murray <johnmurrayvi@gmail.com>
2013-04-22 10:56:15 -0400

    initial files

---
+++ extensions/libnotify-notifs/components/src/NotifyProxy.cpp
@@ -0,0 +1,13 @@
+
+#include "NotifyProxy.h"
+#include <gtk/gtk.h>
+#include <libnotify/notify.h>
+#include <nsCOMPtr.h>
+#include <nsIObserverService.h>
+#include <nsServiceManagerUtils.h>
+
+
+
+
+
+
