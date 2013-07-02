417ad69899e5a825cbdfcfcdb3cc12eca88c603b
John Murray <johnmurrayvi@gmail.com>
2013-07-02 11:51:50 -0500

    remove version 6 stuff, add old mozexthelper from 1.12 vendor deps

--- dependencies/vendor/mozbrowser.v6/components/sessionstore/test/browser/browser_623779.js
+++
@@ -1,7 +0,0 @@
-function test() {
-  gBrowser.pinTab(gBrowser.selectedTab);
-  var newTab = gBrowser.duplicateTab(gBrowser.selectedTab);
-  ok(!newTab.pinned, "duplicating a pinned tab creates unpinned tab");
-  gBrowser.removeTab(newTab);
-  gBrowser.unpinTab(gBrowser.selectedTab);
-}
