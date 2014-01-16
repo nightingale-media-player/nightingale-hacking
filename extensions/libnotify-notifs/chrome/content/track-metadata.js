35a1cc771a2a97bf0d8f6ee68ff33dd8d17d3d53
freaktechnik <martin.a.giger@sunrise.ch>
2014-01-16 17:11:22 +0100

    Improve libnotify cover loading code

--- extensions/libnotify-notifs/chrome/content/track-metadata.js
+++ extensions/libnotify-notifs/chrome/content/track-metadata.js
@@ -47,6 +47,7 @@ lnNotifs = {
     mainwindow: null,
     prefs: null,
     lnNotifsService: null,
+    ios: null,
 
     onLoad: function() {
         var mainwindow = window.QueryInterface(Ci.nsIInterfaceRequestor).getInterface(Ci.nsIWebNavigation)
@@ -56,8 +57,11 @@ lnNotifs = {
         this.strConv = Cc["@mozilla.org/intl/scriptableunicodeconverter"].getService(Ci.nsIScriptableUnicodeConverter);
         this.lnNotifsService = Cc["@getnightingale.com/Nightingale/lnNotifs;1"].getService(Ci.ILNNotifs);
         this.wm = Cc["@mozilla.org/appshell/window-mediator;1"].getService(Ci.nsIWindowMediator);
+        this.ios = Cc["@mozilla.org/network/io-service;1"].getService(Ci.nsIIOService);
+
         this.mainwindow = this.wm.getMostRecentWindow("Songbird:Main");
         var windowTitle = this.mainwindow.document.getElementById("mainplayer").getAttribute("title");
+
         this.prefs = Cc["@mozilla.org/preferences-service;1"].getService(Ci.nsIPrefService).getBranch("extensions.libnotify-notifs.");
 
         this.lnNotifsService.InitNotifs(windowTitle);
@@ -71,28 +75,33 @@ lnNotifs = {
                 // Get mediacore event
                 if (that.mmService.sequencer.view == null) return;
 
-                switch (event.type) {
-                    case Ci.sbIMediacoreEvent.TRACK_CHANGE:
-                        // Check if notifications are enabled
-                        var notifsEnabled = lnNotifs.prefs.getBoolPref("enableNotifications");
-                        lnNotifs.lnNotifsService.EnableNotifications(notifsEnabled);
+                if (event.type == Ci.sbIMediacoreEvent.TRACK_CHANGE) {
+                    // Check if notifications are enabled
+                    var notifsEnabled = lnNotifs.prefs.getBoolPref("enableNotifications");
+                    lnNotifs.lnNotifsService.EnableNotifications(notifsEnabled);
 
-                        var curItem = that.mmService.sequencer.currentItem;
-                        // Don't notify for mediacore sequencing oddities
-                        if (that.lastItem == curItem) return;
-                        else that.lastItem = curItem;
+                    var curItem = that.mmService.sequencer.currentItem;
+                    // Don't notify for mediacore sequencing oddities
+                    if (that.lastItem == curItem) return;
+                    else that.lastItem = curItem;
 
-                        var resourceURL = curItem.getProperty(SBProperties.primaryImageURL);
-                        var artist = that.strConv.ConvertFromUnicode(curItem.getProperty(SBProperties.artistName));
-                        var album = that.strConv.ConvertFromUnicode(curItem.getProperty(SBProperties.albumName));
-                        var track = that.strConv.ConvertFromUnicode(curItem.getProperty(SBProperties.trackName));
-                        var timeout = that.prefs.getIntPref("notificationTimeout");
-                        that.downloadFileToTemp(resourceURL, function (coverFilePath) {
-                            that.lnNotifsService.TrackChangeNotify(track, artist, album, coverFilePath, timeout);
-                        });
-                        break;
-                    default:
-                        break;
+                    var resourceURL = curItem.getProperty(SBProperties.primaryImageURL);
+                    var fileURI = null;
+                    if(resourceURL) {
+                        try{
+                            fileURI = that.ios.newURI(decodeURI(resourceURL), null, null).QueryInterface(Ci.nsIFileURL).file.path;
+                        }
+                        catch(e) {
+                            Cu.reportError(e);
+                        }
+                    }
+
+                    var artist = that.strConv.ConvertFromUnicode(curItem.getProperty(SBProperties.artistName));
+                    var album = that.strConv.ConvertFromUnicode(curItem.getProperty(SBProperties.albumName));
+                    var track = that.strConv.ConvertFromUnicode(curItem.getProperty(SBProperties.trackName));
+                    var timeout = that.prefs.getIntPref("notificationTimeout");
+
+                    that.lnNotifsService.TrackChangeNotify(track, artist, album, fileURI, timeout);
                 }
             }
         });
@@ -100,68 +109,6 @@ lnNotifs = {
 
     onUnload: function() {
 
-    },
-
-    downloadFileToTemp: function (aWebURL, aCallback) {
-        if (!aWebURL || aWebURL == "") {
-            aCallback(null);
-            return;
-        }
-        
-        // The tempFile we are saving to.
-        var tempFile = Cc["@mozilla.org/file/directory_service;1"]
-                         .getService(Ci.nsIProperties)
-                         .get("TmpD", Ci.nsIFile);
-        
-        var webProgressListener = {
-            QueryInterface: XPCOMUtils.generateQI([Ci.nsIWebProgressListener]),
-
-            /* nsIWebProgressListener methods */
-            // No need to implement anything in these functions
-            onLocationChange : function (a, b, c) { },
-            onProgressChange : function (a, b, c, d, e, f) { },
-            onSecurityChange : function (a, b, c) { },
-            onStatusChange : function (a, b, c, d) { },
-            onStateChange : function (aWebProgress, aRequest, aStateFlags, aStatus) {
-                // when the transfer is complete...
-                if (aStateFlags & Ci.nsIWebProgressListener.STATE_STOP) {
-                    if (aStatus == 0) {
-                        aCallback(tempFile.path);
-                    } else { }
-                }
-            }
-        };
-        
-        var fileName = aWebURL.substr(aWebURL.lastIndexOf("/")+1);
-        tempFile.append(fileName);
-        
-        try {
-            tempFile.create(Ci.nsIFile.NORMAL_FILE_TYPE, 0644);
-        } catch (e) {
-            if (e.name == "NS_ERROR_FILE_ALREADY_EXISTS")
-                aCallback(tempFile.path);
-            else
-                aCallback(null);
-                
-            return;
-        };
-        
-        // Make sure it is deleted when we shutdown
-        var registerFileForDelete = Cc["@mozilla.org/uriloader/external-helper-app-service;1"]
-                                     .getService(Ci.nsPIExternalAppLauncher);
-        registerFileForDelete.deleteTemporaryFileOnExit(tempFile);
-        
-        // Download the file
-        var ioService = Cc["@mozilla.org/network/io-service;1"].getService(Ci.nsIIOService);
-        var webDownloader = Cc['@mozilla.org/embedding/browser/nsWebBrowserPersist;1'].createInstance(Ci.nsIWebBrowserPersist);
-        webDownloader.persistFlags = Ci.nsIWebBrowserPersist.PERSIST_FLAGS_NONE;
-        webDownloader.progressListener = webProgressListener;
-        webDownloader.saveURI(ioService.newURI(aWebURL, null, null), // URL
-                              null,
-                              null,
-                              null,
-                              null,
-                              tempFile);  // File to save to
     }
 };
 
