dade6863770e001e3f88a0a0678971150d84ac09
Martin Giger <martin.a.giger@sunrise.ch>
2014-05-28 19:36:19 +0200

    Added some signals for the tracklist interface

--- extensions/mpris/extension-config.mk
+++ extensions/mpris/extension-config.mk
@@ -4,10 +4,10 @@
 # This file is part of the Nightingale web player.
 #
 # http://getnightingale.com
-# 
+#
 # This file may be licensed under the terms of of the
 # GNU General Public License Version 2 (the "GPL").
-# 
+#
 # Software distributed under the License is distributed
 # on an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either
 # express or implied. See the GPL for the specific language
@@ -24,7 +24,7 @@
 EXTENSION_NAME = mpris
 EXTENSION_UUID = $(EXTENSION_NAME)@getnightingale.com
 
-EXTENSION_VER = 1.0.0.0
+EXTENSION_VER = 1.1.0.0
 EXTENSION_MIN_VER = $(SB_BINARY_EXTENSION_MIN_VER)
 EXTENSION_MAX_VER = $(SB_BINARY_EXTENSION_MAX_VER)
 
