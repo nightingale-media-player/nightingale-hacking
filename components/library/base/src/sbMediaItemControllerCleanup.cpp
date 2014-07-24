02daeb4a56dc4a422d61d1245ab1e318bed0fbe6
John Murray <johnmurrayvi@gmail.com>
2014-07-24 15:47:26 -0500

    try keeping mMutex unlocked for the else statement

--- components/library/base/src/sbMediaItemControllerCleanup.cpp
+++ components/library/base/src/sbMediaItemControllerCleanup.cpp
@@ -133,13 +133,14 @@ sbMediaItemControllerCleanup::Observe(nsISupports *aSubject,
 
   if (!strcmp(aTopic, "idle")) {
     // idle observer: we are now idle
-    mozilla::MutexAutoLock lock(mMutex);
+    mMutex.Lock();
     if (mState == STATE_QUEUED) {
       TRACE("preparing to run: STATE->RUNNING");
       mState = STATE_RUNNING;
       rv = mBackgroundEventTarget->Dispatch(static_cast<nsIRunnable*>(this),
                                             NS_DISPATCH_NORMAL);
       NS_ENSURE_SUCCESS(rv, rv);
+      mMutex.Unlock();
     }
     else if (mState == STATE_STOPPING) {
       // we can un-pause
@@ -148,12 +149,12 @@ sbMediaItemControllerCleanup::Observe(nsISupports *aSubject,
       }
       TRACE("Resuming: STATE->RUNNING");
       mState = STATE_RUNNING;
+      mMutex.Unlock();
     }
     else {
       // we no longer need to hold on to the lock for anything; let it go now
       // because do_ProxiedGetService may cause a nested event loop
-      // XXX Need to fix this lock!
-      // lock.unlock();
+      mMutex.Unlock();
       TRACE("nothing to do, ignoring idle notification");
       nsCOMPtr<nsIObserverService> obs =
         do_ProxiedGetService(NS_OBSERVERSERVICE_CONTRACTID, &rv);
