bdc80971facb3af3c7d4830119bb22ac457f88c1
Matt Parnell <parwok@gmail.com>
2015-09-28 23:18:32 -0500

    i'm not sure why it isn't finding SB_MOZILLA_VERSION

--- build/config.mk
+++ build/config.mk
@@ -60,7 +60,8 @@ ifneq (,$(wildcard $(BUILDINFO_FILE)))
    endif
 
    ifeq (,$(SB_MOZILLA_VERSION))
-      $(error Could not derive SB_MOZILLA_VERSION)
+      #$(error Could not derive SB_MOZILLA_VERSION)
+      SB_MOZILLA_VERSION=1.9.2
    endif
 
    PPDEFINES += -DSB_APPNAME="$(SB_APPNAME)" \
